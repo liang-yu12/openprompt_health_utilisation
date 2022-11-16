5def46ba8c751e749cf814b4efdd44a87e4dc485
Ben Butler-Cole <ben.butler-cole@thedatalab.org>
2022-11-16 16:42:15 +0000

    Remove some duplicated code

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -20,11 +20,6 @@ historical_registration = practice_registrations \
 age_okay = age >= 18
 registered_okay = registration.exists_for_patient()
 
-registration = practice_registrations \
-    .drop(practice_registrations.start_date.difference_in_years(index_date) < 1) \
-    .drop(practice_registrations.end_date <= index_date) \
-    .sort_by(practice_registrations.start_date).last_for_patient()
-
 dataset = Dataset()
 dataset.set_population(age_okay & registered_okay)
 dataset.age = age
