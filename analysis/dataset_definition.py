976b1b9c2a83d327734ad33f803c5dfe22a94edd
Ben Butler-Cole <ben.butler-cole@thedatalab.org>
2022-11-16 16:54:03 +0000

    Add long-covid diagnoses to dataset

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -1,7 +1,9 @@
 from datetime import date
 
 from databuilder.ehrql import Dataset
-from databuilder.tables.beta.tpp import patients, practice_registrations
+from databuilder.tables.beta.tpp import patients, practice_registrations, clinical_events
+
+from codelists_ehrql import lc_codelists_combined
 
 index_date = date(2020, 11, 1)
 age = patients.date_of_birth.difference_in_years(index_date)
@@ -17,8 +19,15 @@ historical_registration = practice_registrations \
     .drop(practice_registrations.start_date > date(2018, 11, 1)) \
     .drop(practice_registrations.end_date < date(2019, 11, 1))
 
+# long covid diagnoses
+lc_dx = clinical_events.take(clinical_events.snomedct_code.is_in(lc_codelists_combined)) \
+    .sort_by(clinical_events.date) \
+    .first_for_patient()
+
 dataset = Dataset()
 dataset.set_population((age >= 18) & registration.exists_for_patient())
 dataset.age = age
 dataset.registration_date = registration.start_date
 dataset.historical_comparison_group = historical_registration.exists_for_patient()
+dataset.has_lc_dx = lc_dx.exists_for_patient()
+dataset.dx_date = lc_dx.date
