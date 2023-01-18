81422bf690a490bde2f3274ff90c80b41c281afe
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-01-18 11:49:42 +0000

    change paths again

--- analysis/dm01_filter_gp_without_lc_dx.R
+++ analysis/dm01_filter_gp_without_lc_dx.R
@@ -2,14 +2,13 @@ library(data.table)
 library(magrittr)
 
 # make a list of GP using long COVID diagnosis
-gp_list <- unlist(
-  fread(here::here("./output","dataset_lc_gp_list.csv"), 
-  select = "gp_practice")
-)
+gp_list_data <- fread(here::here("output","dataset_lc_gp_list.csv")
+
+gp_list <- unlist(gp_list_data$gp_practice)
 
 # read in the potential comparator groups 
-potential_comparator <- fread("./output/dataset_comparator_large.csv") %>% data.table()
+potential_comparator <- fread("output/dataset_comparator_large.csv") %>% data.table()
 filtered_potential_comparator <- potential_comparator[gp_practice %in% gp_list]
 
 # save the output
-filtered_potential_comparator %>% fwrite("./output/dataset_comparator_filtered_gp.csv")
+filtered_potential_comparator %>% fwrite("output/dataset_comparator_filtered_gp.csv")
