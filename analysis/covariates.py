85371491b8a09343e1c6ee846d4b5291f95f99bb
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-02-15 17:06:46 +0000

    update import for cov; use three months regist GP

--- analysis/covariates.py
+++ analysis/covariates.py
@@ -1,13 +1,14 @@
-# Functions for defining covariates
+# local variables for defining covariates
 from datetime import date
-from databuilder.ehrql import Dataset, days, years
+from databuilder.ehrql import Dataset, days, years,  case, when
 from databuilder.tables.beta.tpp import (
     patients, addresses, appointments,
     practice_registrations, clinical_events,
     sgss_covid_all_tests, ons_deaths, hospital_admissions,
 )
-from databuilder.codes import CTV3Code, DMDCode, ICD10Code, SNOMEDCTCode
+from databuilder.codes import CTV3Code, DMDCode, ICD10Code, SNOMEDCTCode, Codelist
 import codelists
+
 from variables import add_visits
 
 study_start_date = date(2020, 11, 1)
@@ -34,9 +35,12 @@ index_date_address = addresses.drop(addresses.start_date > study_start_date) \
 # Contemporary comorbidities: 
 
 # # Hypertension
-htn = clinical_events.take(clinical_events.)
+htn = clinical_events.take(clinical_events)
 
 
 
 
 # severe immunosuppression
+
+# vaccine dose: at least one dose/one dose/two dose/three doses or more
+
