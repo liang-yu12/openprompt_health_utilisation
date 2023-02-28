fadd22d02fc4e3c33ce1965e48d04e3edb5318ab
David Evans <dave.evans@thedatalab.org>
2023-02-28 13:20:12 +0000

    Use new, simpler codelists API

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -1,54 +1,41 @@
-from databuilder.codes import REGISTRY, Codelist, codelist_from_csv
-from codelists import *
-
-# 0. define a function to combine multiple codeslists
-def combine_codelists(*codelists):
-    codes = set()
-    for codelist in codelists:
-        codes.update(codelist.codes)
-    return Codelist(codes=codes, category_maps={})
+from databuilder.ehrql import codelist_from_csv
 
 # 1. Long COVID
 # # import different long COVID codelists:
 long_covid_assessment_codes = codelist_from_csv(
     "codelists/opensafely-assessment-instruments-and-outcome-measures-for-long-covid.csv",
-    system="snomedct", 
     column = "code"
 )     
     
 long_covid_dx_codes =  codelist_from_csv(
     "codelists/opensafely-nice-managing-the-long-term-effects-of-covid-19.csv",
-    system="snomedct", 
     column = "code"
 ) 
 
 long_covid_referral_codes = codelist_from_csv(
     "codelists/opensafely-referral-and-signposting-for-long-covid.csv",
-    system="snomedct", 
     column = "code"
 ) 
 
 # # Combine long covid codelists
-lc_codelists_combined = combine_codelists(
-    long_covid_dx_codes,
-    long_covid_referral_codes,
-    long_covid_assessment_codes
+lc_codelists_combined = (
+    long_covid_dx_codes
+    + long_covid_referral_codes
+    + long_covid_assessment_codes
 )
 
 # 2. Ethnicities: 
 
 ethnicity = codelist_from_csv(
     "codelists/opensafely-ethnicity.csv",
-    system="ctv3",
     column="Code",
 )
 
 # 3. Mental issues:
 psychosis_schizophrenia_bipolar_codes = codelist_from_csv(
     "codelists/opensafely-psychosis-schizophrenia-bipolar-affective-disease.csv",
-    system="ctv3",
     column="CTV3Code",
 )
 depression_codes = codelist_from_csv(
-    "codelists/opensafely-depression.csv", system="ctv3", column="CTV3Code"
+    "codelists/opensafely-depression.csv", column="CTV3Code"
 )
