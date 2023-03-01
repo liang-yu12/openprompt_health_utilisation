511c04b415397445b1a7e52ec610a2bee03b2eaf
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-03-01 18:08:38 +0000

    ethnicities are not working, don't know why

--- analysis/covariates.py
+++ analysis/covariates.py
@@ -21,18 +21,37 @@ study_start_date = date(2020, 11, 1)
 age = (study_start_date - patients.date_of_birth).years
 
 # Demographic: ethnicity
-# # Ethnicity 
+## Ethnicity 
 ethnicity = clinical_events.take(clinical_events.ctv3_code.is_in(codelists.ethnicity)) \
     .sort_by(clinical_events.date) \
     .last_for_patient() \
     .ctv3_code.to_category(codelists.ethnicity.Grouping_6)
 
-# IMD
+## IMD
 # # 1. drop the start date records after index date
 # # 2. sort the date, keep the latest
 imd = addresses.drop(addresses.start_date > study_start_date) \
     .sort_by(addresses.start_date) \
     .last_for_patient().imd_rounded
+
+## BMI
+bmi_record = (
+    clinical_events.take(
+        clinical_events.snomedct_code.is_in(
+            [SNOMEDCTCode("60621009"), SNOMEDCTCode("846931000000101")]
+        )
+    )
+    # Exclude out-of-range values
+    .take((clinical_events.numeric_value > 4.0) & (clinical_events.numeric_value < 200.0))
+    # Exclude measurements taken when patient was younger than 16
+    .take(clinical_events.date >= patients.date_of_birth + years(16))
+    .sort_by(clinical_events.date)
+    .last_for_patient()
+)
+
+bmi = bmi_record.numeric_value
+bmi_date = bmi_record.date
+
 
 # Clinical factors:
 
@@ -54,3 +73,5 @@ dataset = Dataset()
 dataset.set_population(age>= 18)
 dataset.ethnicity = ethnicity
 dataset.imd = imd
+dataset.bmi = bmi
+dataset.bmi_date = bmi_date
