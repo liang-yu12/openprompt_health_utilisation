22ede4ad36d56c995bd7b6911742afb84fbc8b49
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-03-22 19:00:58 +0000

    packages settings

---
+++ analysis/settings_packages.R
@@ -0,0 +1,7 @@
+# loading r packages
+
+packages <- c("data.table", "lubridate", "markdown", "forcats", "Hmisc",
+              "broom", "dplyr", "finalfit", "forcats", "ggplot2", "ggpubr",
+              "MASS", "stringr", "tidyr", "here")
+
+lapply(packages, require, character.only = TRUE)
