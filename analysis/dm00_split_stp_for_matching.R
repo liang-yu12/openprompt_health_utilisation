e1fa4a982ec5093439bed6965e3d0420fe428498
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-03-30 13:58:43 +0100

    Keep the original names

--- analysis/dm00_split_stp_for_matching.R
+++ analysis/dm00_split_stp_for_matching.R
@@ -14,15 +14,15 @@ exp_split_list <- split(dataset_exp_lc_unmatched, by = "region")
 # save the original titles for checking:
 data.frame(
       stp_names = names(exp_split_list),
-      numbers = seq_along(exp_split_list)
+      numbers = str_sub(names(exp_split_list),-2, -1)
 ) %>% as.list() %>% 
       fwrite(here("output", "exp_stp_names_numbers.csv"))
 
-names(exp_split_list) <- seq_along(exp_split_list) # rename the list for saving
+# names(exp_split_list) <- str_sub(names(exp_split_list) # rename the list for saving
 
 # # Step 3: Save each split data.table to csv files using write.csv()
 # lapply(names(exp_split_list), function(x) {
-#       fwrite(as.list(exp_split_list[[x]]), file = here("output", paste0("exp_stp_", x, ".csv")), 
+#       fwrite(as.list(exp_split_list[[x]]), file = here("output", paste0("exp_stp_", str_sub(names(x),-2, -1), ".csv")), 
 #              row.names = FALSE)
 # })
 
@@ -32,21 +32,21 @@ names(exp_split_list) <- seq_along(exp_split_list) # rename the list for saving
 dataset_comparator_unmatched <- fread(here::here("output", "dataset_comparator_unmatched.csv"))
 
 # Step 2: Split the data.table by a variable level using data.table::split()
-comp_split_list <- split(dataset_comparator_unmatched, by = "region")
+com_split_list <- split(dataset_comparator_unmatched, by = "region")
 
 # save the original titles for checking:
 data.frame(
-      stp_names = names(comp_split_list),
-      numbers = seq_along(comp_split_list)
+      stp_names = names(com_split_list),
+      numbers = str_sub(names(com_split_list),-2, -1)
 ) %>% as.list() %>% 
       fwrite(here("output", "com_stp_names_numbers.csv"))
 
        
-# names(comp_split_list) <- seq_along(comp_split_list)
+# names(comp_split_list) <- str_sub(names(com_split_list)
 # 
 # # Step 3: Save each split data.table to csv files using write.csv()
 # lapply(names(comp_split_list), function(x) {
-#       fwrite(as.list(comp_split_list[[x]]), file = here("output", paste0("comp_stp_", x, ".csv")), 
+#       fwrite(as.list(comp_split_list[[x]]), file = here("output", paste0("comp_stp_", str_sub(names(x),-2, -1), ".csv")), 
 #                 row.names = FALSE)
 # })
 
