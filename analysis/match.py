816a5eb810509e7edc4747263f84166b51c07d47
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-03-30 18:24:31 +0100

    generate py files for matching

--- analysis/match.py
+++ analysis/match.py
@@ -1,25 +1,38 @@
 import pandas as pd
+
+# List of numbers to replace "05" in filenames
+numbers = ['05', '24', '26', '23', '06', '13', '49', '15', '41', '09', '17', '25', '16',
+           '07', '42', '21', '40', '27', '14', '20', '29', '43', '08', '37', '22', '12',
+           '33', '35', '36', '10', '44']
+
+# Common code
+common_code = '''
+import pandas as pd
 from osmatching import match
 
-## MATCH SEPARATE STPS OF CASES AND CONTROLS
-
 match(
-    case_csv="dataset_exp_lc_unmatched",
-    match_csv="dataset_comparator_unmatched",
+    case_csv="exp_stp_{num}.csv",
+    match_csv="com_stp_{num}.csv",
     matches_per_case=5,
-    match_variables={
+    match_variables={{
         "age": 1,
         "sex": "category",
-        "region": "category",
-    },
+    }},
     closest_match_variables=["age"],
-    index_date_variable = "index_date",
+    index_date_variable="index_date",
     replace_match_index_date_with_case="no_offset", 
-    indicator_variable_name = "exposure",
-    date_exclusion_variables={
+    indicator_variable_name="exposure",
+    date_exclusion_variables={{
         "end_death": "before",
         "end_deregist": "before",
         "long_covid_dx_date": "before",
-    },
+    }},
     output_path="output",
 )
+'''
+
+# Loop over numbers and generate code for each file
+for num in numbers:
+    code = common_code.format(num=num)
+    with open(f'matching_{num}.py', 'w') as f:
+        f.write(code)
---
+++ analysis/matching_49.py
@@ -0,0 +1,23 @@
+
+import pandas as pd
+from osmatching import match
+
+match(
+    case_csv="exp_stp_49.csv",
+    match_csv="com_stp_49.csv",
+    matches_per_case=5,
+    match_variables={
+        "age": 1,
+        "sex": "category",
+    },
+    closest_match_variables=["age"],
+    index_date_variable="index_date",
+    replace_match_index_date_with_case="no_offset", 
+    indicator_variable_name="exposure",
+    date_exclusion_variables={
+        "end_death": "before",
+        "end_deregist": "before",
+        "long_covid_dx_date": "before",
+    },
+    output_path="output",
+)
