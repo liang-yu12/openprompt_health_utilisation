9a10c8fa6a4ef839383d57ec1fbf44d6aef41aab
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-03-30 11:56:17 +0100

    add gtools to the package list

--- analysis/settings_packages.R
+++ analysis/settings_packages.R
@@ -2,6 +2,6 @@
 
 packages <- c("data.table", "lubridate", "markdown", "forcats", "Hmisc",
               "broom", "dplyr", "finalfit", "forcats", "ggplot2", "ggpubr",
-              "MASS", "stringr", "tidyr", "here")
+              "MASS", "stringr", "tidyr", "here", "gtools")
 
 lapply(packages, require, character.only = TRUE)
