b12ea877e01c9f3bd94ba23e2856745d1c81a68a
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-03-30 15:41:58 +0100

    fix the naming length issue

--- analysis/dm00_split_stp_for_matching.R
+++ analysis/dm00_split_stp_for_matching.R
@@ -18,11 +18,11 @@ data.frame(
 ) %>% as.list() %>% 
       fwrite(here("output", "exp_stp_names_numbers.csv"))
 
-names(exp_split_list) <- str_sub(names(exp_split_list)) # rename the list for saving
+names(exp_split_list) <- str_sub(names(exp_split_list),-2, -1) # rename the list for saving
 
 # Step 3: Save each split data.table to csv files using write.csv()
 lapply(names(exp_split_list), function(x) {
-      fwrite(as.list(exp_split_list[[x]]), file = here("output", paste0("exp_stp_", str_sub(names(x),-2, -1), ".csv")),
+      fwrite(as.list(exp_split_list[[x]]), file = here("output", paste0("exp_stp_", x, ".csv")),
              row.names = FALSE)
 })
 
@@ -42,11 +42,11 @@ data.frame(
       fwrite(here("output", "com_stp_names_numbers.csv"))
 
        
-names(com_split_list) <- str_sub(names(com_split_list))
+names(com_split_list) <- str_sub(names(com_split_list),-2,-1)
 
 # Step 3: Save each split data.table to csv files using write.csv()
 lapply(names(com_split_list), function(x) {
-      fwrite(as.list(com_split_list[[x]]), file = here("output", paste0("com_stp_", str_sub(names(x),-2, -1), ".csv")),
+      fwrite(as.list(com_split_list[[x]]), file = here("output", paste0("com_stp_", x, ".csv")),
                 row.names = FALSE)
 })
 
