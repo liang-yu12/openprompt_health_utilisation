38d4e382b2b2752f36ef4c8bc83496f31a4db3d7
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-03-30 18:40:30 +0100

    cahnge the matching yaml

--- analysis/match.py
+++ analysis/match.py
@@ -37,3 +37,24 @@ for num in numbers:
     code = common_code.format(num=num)
     with open(f'matching_{num}.py', 'w') as f:
         f.write(code)
+
+
+
+# numbers = ['24', '26', '23', '6', '13', '49', '15', '41', '09', '17', '25', '16', '07', '42', '21', '40', '27', '14', '20', '29', '43', '08', '37', '22', '12', '33', '35', '36', '10', '44']
+
+# # Loop over numbers and generate code for each file
+# for num in numbers:
+#     yaml_str = '''
+#   match_comparators_{num}:
+#     run:
+#       python:latest python analysis/matching_{num}.py
+#     needs: [split_unmatched_data_by_stp_regions]
+#     outputs: 
+#       highly_sensitive:
+#         matched_cases: output/matched_cases_stp_{num}.csv
+#         matched_matches: output/matched_matches_stp_{num}.csv
+#         matched_all: output/matched_combined_stp_{num}.csv
+#       moderately_sensitive: 
+#         matching_report: output/matching_report_stp_{num}.txt
+# '''.format(num=num)
+#     print(yaml_str) # replace with your preferred way to output or save the YAML string
