90648c7ca90f329b5cf7a1dd6d42d833bdf2d882
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-03-31 16:02:02 +0100

    no tidyr

--- analysis/settings_packages.R
+++ analysis/settings_packages.R
@@ -2,7 +2,7 @@
 
 packages <- c("data.table", "lubridate", "markdown", "forcats", "Hmisc",
               "broom", "dplyr", "finalfit", "forcats", "ggplot2", "ggpubr",
-              "MASS", "stringr", "tidyr", "here", "gtools",
+              "MASS", "stringr", "here", "gtools",
               "knitr")
 
 lapply(packages, require, character.only = TRUE)
