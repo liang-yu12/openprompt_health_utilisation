2e8e907f76e94db5c48cc7264064a2f13c5ffb37
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-03-31 16:00:23 +0100

    no arsenal package

--- analysis/settings_packages.R
+++ analysis/settings_packages.R
@@ -3,6 +3,6 @@
 packages <- c("data.table", "lubridate", "markdown", "forcats", "Hmisc",
               "broom", "dplyr", "finalfit", "forcats", "ggplot2", "ggpubr",
               "MASS", "stringr", "tidyr", "here", "gtools",
-              "arsenal", "knitr")
+              "knitr")
 
 lapply(packages, require, character.only = TRUE)
