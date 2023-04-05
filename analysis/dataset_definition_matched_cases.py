e93a5826c475d39756c686575d8f310181c641cc
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-04-05 19:38:28 +0100

    Figuring out the table_from_file

--- analysis/dataset_definition_matched_cases.py
+++ analysis/dataset_definition_matched_cases.py
@@ -3,13 +3,19 @@ from databuilder.tables.beta.tpp import (
     patients, addresses, ons_deaths, sgss_covid_all_tests,
     practice_registrations, clinical_events,
 )
+from databuilder.tables import PatientFrame, Series, table_from_file
 from covariates import *
 import csv 
 
-with open("output/matched_cases_stp.csv") as csv_file:
-    matched_cases = csv.DictReader(csv_file)
 
-
+@table_from_file(["output/matched_cases_stp.csv"])
+class matched_cases(PatientFrame):
+  [patient_id] = Series([int])
+  [age] = Series([int])
+  [sex] = Series([str])
+  [region] = Series([str])
+  [gp_practice] = Series([int])
+  [registration_date] = Series([date])
 
 dataset = Dataset()
 dataset.define_population((age >= 18) & registration.exists_for_patient() & matched_cases.exists_for_patient())
