1ebe21d20687fc062951f90034db9caa41c86fc7
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-04-06 13:02:26 +0100

    Try table_from_file but not successful

--- analysis/dataset_definition_matched_cases.py
+++ analysis/dataset_definition_matched_cases.py
@@ -1,21 +1,33 @@
+from datetime import date
 from databuilder.ehrql import Dataset, days, years
 from databuilder.tables.beta.tpp import (
     patients, addresses, ons_deaths, sgss_covid_all_tests,
     practice_registrations, clinical_events,
 )
-from databuilder.tables import PatientFrame, Series, table_from_file
+from databuilder.query_language import table_from_file, PatientFrame, Series
 from covariates import *
-import csv 
 
-
-@table_from_file(["output/matched_cases_stp.csv"])
+@table_from_file("output/matched_cases_stp.csv")
 class matched_cases(PatientFrame):
-  [patient_id] = Series([int])
-  [age] = Series([int])
-  [sex] = Series([str])
-  [region] = Series([str])
-  [gp_practice] = Series([int])
-  [registration_date] = Series([date])
+    patient_id = Series(int)
+    age = Series(int)
+    sex = Series(str)
+    region = Series(str)
+    gp_practice =  Series(int)
+    registration_date = Series(date)
+    long_covid_dx = Series(int)
+    long_covid_dx_date= Series(date)
+    index_date = Series(date)
+    end_death = Series(date)
+    end_deregist = Series(date)
+    end_lc_cure = Series(date)
+    set_id = Series(int)
+    exposure = Series(int)
+    match_counts = Series(int)
 
 dataset = Dataset()
-dataset.define_population((age >= 18) & registration.exists_for_patient() & matched_cases.exists_for_patient())
+dataset.define_population(
+    (age >= 18) 
+    & registration.exists_for_patient() 
+    & matched_cases.exists_for_patient()
+)
