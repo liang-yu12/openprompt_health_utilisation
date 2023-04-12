2bfaf5434ba7ea90b4228b175b8e5abae041deda
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-04-12 15:15:59 +0100

    add other covariates but not working

--- analysis/dataset_definition_matched_cases.py
+++ analysis/dataset_definition_matched_cases.py
@@ -27,7 +27,29 @@ class matched_cases(PatientFrame):
 
 dataset = Dataset()
 dataset.define_population(
-    (age >= 18) 
-    & registration.exists_for_patient() 
+    (age >= 18)
     & matched_cases.exists_for_patient()
 )
+dataset.age = matched_cases.age
+dataset.covid_positive = latest_test_before_diagnosis.exists_for_patient()
+dataset.covid_dx_month = latest_test_before_diagnosis.specimen_taken_date.to_first_of_month() # only need dx month
+dataset.ethnicity = ethnicity
+dataset.imd = imd
+dataset.bmi = bmi
+dataset.bmi_date = bmi_date
+dataset.previous_covid_hosp = previous_covid_hos.exists_for_patient()
+dataset.cov_c19_vaccine_number = c19_vaccine_number
+dataset.cov_cancer = cancer_all.exists_for_patient()
+dataset.cov_mental_health = mental_health_issues.exists_for_patient()
+dataset.cov_asthma = asthma.exists_for_patient() & ~copd.exists_for_patient()
+dataset.cov_organ_transplant = organ_transplant.exists_for_patient()
+dataset.cov_chronic_cardiac_disease = chronic_cardiac_disease.exists_for_patient()
+dataset.cov_chronic_liver_disease = chronic_liver_disease.exists_for_patient()
+dataset.cov_stroke_dementia = stroke.exists_for_patient() | dementia.exists_for_patient()
+dataset.cov_other_neuro_diseases = other_neuro_diseases.exists_for_patient()
+dataset.cov_ra_sle_psoriasis = ra_sle_psoriasis.exists_for_patient()
+dataset.cov_asplenia = asplenia.exists_for_patient()
+dataset.cov_hiv = hiv.exists_for_patient()
+dataset.cov_aplastic_anemia = aplastic_anemia.exists_for_patient()
+dataset.cov_permanent_immune_suppress = permanent_immune_suppress.exists_for_patient()
+dataset.cov_temporary_immune_suppress = temporary_immune_suppress.exists_for_patient()
