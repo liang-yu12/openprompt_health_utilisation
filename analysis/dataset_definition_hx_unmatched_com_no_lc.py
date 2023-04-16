441a4106ebf88e113d092f005a297bb91025dcd8
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-04-16 17:27:34 +0100

    Create comp group without matching; del hx_index_d

--- analysis/dataset_definition_hx_unmatched_com_no_lc.py
+++ analysis/dataset_definition_hx_unmatched_com_no_lc.py
@@ -1,19 +1,28 @@
 from datetime import date
 
-from databuilder.ehrql import Dataset, days, years
+from databuilder.ehrql import Dataset, days, years, months
 from databuilder.tables.beta.tpp import (
-    patients, addresses, ons_deaths, sgss_covid_all_tests,
+    patients, addresses, appointments,
     practice_registrations, clinical_events,
+    sgss_covid_all_tests, ons_deaths, 
 )
-from databuilder.codes import SNOMEDCTCode
 from codelists import lc_codelists_combined
-
+from covariates import *
+import codelists
+import csv 
 
 # Defining the historical exposure groups (before matching) 
 study_start_date = date(2020, 11, 1)
 study_end_date = date(2023, 3, 31)
 hx_study_start_date = date(2018, 11, 1)
 hx_study_end_date = date(2020, 3, 1)
+
+# Import the filtered GP list to exclude GP that was not using the long COVID codes
+with open("output/dataset_lc_gp_list.csv") as csv_file:
+    reader = csv.DictReader(csv_file)
+    lc_gp = [int(row["gp_practice"]) for row in reader]
+
+target_practices = practice_registrations.where(practice_registrations.practice_pseudo_id.is_in(lc_gp))
 
 # Registration data: registered on study_start_date & registered on hx_study_start_date
 had_hx_registration = practice_registrations \
@@ -28,21 +37,17 @@ lc_dx = clinical_events.where(clinical_events.snomedct_code.is_in(lc_codelists_c
     .sort_by(clinical_events.date) \
     .first_for_patient()# had lc dx and dx dates
 
-index_date = lc_dx.date
-
 # other demographic vars for matching
 age = (study_start_date - patients.date_of_birth).years
 
-# dataset definition
 dataset = Dataset()
 dataset.define_population(
     had_hx_registration.exists_for_patient()
-    & lc_dx.exists_for_patient()
+    & ~lc_dx.exists_for_patient()
     & (age>= 18) 
     & (age <=100)
 )
 dataset.age = age
 dataset.sex = patients.sex
 dataset.lc_exp = lc_dx.exists_for_patient().map_values({True: 1, False: 0})
-dataset.index_date = index_date
 dataset.region = had_hx_registration.practice_stp
