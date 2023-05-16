5dd3131f7094ba873dc1ddb61a757d1a54877ecc
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-05-16 11:19:42 +0100

    Initial try of twopart model

---
+++ analysis/st03_two_part_model.R
@@ -0,0 +1,23 @@
+# Load previous data management
+source("analysis/dm03_matched_current_data_combine_visits_numbers.R")
+
+# two part model package:
+library(twopartm) # will be merge to setting once the package is added
+
+# test running the model 
+month_1 <- tpm(
+      month_1 ~ sex + age_cat + ethnicity_6 + bmi_cat + imd_q5,
+      data = matched_data,
+      link_part1 = "logit",
+      family_part2 = poisson
+)
+
+# # create a function for monthly healthcare utilisation
+# two_part_model_hc_visit_month_fn <- function(month){
+#       tpm(
+#             month ~ sex + age_cat + ethnicity_6 + bmi_cat + imd_q5,
+#             data = matched_data,
+#             link_part1 = "logit",
+#             family_part2 = poisson
+#       )
+# }
