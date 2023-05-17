c46227d4b070f10e8764800a393c03549f57ef65
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-05-17 14:27:42 +0100

    remove wrong old file

--- analysis/dm03_matched_define_followup_time.R
+++
@@ -1,8 +0,0 @@
-# Load previous data management
-source("analysis/dm01_matched_current_data.R")
-
-# ============== Caclulate follow-up time by month ============== 
-# Explanation: people were followed from the index date, and we will estimate 
-# the healthcare utilisation by month. However, we need to consider 
-# end_date may fall within the monthly follow-up time. 
-# Need to define monthly
