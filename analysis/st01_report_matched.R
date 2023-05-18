a8dbeb26996523e3cf9de94b47f9399c4929ae68
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-05-18 10:02:47 +0100

    Add codes to check missing patterns

--- analysis/st01_report_matched.R
+++ analysis/st01_report_matched.R
@@ -1,11 +1,25 @@
 # Source data management
 source("analysis/dm01_matched_current_data.R")
 
+
+dependent = "exposure"
+explanatory = c("sex", "age","age_cat", "ethnicity_6", "bmi_cat", "imd_q5", 
+                "long_covid_dx", "covid_positive","previous_covid_hosp",
+                "cov_covid_vax_n_cat", "number_comorbidities_cat")
+
 # report numbers: 
-matched_data %>% summary_factorlist(
-      dependent = "exposure",
-      explanatory = c("sex", "age","age_cat", "ethnicity_6", "bmi_cat", "imd_q5", 
-                      "long_covid_dx", "covid_positive","previous_covid_hosp",
-                      "cov_covid_vax_n_cat", "number_comorbidities_cat"),
-      p = TRUE
-) %>% write.csv(here("output", "matched_numbers_table.csv"), row.names = F)
+matched_data %>% summary_factorlist(dependent, explanatory, p = TRUE) %>% 
+      write.csv(here("output", "matched_numbers_table.csv"), row.names = F)
+
+
+# check missing value distribution:
+matched_data %>% ff_glimpse(dependent, explanatory) %>% 
+      write.csv(here("output", "missing_distribution_table.csv"), row.names = F)
+
+
+# check missing pattern:
+png(file=here("output", "missing_pattern_current.png"),
+    width=600, height=600)
+matched_data %>% 
+      missing_pattern(dependent, explanatory)
+dev.off()
