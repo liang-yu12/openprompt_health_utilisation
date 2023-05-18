3198ddc894e027cb6c375d749293101daeb72dae
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-05-18 09:37:57 +0100

    Add covid vax numbers

--- analysis/st01_report_matched.R
+++ analysis/st01_report_matched.R
@@ -6,6 +6,6 @@ matched_data %>% summary_factorlist(
       dependent = "exposure",
       explanatory = c("sex", "age","age_cat", "ethnicity_6", "bmi_cat", "imd_q5", 
                       "long_covid_dx", "covid_positive","previous_covid_hosp",
-                      "number_comorbidities_cat"),
+                      "cov_covid_vax_n_cat", "number_comorbidities_cat"),
       p = TRUE
 ) %>% write.csv(here("output", "matched_numbers_table.csv"), row.names = F)
