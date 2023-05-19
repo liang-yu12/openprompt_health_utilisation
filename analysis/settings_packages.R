5cae4ba4db3163d51828efc387b29ac56ba7272f
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-05-19 11:42:43 +0100

    ass pscl package for hurdle model

--- analysis/settings_packages.R
+++ analysis/settings_packages.R
@@ -3,6 +3,6 @@
 packages <- c("data.table", "lubridate", "markdown", "forcats", "Hmisc",
               "broom", "dplyr", "finalfit", "forcats", "ggplot2", "ggpubr",
               "MASS", "stringr", "here", "gtools", "rmarkdown",
-              "knitr", "twopartm")
+              "knitr", "pscl","twopartm")
 
 lapply(packages, require, character.only = TRUE)
