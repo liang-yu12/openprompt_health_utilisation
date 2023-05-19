f2b5569e083ec9153b9e8780f7580cf53614801b
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-05-19 13:20:10 +0100

    rename the function; change the zero regression using sex, age, regions (matching variables)

--- analysis/st03_hurdle_model.R
+++ analysis/st03_hurdle_model.R
@@ -8,32 +8,13 @@ options(digits=2)
 # The first part: counted data regression (truncated Poisson)
 # The second part: the zero regression (logit)
 
-# crude 
-# Testing  codes:
-# # month 1 
-# crude_hc_m1_reg <- hurdle(
-#       matched_data$all_month1 ~ exposure |  
-#             exposure + age + sex,
-#       data = matched_data,
-#       zero.dist = "binomial",
-#       dist = "poisson"
-# )
-# 
-# # extract the output:
-# # coefficients:
-# coef_results <- data.frame(exp(coef((crude_hc_m1_reg))))
-# names(coef_results) <- "Coeffiencts"
-# # 95%CI:
-# results_ci <- data.frame(exp(confint(crude_hc_m1_reg))) %>% 
-#       setnames(old = c("X2.5..", "X97.5.."), new = c("Lower CI", "Upper CI"))
-# # combine the results
-# results_m1 <- cbind(coef_results, results_ci)
+# Crude: only look at the association between exposure and outcome
 
 
-month_hurdle_fn <- function(month, n) {
+crude_month_hurdle_fn <- function(month, n) {
       # Fit a hurdle model to the data.
       model <- hurdle(
-            month ~ exposure | exposure + age + sex,
+            month ~ exposure | age + sex + region,
             data = matched_data,
             zero.dist = "binomial",
             dist = "poisson"
@@ -58,16 +39,16 @@ month_hurdle_fn <- function(month, n) {
 }
 
 all_results <- bind_rows(
-      month_hurdle_fn(matched_data$all_month1, 1),
-      month_hurdle_fn(matched_data$all_month2, 2),
-      month_hurdle_fn(matched_data$all_month2, 3),
-      month_hurdle_fn(matched_data$all_month2, 4),
-      month_hurdle_fn(matched_data$all_month2, 5),
-      month_hurdle_fn(matched_data$all_month2, 6),
-      month_hurdle_fn(matched_data$all_month2, 7),
-      month_hurdle_fn(matched_data$all_month2, 8),
-      month_hurdle_fn(matched_data$all_month2, 9),
-      month_hurdle_fn(matched_data$all_month2, 10),
-      month_hurdle_fn(matched_data$all_month2, 11),
-      month_hurdle_fn(matched_data$all_month2, 12),
+      crude_month_hurdle_fn(matched_data$all_month1, 1),
+      crude_month_hurdle_fn(matched_data$all_month2, 2),
+      crude_month_hurdle_fn(matched_data$all_month2, 3),
+      crude_month_hurdle_fn(matched_data$all_month2, 4),
+      crude_month_hurdle_fn(matched_data$all_month2, 5),
+      crude_month_hurdle_fn(matched_data$all_month2, 6),
+      crude_month_hurdle_fn(matched_data$all_month2, 7),
+      crude_month_hurdle_fn(matched_data$all_month2, 8),
+      crude_month_hurdle_fn(matched_data$all_month2, 9),
+      crude_month_hurdle_fn(matched_data$all_month2, 10),
+      crude_month_hurdle_fn(matched_data$all_month2, 11),
+      crude_month_hurdle_fn(matched_data$all_month2, 12),
 )
