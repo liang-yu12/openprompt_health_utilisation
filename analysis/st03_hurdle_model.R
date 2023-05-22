a7b0342b6c034feb2ad3022a37fb63605554e400
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-05-22 11:12:51 +0100

    modify the output names for lookup

--- analysis/st03_hurdle_model.R
+++ analysis/st03_hurdle_model.R
@@ -73,4 +73,4 @@ all_results <- bind_rows(
 )
  
 
-all_results %>% write.csv(here("output", "monthly_visits_crude_hurdle.csv"), row.names = T)
+all_results %>% write.csv(here("output", "st03_monthly_visits_crude_hurdle.csv"), row.names = T)
