d29a5978f77aaed04aea2a7ada52505481781669
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-05-24 14:49:07 +0100

    Explore the vaccine doses and index dates

--- analysis/st01_report_matched.R
+++ analysis/st01_report_matched.R
@@ -8,12 +8,43 @@ explanatory = c("sex", "age","age_cat", "ethnicity_6", "bmi_cat", "imd_q5",
                 "cov_c19_vaccine_number", "cov_covid_vaccine_number", 
                 "cov_covid_vax_n_cat", "number_comorbidities_cat")
 
-# report numbers: 
+# Table 1 reporting numbers:  -----
 matched_data %>% summary_factorlist(dependent, explanatory, p = TRUE) %>% 
       write.csv(here("output", "st01_matched_numbers_table.csv"), row.names = F)
 
+# Exploring the distribution of vaccine number and index dates
+plot_vaccine_distribution <- function(x) {
+      # Filter the data by exposure.
+      bind_rows((matched_data %>% 
+            filter(cov_covid_vax_n_cat == x) %>% 
+            filter(exposure == "Comparator") %>% 
+            group_by(index_date) %>% count %>% 
+            mutate(exposure = "Comparator")),
+            (matched_data %>% 
+            filter(cov_covid_vax_n_cat == x) %>% 
+            filter(exposure == "Long covid exposure") %>% 
+            group_by(index_date) %>% count %>% 
+            mutate(exposure = "Long COVID exposure"))) %>% 
+            ggplot(aes(x=index_date, y=n, color = exposure)) +
+            geom_histogram(stat = "identity") +
+            ggtitle(x) +
+            xlab("Index dates") + ylab("Number") 
+}
 
-# check missing value distribution:
+a_0dose <- plot_vaccine_distribution("0 dose")
+b_1dose <- plot_vaccine_distribution("1 dose")
+c_2dose <- plot_vaccine_distribution("2 doses")
+d_3dose <- plot_vaccine_distribution("3 or more doses")
+
+# combine and save the outputs in a picture
+png(file=here("output", "st1_exporing_vax_index_date.png"),
+    width=800, height=1200)
+ggarrange(a_0dose,b_1dose,c_2dose, d_3dose,
+          ncol = 1, nrow = 4) 
+dev.off()
+
+
+# check missing value distribution:------
 matched_data %>% 
       missing_glimpse(dependent, explanatory) %>% 
       write.csv(here("output", "missing_distribution_table.csv"), row.names = F)
