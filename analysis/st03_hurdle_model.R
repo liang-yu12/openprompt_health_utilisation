806f3ac867f9fe3dc14d71f421ab9a8f7ac1f214
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-05-31 16:08:02 +0100

    Stratify the analysis by hospitalisation

--- analysis/st03_hurdle_model.R
+++ analysis/st03_hurdle_model.R
@@ -4,37 +4,19 @@ source("analysis/dm03_matched_define_monthly_follow_up_time.R")
 # For organising the outputs
 options(digits=2)
 
-# Fit hurdle models: 
-# The first part: counted data regression (truncated Poisson)
-# The second part: the zero regression (logit)
 
-# crude 
-# Testing  codes:
-# # month 1 
-# crude_hc_m1_reg <- hurdle(
-#       matched_data$all_month1 ~ exposure |  
-#             exposure + age + sex,
-#       data = matched_data,
-#       zero.dist = "binomial",
-#       dist = "poisson"
-# )
-# 
-# # extract the output:
-# # coefficients:
-# coef_results <- data.frame(exp(coef((crude_hc_m1_reg))))
-# names(coef_results) <- "Coeffiencts"
-# # 95%CI:
-# results_ci <- data.frame(exp(confint(crude_hc_m1_reg))) %>% 
-#       setnames(old = c("X2.5..", "X97.5.."), new = c("Lower CI", "Upper CI"))
-# # combine the results
-# results_m1 <- cbind(coef_results, results_ci)
+# Saperate data for subgroup analysis:
+# previous hospitalisation due to covid
+hospital <- matched_data %>% filter(previous_covid_hosp == "T") 
+# Not hospitalised due to COVID
+non_hospital <- matched_data %>% filter(previous_covid_hosp == "F")
 
-
-month_hurdle_fn <- function(month, n) {
+# Crude Hurdle model function: 
+month_hurdle_fn <- function(month, n, data) {
       # Fit a hurdle model to the data.
       model <- hurdle(
             month ~ exposure | exposure + age + sex,
-            data = matched_data,
+            data = data,
             zero.dist = "binomial",
             dist = "poisson"
       )
@@ -57,22 +39,65 @@ month_hurdle_fn <- function(month, n) {
       return(results)
 }
 
+# A.
+# 1. Crude results: overall ----
 all_results <- bind_rows(
-      month_hurdle_fn(matched_data$all_month1, 1),
-      month_hurdle_fn(matched_data$all_month2, 2),
-      month_hurdle_fn(matched_data$all_month3, 3),
-      month_hurdle_fn(matched_data$all_month4, 4),
-      month_hurdle_fn(matched_data$all_month5, 5),
-      month_hurdle_fn(matched_data$all_month6, 6),
-      month_hurdle_fn(matched_data$all_month7, 7),
-      month_hurdle_fn(matched_data$all_month8, 8),
-      month_hurdle_fn(matched_data$all_month9, 9),
-      month_hurdle_fn(matched_data$all_month10, 10),
-      month_hurdle_fn(matched_data$all_month11, 11),
-      month_hurdle_fn(matched_data$all_month12, 12),
+      month_hurdle_fn(matched_data$all_month1, 1, data = matched_data),
+      month_hurdle_fn(matched_data$all_month2, 2, data = matched_data),
+      month_hurdle_fn(matched_data$all_month3, 3, data = matched_data),
+      month_hurdle_fn(matched_data$all_month4, 4, data = matched_data),
+      month_hurdle_fn(matched_data$all_month5, 5, data = matched_data),
+      month_hurdle_fn(matched_data$all_month6, 6, data = matched_data),
+      month_hurdle_fn(matched_data$all_month7, 7, data = matched_data),
+      month_hurdle_fn(matched_data$all_month8, 8, data = matched_data),
+      month_hurdle_fn(matched_data$all_month9, 9, data = matched_data),
+      month_hurdle_fn(matched_data$all_month10, 10, data = matched_data),
+      month_hurdle_fn(matched_data$all_month11, 11, data = matched_data),
+      month_hurdle_fn(matched_data$all_month12, 12, data = matched_data),
 )
 
 all_results %>% 
       filter(rownames(.) %>% startsWith("count_")) %>% 
       write.csv(here("output", "st03_monthly_visits_crude_hurdle.csv"), 
+                row.names = T)
+
+# 2. Crude results: stratified by previous hospitalisation -----
+# COVID hospitalised people:
+hospital_crude_results <- bind_rows(
+      month_hurdle_fn(hospital$all_month1, 1, data = hospital),
+      month_hurdle_fn(hospital$all_month2, 2, data = hospital),
+      month_hurdle_fn(hospital$all_month3, 3, data = hospital),
+      month_hurdle_fn(hospital$all_month4, 4, data = hospital),
+      month_hurdle_fn(hospital$all_month5, 5, data = hospital),
+      month_hurdle_fn(hospital$all_month6, 6, data = hospital),
+      month_hurdle_fn(hospital$all_month7, 7, data = hospital),
+      month_hurdle_fn(hospital$all_month8, 8, data = hospital),
+      month_hurdle_fn(hospital$all_month9, 9, data = hospital),
+      month_hurdle_fn(hospital$all_month10, 10, data = hospital),
+      month_hurdle_fn(hospital$all_month11, 11, data = hospital),
+      month_hurdle_fn(hospital$all_month12, 12, data = hospital),
+)
+hospital_crude_results %>% 
+      filter(rownames(.) %>% startsWith("count_")) %>% 
+      write.csv(here("output", "st03_monthly_visits_crude_hurdle_hospital.csv"), 
+                row.names = T)
+
+# No hospitalisation due to COVID:
+no_hos_crude_results<- bind_rows(
+      month_hurdle_fn(non_hospital$all_month1, 1, data = non_hospital),
+      month_hurdle_fn(non_hospital$all_month2, 2, data = non_hospital),
+      month_hurdle_fn(non_hospital$all_month3, 3, data = non_hospital),
+      month_hurdle_fn(non_hospital$all_month4, 4, data = non_hospital),
+      month_hurdle_fn(non_hospital$all_month5, 5, data = non_hospital),
+      month_hurdle_fn(non_hospital$all_month6, 6, data = non_hospital),
+      month_hurdle_fn(non_hospital$all_month7, 7, data = non_hospital),
+      month_hurdle_fn(non_hospital$all_month8, 8, data = non_hospital),
+      month_hurdle_fn(non_hospital$all_month9, 9, data = non_hospital),
+      month_hurdle_fn(non_hospital$all_month10, 10, data = non_hospital),
+      month_hurdle_fn(non_hospital$all_month11, 11, data = non_hospital),
+      month_hurdle_fn(non_hospital$all_month12, 12, data = non_hospital),
+)
+no_hos_crude_results%>% 
+      filter(rownames(.) %>% startsWith("count_")) %>% 
+      write.csv(here("output", "st03_monthly_visits_crude_hurdle_non_hospital.csv"), 
                 row.names = T)
