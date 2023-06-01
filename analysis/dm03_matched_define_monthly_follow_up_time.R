f69f9e07008a96703b8d3207bc8e56fe24ae4fdc
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-06-01 17:47:26 +0100

    Fix the negative follow-up period issues and add NA

--- analysis/dm03_matched_define_monthly_follow_up_time.R
+++ analysis/dm03_matched_define_monthly_follow_up_time.R
@@ -9,126 +9,136 @@ source("analysis/dm01_matched_current_data.R")
 
 # Concept: ---------------------------
 # if index_date + days(month_number) <= end_date, 
-# which means we can follow them for a full month;
+# which means we can follow them for a full month; 
+#     codes:      time1 + 30*1 < time2 ~ 30,
+
 # if index_date + days(month_number) > end_date, 
-# which means people were censored earlier then the end of the month,
-# so that we can only follow them from the beginning of the month 
-# until the end_date
+# people were censored earlier then the end of the month,
+# so that we can only follow them from the beginning of the month (time1 + 30*n)
+# until the end_date (time2)  
+# codes:   
+# (time2 < (time1 + 30*1)) & (time2 - time1)>0 ~ (time2 - (time1 + 30*0))
+
+
+# Also, if the follow-up period is 0, then it should be coded as NA.
+# codes: time2-time1 <=0 ~ NA
 
 # key dates:index_date, end_date, and the monthly date 
 # ------------------------------------
 
 # First we need to change them into Date format:
-matched_data$index_date <- as.Date(matched_data$index_date)
-matched_data$end_date <- as.Date(matched_data$end_date)
+matched_data$time1 <- as.numeric(matched_data$index_date)
+matched_data$time2 <- as.numeric(matched_data$end_date)
 
 # Month 1:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m1 = ifelse(
-            index_date + days(30*1) <= end_date , 
-            30, 
-            as.numeric(end_date) - as.numeric(index_date)
+      mutate(follow_up_m1 = case_when(
+            time1 + 30*1 < time2 ~ 30,
+            (time2 < (time1 + 30*1)) & (time2 - (time1 + 30*(1-1)))>0 ~ 
+                  (time2 - (time1 + 30*(1-1))),
+            (time2 - (time1 + 30*(1-1))) <=0 ~ NA
             )
 )
 
 
 # Month 2:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m2 = ifelse(
-            index_date + days(30*2) < end_date , 
-            30, 
-            as.numeric(end_date) - as.numeric(index_date) - (30*(2-1))
+      mutate(follow_up_m2 = case_when(
+            time1 + 30*2 < time2 ~ 30,
+            (time2 < (time1 + 30*2)) & (time2 - (time1 + 30*(2-1)))>0 ~ (time2 - (time1 + 30*(2-1))),
+            (time2 - (time1 + 30*(2-1))) <=0 ~ NA
             )
 )
 
 # Month 3:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m3 = ifelse(
-            index_date + days(30*3) < end_date , 
-            30, 
-            as.numeric(end_date) - as.numeric(index_date) - (30*(3-1))
+      mutate(follow_up_m3 = case_when(
+            time1 + 30*3 < time2 ~ 30,
+            (time2 < (time1 + 30*3)) & (time2 - (time1 + 30*(3-1)))>0 ~ (time2 - (time1 + 30*(3-1))),
+            (time2 - (time1 + 30*(3-1))) <=0 ~ NA
       )
 )
+
 
 # Month 4:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m4 = ifelse(
-            index_date + days(30*4) < end_date , 
-            30, 
-            as.numeric(end_date) - as.numeric(index_date) - (30*(4-1))
+      mutate(follow_up_m4 = case_when(
+            time1 + 30*4 < time2 ~ 30,
+            (time2 < (time1 + 30*4)) & (time2 - (time1 + 30*(4-1)))>0 ~ (time2 - (time1 + 30*(4-1))),
+            (time2 - (time1 + 30*(4-1))) <=0 ~ NA
       )
 )
 
+
 # Month 5:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m5 = ifelse(
-            index_date + days(30*5) < end_date , 
-            30, 
-            as.numeric(end_date) - as.numeric(index_date) - (30*(5-1))
+      mutate(follow_up_m5 = case_when(
+            time1 + 30*5 < time2 ~ 30,
+            (time2 < (time1 + 30*5)) & (time2 - (time1 + 30*(5-1)))>0 ~ (time2 - (time1 + 30*(5-1))),
+            (time2 - (time1 + 30*(5-1))) <=0 ~ NA
       )
 )
 
 # Month 6:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m6 = ifelse(
-            index_date + days(30*6) < end_date , 
-            30, 
-            as.numeric(end_date) - as.numeric(index_date) - (30*(6-1))
+      mutate(follow_up_m6 = case_when(
+            time1 + 30*6 < time2 ~ 30,
+            (time2 < (time1 + 30*6)) & (time2 - (time1 + 30*(6-1)))>0 ~ (time2 - (time1 + 30*(6-1))),
+            (time2 - (time1 + 30*(6-1))) <=0 ~ NA
       )
 )
 
 # Month 7:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m7 = ifelse(
-            index_date + days(30*7) < end_date , 
-            30, 
-            as.numeric(end_date) - as.numeric(index_date) - (30*(7-1))
+      mutate(follow_up_m7 = case_when(
+            time1 + 30*7 < time2 ~ 30,
+            (time2 < (time1 + 30*7)) & (time2 - (time1 + 30*(7-1)))>0 ~ (time2 - (time1 + 30*(7-1))),
+            (time2 - (time1 + 30*(7-1))) <=0 ~ NA
       )
 )
 
 # Month 8:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m8 = ifelse(
-            index_date + days(30*8) < end_date , 
-            30, 
-            as.numeric(end_date) - as.numeric(index_date) - (30*(8-1))
+      mutate(follow_up_m8 = case_when(
+            time1 + 30*8 < time2 ~ 30,
+            (time2 < (time1 + 30*8)) & (time2 - (time1 + 30*(8-1)))>0 ~ (time2 - (time1 + 30*(8-1))),
+            (time2 - (time1 + 30*(8-1))) <=0 ~ NA
       )
 )
 
-
 # Month 9:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m9 = ifelse(
-            index_date + days(30*9) < end_date , 
-            30, 
-            as.numeric(end_date) - as.numeric(index_date) - (30*(9-1))
+      mutate(follow_up_m9 = case_when(
+            time1 + 30*9 < time2 ~ 30,
+            (time2 < (time1 + 30*9)) & (time2 - (time1 + 30*(9-1)))>0 ~ (time2 - (time1 + 30*(9-1))),
+            (time2 - (time1 + 30*(9-1))) <=0 ~ NA
       )
 )
 
 # Month 10:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m10 = ifelse(
-            index_date + days(30*10) < end_date , 
-            30, 
-            as.numeric(end_date) - as.numeric(index_date) - (30*(10-1))
+      mutate(follow_up_m10 = case_when(
+            time1 + 30*10 < time2 ~ 30,
+            (time2 < (time1 + 30*10)) & (time2 - (time1 + 30*(10-1)))>0 ~ (time2 - (time1 + 30*(10-1))),
+            (time2 - (time1 + 30*(10-1))) <=0 ~ NA
       )
 )
 
 
 # Month 11:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m11 = ifelse(
-            index_date + days(30*11) < end_date , 
-            30, 
-            as.numeric(end_date) - as.numeric(index_date) - (30*(11-1))
+      mutate(follow_up_m11 = case_when(
+            time1 + 30*11 < time2 ~ 30,
+            (time2 < (time1 + 30*11)) & (time2 - (time1 + 30*(11-1)))>0 ~ (time2 - (time1 + 30*(11-1))),
+            (time2 - (time1 + 30*(11-1))) <=0 ~ NA
       )
 )
 
 # Month 12:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m12 = ifelse(
-            index_date + days(30*12) < end_date , 
-            30, 
-            as.numeric(end_date) - as.numeric(index_date) - (30*(12-1))
+      mutate(follow_up_m12 = case_when(
+            time1 + 30*12 < time2 ~ 30,
+            (time2 < (time1 + 30*12)) & (time2 - (time1 + 30*(12-1)))>0 ~ (time2 - (time1 + 30*(12-1))),
+            (time2 - (time1 + 30*(12-1))) <=0 ~ NA
       )
 )
