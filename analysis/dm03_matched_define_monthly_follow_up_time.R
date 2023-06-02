2fbd293cfc67d3345cfeb3d6d336f1fee2564c0d
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-06-02 16:44:15 +0100

    try to change the follow-up time into numeric to fix the error message on server

--- analysis/dm03_matched_define_monthly_follow_up_time.R
+++ analysis/dm03_matched_define_monthly_follow_up_time.R
@@ -32,115 +32,115 @@ matched_data$time2 <- as.numeric(matched_data$end_date)
 
 # Month 1:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m1 = case_when(
+      mutate(follow_up_m1 = as.double(case_when(
             time1 + 30*1 < time2 ~ 30,
             (time2 < (time1 + 30*1)) & (time2 - (time1 + 30*(1-1)))>0 ~ 
                   (time2 - (time1 + 30*(1-1))),
             (time2 - (time1 + 30*(1-1))) <=0 ~ NA
-            )
+            ))
 )
 
 
 # Month 2:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m2 = case_when(
+      mutate(follow_up_m2 = as.double(case_when(
             time1 + 30*2 < time2 ~ 30,
             (time2 < (time1 + 30*2)) & (time2 - (time1 + 30*(2-1)))>0 ~ (time2 - (time1 + 30*(2-1))),
             (time2 - (time1 + 30*(2-1))) <=0 ~ NA
-            )
+            ))
 )
 
 # Month 3:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m3 = case_when(
+      mutate(follow_up_m3 = as.double(case_when(
             time1 + 30*3 < time2 ~ 30,
             (time2 < (time1 + 30*3)) & (time2 - (time1 + 30*(3-1)))>0 ~ (time2 - (time1 + 30*(3-1))),
             (time2 - (time1 + 30*(3-1))) <=0 ~ NA
-      )
+            ))
 )
 
 
 # Month 4:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m4 = case_when(
+      mutate(follow_up_m4 = as.double(case_when(
             time1 + 30*4 < time2 ~ 30,
             (time2 < (time1 + 30*4)) & (time2 - (time1 + 30*(4-1)))>0 ~ (time2 - (time1 + 30*(4-1))),
             (time2 - (time1 + 30*(4-1))) <=0 ~ NA
-      )
+      ))
 )
 
 
 # Month 5:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m5 = case_when(
+      mutate(follow_up_m5 = as.double(case_when(
             time1 + 30*5 < time2 ~ 30,
             (time2 < (time1 + 30*5)) & (time2 - (time1 + 30*(5-1)))>0 ~ (time2 - (time1 + 30*(5-1))),
             (time2 - (time1 + 30*(5-1))) <=0 ~ NA
-      )
+      ))
 )
 
 # Month 6:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m6 = case_when(
+      mutate(follow_up_m6 = as.double(case_when(
             time1 + 30*6 < time2 ~ 30,
             (time2 < (time1 + 30*6)) & (time2 - (time1 + 30*(6-1)))>0 ~ (time2 - (time1 + 30*(6-1))),
             (time2 - (time1 + 30*(6-1))) <=0 ~ NA
-      )
+      ))
 )
 
 # Month 7:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m7 = case_when(
+      mutate(follow_up_m7 = as.double(case_when(
             time1 + 30*7 < time2 ~ 30,
             (time2 < (time1 + 30*7)) & (time2 - (time1 + 30*(7-1)))>0 ~ (time2 - (time1 + 30*(7-1))),
             (time2 - (time1 + 30*(7-1))) <=0 ~ NA
-      )
+      ))
 )
 
 # Month 8:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m8 = case_when(
+      mutate(follow_up_m8 = as.double(case_when(
             time1 + 30*8 < time2 ~ 30,
             (time2 < (time1 + 30*8)) & (time2 - (time1 + 30*(8-1)))>0 ~ (time2 - (time1 + 30*(8-1))),
             (time2 - (time1 + 30*(8-1))) <=0 ~ NA
-      )
+      ))
 )
 
 # Month 9:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m9 = case_when(
+      mutate(follow_up_m9 = as.double(case_when(
             time1 + 30*9 < time2 ~ 30,
             (time2 < (time1 + 30*9)) & (time2 - (time1 + 30*(9-1)))>0 ~ (time2 - (time1 + 30*(9-1))),
             (time2 - (time1 + 30*(9-1))) <=0 ~ NA
-      )
+      ))
 )
 
 # Month 10:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m10 = case_when(
+      mutate(follow_up_m10 = as.double(case_when(
             time1 + 30*10 < time2 ~ 30,
             (time2 < (time1 + 30*10)) & (time2 - (time1 + 30*(10-1)))>0 ~ (time2 - (time1 + 30*(10-1))),
             (time2 - (time1 + 30*(10-1))) <=0 ~ NA
-      )
+      ))
 )
 
 
 # Month 11:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m11 = case_when(
+      mutate(follow_up_m11 = as.double(case_when(
             time1 + 30*11 < time2 ~ 30,
             (time2 < (time1 + 30*11)) & (time2 - (time1 + 30*(11-1)))>0 ~ (time2 - (time1 + 30*(11-1))),
             (time2 - (time1 + 30*(11-1))) <=0 ~ NA
-      )
+      ))
 )
 
 # Month 12:
 matched_data <- matched_data %>% 
-      mutate(follow_up_m12 = case_when(
+      mutate(follow_up_m12 = as.double(case_when(
             time1 + 30*12 < time2 ~ 30,
             (time2 < (time1 + 30*12)) & (time2 - (time1 + 30*(12-1)))>0 ~ (time2 - (time1 + 30*(12-1))),
             (time2 - (time1 + 30*(12-1))) <=0 ~ NA
-      )
+      ))
 )
 
 
