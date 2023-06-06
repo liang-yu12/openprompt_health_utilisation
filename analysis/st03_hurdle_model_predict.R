65c87adf93d6bfab5a7d1c30609143811fe0bf69
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-06-06 13:51:38 +0100

    Add the full adjustement model. IT won't run on dummy data because of some empty variables.

---
+++ analysis/st03_hurdle_model_predict.R
@@ -0,0 +1,203 @@
+# Load previous data management
+source("analysis/dm03_matched_define_monthly_follow_up_time.R")
+
+# For organising the outputs
+options(digits=2)
+
+# Crude hurdle model
+cumulative_visit_crude_fn <- function(visit, fu_time, data, month) {
+      
+      # Create a hurdle model
+      model <- hurdle(
+            visit ~ exposure + offset(fu_time) | age_cat + sex+ region,
+            data = data,
+            zero.dist = "binomial",
+            dist = "poisson"
+      )
+      
+      # because original dataset has NA, need to first exclude them first
+      # then add the predicted value back to the table. 
+      no_na <- filter(matched_data, !is.na(fu_time))
+      no_na$predict <- predict(model, na.action = na.exclude)
+      results <- no_na %>% # summarise the mean visit and sd.
+            group_by(exposure) %>% 
+            summarise(mean = mean(predict),
+                      sd = sd(predict)) %>% 
+            mutate(month = month) %>% relocate(month)
+      
+      return(results)
+}
+
+results_crude <- bind_rows(
+      cumulative_visit_crude_fn(visit = matched_data$all_month1,
+                                fu_time = matched_data$follow_up_m1,
+                                data = matched_data,
+                                month = 1),
+      cumulative_visit_crude_fn(visit = matched_data$all_month2,
+                                fu_time = matched_data$follow_up_m2,
+                                data = matched_data,
+                                month = 2),
+      cumulative_visit_crude_fn(visit = matched_data$all_month3,
+                                fu_time = matched_data$follow_up_m3,
+                                data = matched_data,
+                                month = 3),
+      cumulative_visit_crude_fn(visit = matched_data$all_month4,
+                                fu_time = matched_data$follow_up_m4,
+                                data = matched_data,
+                                month = 4),
+      cumulative_visit_crude_fn(visit = matched_data$all_month5,
+                                fu_time = matched_data$follow_up_m5,
+                                data = matched_data,
+                                month = 5),
+      cumulative_visit_crude_fn(visit = matched_data$all_month6,
+                                fu_time = matched_data$follow_up_m6,
+                                data = matched_data,
+                                month = 6),
+      cumulative_visit_crude_fn(visit = matched_data$all_month7,
+                                fu_time = matched_data$follow_up_m7,
+                                data = matched_data,
+                                month = 7),
+      cumulative_visit_crude_fn(visit = matched_data$all_month8,
+                                fu_time = matched_data$follow_up_m8,
+                                data = matched_data,
+                                month = 8),
+      cumulative_visit_crude_fn(visit = matched_data$all_month9,
+                                fu_time = matched_data$follow_up_m9,
+                                data = matched_data,
+                                month = 9),
+      cumulative_visit_crude_fn(visit = matched_data$all_month10,
+                                fu_time = matched_data$follow_up_m10,
+                                data = matched_data,
+                                month = 10),
+      cumulative_visit_crude_fn(visit = matched_data$all_month11,
+                                fu_time = matched_data$follow_up_m11,
+                                data = matched_data,
+                                month = 11),
+      cumulative_visit_crude_fn(visit = matched_data$all_month12,
+                                fu_time = matched_data$follow_up_m12,
+                                data = matched_data,
+                                month = 12)) %>% 
+      mutate(model = "Crude") %>% 
+      relocate(model)
+
+# B. Partially adjusted model
+cumulative_visit_partially_adj_fn <- function(visit, fu_time, data, month) {
+      
+      # Create a hurdle model
+      model <- hurdle(
+            visit ~ exposure + sex + age_cat + offset(fu_time) | age_cat + sex+ region,
+            data = data,
+            zero.dist = "binomial",
+            dist = "poisson"
+      )
+      
+      # because original dataset has NA, need to first exclude them first
+      # then add the predicted value back to the table. 
+      no_na <- filter(matched_data, !is.na(fu_time))
+      cov <- c("exposure","sex","age_cat")
+      no_na <- no_na[complete.cases(no_na[, cov]), ]
+      
+      no_na$predict <- predict(model, na.action = na.exclude)
+      results <- no_na %>% # summarise the mean visit and sd.
+            group_by(exposure) %>% 
+            summarise(mean = mean(predict),
+                      sd = sd(predict)) %>% 
+            mutate(month = month) %>% relocate(month)
+      
+      return(results)
+}
+
+results_part_adjusted <- bind_rows(
+      cumulative_visit_partially_adj_fn(visit = matched_data$all_month1,
+                                fu_time = matched_data$follow_up_m1,
+                                data = matched_data,
+                                month = 1),
+      cumulative_visit_partially_adj_fn(visit = matched_data$all_month2,
+                                fu_time = matched_data$follow_up_m2,
+                                data = matched_data,
+                                month = 2),
+      cumulative_visit_partially_adj_fn(visit = matched_data$all_month3,
+                                fu_time = matched_data$follow_up_m3,
+                                data = matched_data,
+                                month = 3),
+      cumulative_visit_partially_adj_fn(visit = matched_data$all_month4,
+                                fu_time = matched_data$follow_up_m4,
+                                data = matched_data,
+                                month = 4),
+      cumulative_visit_partially_adj_fn(visit = matched_data$all_month5,
+                                fu_time = matched_data$follow_up_m5,
+                                data = matched_data,
+                                month = 5),
+      cumulative_visit_partially_adj_fn(visit = matched_data$all_month6,
+                                fu_time = matched_data$follow_up_m6,
+                                data = matched_data,
+                                month = 6),
+      cumulative_visit_partially_adj_fn(visit = matched_data$all_month7,
+                                fu_time = matched_data$follow_up_m7,
+                                data = matched_data,
+                                month = 7),
+      cumulative_visit_partially_adj_fn(visit = matched_data$all_month8,
+                                fu_time = matched_data$follow_up_m8,
+                                data = matched_data,
+                                month = 8),
+      cumulative_visit_partially_adj_fn(visit = matched_data$all_month9,
+                                fu_time = matched_data$follow_up_m9,
+                                data = matched_data,
+                                month = 9),
+      cumulative_visit_partially_adj_fn(visit = matched_data$all_month10,
+                                fu_time = matched_data$follow_up_m10,
+                                data = matched_data,
+                                month = 10),
+      cumulative_visit_partially_adj_fn(visit = matched_data$all_month11,
+                                fu_time = matched_data$follow_up_m11,
+                                data = matched_data,
+                                month = 11),
+      cumulative_visit_partially_adj_fn(visit = matched_data$all_month12,
+                                fu_time = matched_data$follow_up_m12,
+                                data = matched_data,
+                                month = 12)) %>% 
+      mutate(model = "Partially adjusted") %>% 
+      relocate(model)
+
+
+
+# C. Fully adjusted model  ---------
+
+
+cumulative_visit_fully_adj_fn <- function(visit, fu_time, data, month) {
+      
+      # Create a hurdle model
+      model <- hurdle(
+            visit ~ exposure + sex + age_cat + cov_covid_vax_n_cat + bmi_cat + imd_q5 + ethnicity_6 + region + 
+                  number_comorbidities_cat + offset(fu_time) | age_cat + sex+ region,
+            data = data,
+            zero.dist = "binomial",
+            dist = "poisson"
+      )
+      
+      # because original dataset has NA, need to first exclude them first
+      # then add the predicted value back to the table. 
+      no_na <- filter(matched_data, !is.na(fu_time))
+      cov <- c("exposure","sex","age_cat", "cov_covid_vax_n_cat", "bmi_cat", "imd_q5", "ethnicity_6", "region",
+            "number_comorbidities_cat")
+      no_na <- no_na[complete.cases(no_na[, cov]), ]
+      
+      no_na$predict <- predict(model, na.action = na.exclude)
+      results <- no_na %>% # summarise the mean visit and sd.
+            group_by(exposure) %>% 
+            summarise(mean = mean(predict),
+                      sd = sd(predict)) %>% 
+            mutate(month = month) %>% relocate(month)
+      
+      return(results)
+}
+
+
+
+
+
+
+
+# Final output: ------
+results_hurdle <- bind_rows(crude, par_adj, full_adj)
+results_hurdle %>% write.csv(here("output", "st_03_result_monthly_visit_hurdle.csv"), row.names = F)
