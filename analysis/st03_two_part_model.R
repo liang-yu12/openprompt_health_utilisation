a6c482c8eb30fc530bd340d14dbdbad679143c27
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-06-07 16:06:16 +0100

    try twopm but not working.

--- analysis/st03_two_part_model.R
+++ analysis/st03_two_part_model.R
@@ -1,27 +1,10 @@
 # Load previous data management
-source("analysis/dm03_matched_define_monthly_follow_up_time.R")
+source("analysis/dm04_matched_estimale_costs.R")
 
 # test running the model 
 month_1 <- tpm(
-      all_month1 ~ exposure + sex + age_cat + ethnicity_6 + bmi_cat + imd_q5,
+      gp_cost_m1 ~ exposure,
       data = matched_data,
-      link_part1 = "logit",
-      family_part2 = "poisson"
+      link_part1 = "logit", family_part2 = Gamma(link = "log")
 )
 
-matched_data %<>% as_tibble()
-
-t <- c("exposure", "sex", "age_cat", "ethnicity_6", "bmi_cat", "imd_q5","follow_up_m1","all_month1")
-map(matched_data[,t], summary)
-
-test_data <- matched_data[1:20,]
-
-
-month_1 <- tpm(all_month1~exposure, 
-               data = matched_data,
-               link_part1 = "logit",
-               family_part2 = poisson
-)
-
-# noted that tpm won't run if there is NA in the data; 
-# will need to manually exclude NA
