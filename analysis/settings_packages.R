895ce29ea83b0e478395cec461d8ca0b0026f5f0
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-06-14 10:04:21 +0100

    use readr to sort the column type issue (more straightforward)

--- analysis/settings_packages.R
+++ analysis/settings_packages.R
@@ -3,6 +3,6 @@
 packages <- c("data.table", "lubridate", "markdown", "forcats", "Hmisc",
               "broom", "dplyr", "finalfit", "forcats", "ggplot2", "ggpubr",
               "MASS", "stringr", "here", "gtools", "rmarkdown",
-              "knitr", "pscl","twopartm", "tidyr")
+              "knitr", "pscl","twopartm", "tidyr", "readr")
 
 lapply(packages, require, character.only = TRUE)
