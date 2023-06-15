227ca43655f5a37f341a172dbc6313a3b0f305d5
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-06-15 16:46:09 +0100

    modify the medication code source

--- analysis/covariates_medications.py
+++ analysis/covariates_medications.py
@@ -3,12 +3,10 @@
 # local variables for defining covariates
 from datetime import date
 from databuilder.ehrql import Dataset, days, years,  case, when
-from databuilder.tables.beta.tpp import (
-    practice_registrations, clinical_events, medications,
-)
+from ehrql.tables.beta.core import medications
 from databuilder.codes import CTV3Code, DMDCode, ICD10Code, SNOMEDCTCode
 import codelists
-from codelists import lc_codelists_combined
+
 from variables import (
     hospitalisation_diagnosis_matches,
     clinical_ctv3_matches, 
@@ -17,7 +15,6 @@ from variables import (
 
 study_start_date = date(2020, 11, 1)
 study_end_date = date(2023, 3, 31)
-
 
 prescriptions = (medications
     .where(date >= study_start_date)
