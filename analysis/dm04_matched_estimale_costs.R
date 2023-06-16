248b58d1c0d508a4f325dd4e4a91e3b8e0a32d3c
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-06-16 11:03:52 +0100

    Fix the error outcome var

--- analysis/dm04_matched_estimale_costs.R
+++ analysis/dm04_matched_estimale_costs.R
@@ -5,16 +5,16 @@ source("analysis/dm03_matched_define_monthly_follow_up_time.R")
 # ref: https://doi.org/10.22024/UniKent%2F01.02.100519
 # £42 per consultation
 matched_data <- matched_data %>%
-      mutate(gp_cost_m1 = gp_ac_visit_m1 * 42,
-             gp_cost_m2 = gp_ac_visit_m2 * 42,
-             gp_cost_m3 = gp_ac_visit_m3 * 42,
-             gp_cost_m4 = gp_ac_visit_m4 * 42,
-             gp_cost_m5 = gp_ac_visit_m5 * 42,
-             gp_cost_m6 = gp_ac_visit_m6 * 42,
-             gp_cost_m7 = gp_ac_visit_m7 * 42,
-             gp_cost_m8 = gp_ac_visit_m8 * 42,
-             gp_cost_m9 = gp_ac_visit_m9 * 42,
-             gp_cost_m10 = gp_ac_visit_m10 * 42,
-             gp_cost_m11 = gp_ac_visit_m11 * 42,
-             gp_cost_m12 = gp_ac_visit_m12 * 42)
+      mutate(gp_cost_m1 = gp_visit_m1 * 42,
+             gp_cost_m2 = gp_visit_m2 * 42,
+             gp_cost_m3 = gp_visit_m3 * 42,
+             gp_cost_m4 = gp_visit_m4 * 42,
+             gp_cost_m5 = gp_visit_m5 * 42,
+             gp_cost_m6 = gp_visit_m6 * 42,
+             gp_cost_m7 = gp_visit_m7 * 42,
+             gp_cost_m8 = gp_visit_m8 * 42,
+             gp_cost_m9 = gp_visit_m9 * 42,
+             gp_cost_m10 = gp_visit_m10 * 42,
+             gp_cost_m11 = gp_visit_m11 * 42,
+             gp_cost_m12 = gp_visit_m12 * 42)
 
