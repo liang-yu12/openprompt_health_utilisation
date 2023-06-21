f56a02db6281599f34a812220824812632c3eb61
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-06-21 16:54:12 +0100

    Added model selection codes and results codes

--- analysis/st05_did_model.R
+++ analysis/st05_did_model.R
@@ -6,29 +6,44 @@ source("analysis/dm02_matched_hx_data.R")
 # For organising the outputs
 options(digits=2)
 
-# 0. model selection: 
-model_nb <- glm.nb(all_month1 ~ exposure*time + offset(fu_time_m1), 
+# 0. model selection: Use 6 month to test models 
+model_nb <- glm.nb(all_month6 ~ exposure*time + offset(fu_time_m6), 
              data = hx_matched_data) 
-model_poisson <- glm(all_month1 ~ exposure*time + offset(fu_time_m1), 
+model_poisson <- glm(all_month6 ~ exposure*time + offset(fu_time_m6), 
        data = hx_matched_data, family = "poisson") 
 
 
-model_nb_adj <- glm.nb(all_month1 ~ exposure*time + age_cat+ sex+ bmi_cat+
+model_nb_adj <- glm.nb(all_month6 ~ exposure*time + age_cat+ sex+ bmi_cat+
                              ethnicity_6+ region+imd_q5+ number_comorbidities_cat+
-                             offset(fu_time_m1), 
+                             offset(fu_time_m6), 
                    data = hx_matched_data) 
-model_poisson_adj <- glm(all_month1 ~ exposure*time + age_cat+ sex+ bmi_cat+
+model_poisson_adj <- glm(all_month6 ~ exposure*time + age_cat+ sex+ bmi_cat+
                                ethnicity_6+ region+imd_q5+ number_comorbidities_cat+
-                               offset(fu_time_m1), 
+                               offset(fu_time_m6), 
                      data = hx_matched_data, family = "poisson") 
 
+# compare models:
+bind_rows(
+      model_nb %>% glance() %>% mutate(model = "Negative binomial - crude"),
+      model_poisson %>% glance() %>% mutate(model = "Poisson - crude"),
+      model_nb_adj %>% glance() %>% mutate(model = "Negative binomial - adjusted"),
+      model_poisson_adj %>% glance() %>% mutate(model = "Poisson - adjusted")
+      ) %>% 
+      write.csv(here("output", "model_comparison.csv"), row.names = F)
 
-
-aic_compare <- data.frame(
-      nb_model = model_nb$aic,
-      adjusted_nb_model = model_nb_adj$aic,
-      poisson_model = model_poisson$aic,
-      adjusted_poisson_model = model_poisson_adj$aic
-)
-
-aic_compare %>% write.csv(here("output", "model_compare_aic.csv"), row.names = F)
+# 1. print the output
+bind_rows(
+      (model_nb %>% 
+             tidy(conf.int = T, conf.level = 0.95, exponentiate = T) %>% 
+      mutate(model = "Negative binomial - crude")),
+      (model_nb_adj %>% 
+             tidy(conf.int = T, conf.level = 0.95, exponentiate = T) %>% 
+      mutate(model = "Negative binomial - adjusted")),
+      (model_poisson %>% 
+            tidy(conf.int = T, conf.level = 0.95, exponentiate = T) %>% 
+            mutate(model = "Poisson - crude")),
+      (model_poisson_adj %>% 
+             tidy(conf.int = T, conf.level = 0.95, exponentiate = T) %>% 
+             mutate(model = "Poisson - adjusted"))
+      ) %>% 
+      write.csv(here("output", "results_models.csv"), row.names = F)
