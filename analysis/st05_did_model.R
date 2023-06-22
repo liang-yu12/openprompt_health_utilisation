e780af8af4b93a34da9fd6d6d9e07b5631cd2556
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-06-22 14:10:15 +0100

    check dispersion

--- analysis/st05_did_model.R
+++ analysis/st05_did_model.R
@@ -5,7 +5,7 @@ source("analysis/dm02_matched_hx_data.R")
 
 # For organising the outputs
 options(digits=2)
-hx_matched_data %>%
+check_zero <- hx_matched_data %>%
       group_by(exposure) %>%
       summarise(
             mean1 = mean(all_month1),
@@ -32,46 +32,38 @@ hx_matched_data %>%
             zero11 = sum(all_month11 == 0),
             mean12 = mean(all_month12),
             zero12 = sum(all_month12 == 0)
-      ) %>% write.csv(here("output", "model_comparison.csv"), row.names = F)
+      ) 
+
+# 0. model selection: Use 6 month to test models
+model_poisson <- glm(all_month6 ~ exposure*time + offset(fu_time_m6),
+       data = hx_matched_data, family = "poisson")
+
+check_zero$dispersion <- summary(model_poisson)$deviance / df.residual(model_poisson)
+
+check_zero %>% write.csv(here("output", "model_comparison.csv"), row.names = F)
 # 
-# # 0. model selection: Use 6 month to test models 
-# model_nb <- glm.nb(all_month6 ~ exposure*time + offset(fu_time_m6), 
-#              data = hx_matched_data) 
-# model_poisson <- glm(all_month6 ~ exposure*time + offset(fu_time_m6), 
-#        data = hx_matched_data, family = "poisson") 
-# 
-# 
-# model_nb_adj <- glm.nb(all_month6 ~ exposure*time + age_cat+ sex+ bmi_cat+
-#                              ethnicity_6+ region+imd_q5+ number_comorbidities_cat+
-#                              offset(fu_time_m6), 
-#                    data = hx_matched_data) 
-# model_poisson_adj <- glm(all_month6 ~ exposure*time + age_cat+ sex+ bmi_cat+
-#                                ethnicity_6+ region+imd_q5+ number_comorbidities_cat+
-#                                offset(fu_time_m6), 
-#                      data = hx_matched_data, family = "poisson") 
-# 
+# model_nb <- glm.nb(all_month6 ~ exposure*time + offset(fu_time_m6),
+#                    data = hx_matched_data)
 # # compare models:
 # bind_rows(
 #       model_nb %>% glance() %>% mutate(model = "Negative binomial - crude"),
-#       model_poisson %>% glance() %>% mutate(model = "Poisson - crude"),
-#       model_nb_adj %>% glance() %>% mutate(model = "Negative binomial - adjusted"),
-#       model_poisson_adj %>% glance() %>% mutate(model = "Poisson - adjusted")
-#       ) %>% 
+#       model_poisson %>% glance() %>% mutate(model = "Poisson - crude")
+#       ) %>%
 #       write.csv(here("output", "model_comparison.csv"), row.names = F)
 # 
 # # 1. print the output
 # bind_rows(
-#       (model_nb %>% 
-#              tidy(conf.int = T, conf.level = 0.95, exponentiate = T) %>% 
+#       (model_nb %>%
+#              tidy(conf.int = T, conf.level = 0.95, exponentiate = T) %>%
 #       mutate(model = "Negative binomial - crude")),
-#       (model_nb_adj %>% 
-#              tidy(conf.int = T, conf.level = 0.95, exponentiate = T) %>% 
+#       (model_nb_adj %>%
+#              tidy(conf.int = T, conf.level = 0.95, exponentiate = T) %>%
 #       mutate(model = "Negative binomial - adjusted")),
-#       (model_poisson %>% 
-#             tidy(conf.int = T, conf.level = 0.95, exponentiate = T) %>% 
+#       (model_poisson %>%
+#             tidy(conf.int = T, conf.level = 0.95, exponentiate = T) %>%
 #             mutate(model = "Poisson - crude")),
-#       (model_poisson_adj %>% 
-#              tidy(conf.int = T, conf.level = 0.95, exponentiate = T) %>% 
+#       (model_poisson_adj %>%
+#              tidy(conf.int = T, conf.level = 0.95, exponentiate = T) %>%
 #              mutate(model = "Poisson - adjusted"))
-#       ) %>% 
+#       ) %>%
 #       write.csv(here("output", "results_models.csv"), row.names = F)
