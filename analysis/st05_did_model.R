332fa15f67e230d481fc0d6028f8daf14e5a323c
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-06-22 14:33:22 +0100

    Add the output for checking over dispersion

--- analysis/st05_did_model.R
+++ analysis/st05_did_model.R
@@ -5,32 +5,20 @@ source("analysis/dm02_matched_hx_data.R")
 
 # For organising the outputs
 options(digits=2)
-check_zero <- hx_matched_data %>%
+check_dispersion <- hx_matched_data %>%
       group_by(exposure) %>%
       summarise(
-            mean1 = mean(all_month1),
             zero1 = sum(all_month1 == 0),
-            mean2 = mean(all_month2),
             zero2 = sum(all_month2 == 0),
-            mean3 = mean(all_month3),
             zero3 = sum(all_month3 == 0),
-            mean4 = mean(all_month4),
             zero4 = sum(all_month4 == 0),
-            mean5 = mean(all_month5),
             zero5 = sum(all_month5 == 0),
-            mean6 = mean(all_month6),
             zero6 = sum(all_month6 == 0),
-            mean7 = mean(all_month7),
             zero7 = sum(all_month7 == 0),
-            mean8 = mean(all_month8),
             zero8 = sum(all_month8 == 0),
-            mean9 = mean(all_month9),
             zero9 = sum(all_month9 == 0),
-            mean10 = mean(all_month10),
             zero10 = sum(all_month10 == 0),
-            mean11 = mean(all_month11),
             zero11 = sum(all_month11 == 0),
-            mean12 = mean(all_month12),
             zero12 = sum(all_month12 == 0)
       ) 
 
@@ -38,12 +26,13 @@ check_zero <- hx_matched_data %>%
 model_poisson <- glm(all_month6 ~ exposure*time + offset(fu_time_m6),
        data = hx_matched_data, family = "poisson")
 
-check_zero$dispersion <- summary(model_poisson)$deviance / df.residual(model_poisson)
+check_dispersion$dispersion <- summary(model_poisson)$deviance / df.residual(model_poisson)
 
-check_zero %>% write.csv(here("output", "model_comparison.csv"), row.names = F)
-# 
-# model_nb <- glm.nb(all_month6 ~ exposure*time + offset(fu_time_m6),
-#                    data = hx_matched_data)
+check_dispersion %>% write.csv(here("output", "sup_st01_check_dispersion_poisson.csv"), row.names = F)
+
+
+
+
 # # compare models:
 # bind_rows(
 #       model_nb %>% glance() %>% mutate(model = "Negative binomial - crude"),
