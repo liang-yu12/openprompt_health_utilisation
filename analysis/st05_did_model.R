94550899908f4fc0fdc2bfb46109c0f7816cb1d6
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-06-22 17:11:17 +0100

    Apply the poisson model

--- analysis/st05_did_model.R
+++ analysis/st05_did_model.R
@@ -21,20 +21,26 @@ bind_rows(
       mutate(dispersion = deviance/df.residual) %>% 
       write.csv(here("output", "sup_st01_model_compare.csv"), row.names = F)
 
-# 
-# # 1. print the output
-# bind_rows(
-#       (model_nb %>%
-#              tidy(conf.int = T, conf.level = 0.95, exponentiate = T) %>%
-#       mutate(model = "Negative binomial - crude")),
-#       (model_nb_adj %>%
-#              tidy(conf.int = T, conf.level = 0.95, exponentiate = T) %>%
-#       mutate(model = "Negative binomial - adjusted")),
-#       (model_poisson %>%
-#             tidy(conf.int = T, conf.level = 0.95, exponentiate = T) %>%
-#             mutate(model = "Poisson - crude")),
-#       (model_poisson_adj %>%
-#              tidy(conf.int = T, conf.level = 0.95, exponentiate = T) %>%
-#              mutate(model = "Poisson - adjusted"))
-#       ) %>%
-#       write.csv(here("output", "results_models.csv"), row.names = F)
+# Poisson functions for all models: 
+
+did_poisson_crude_fn <- function(all_vist, fu_time){
+      glm(all_vist ~ exposure*time + offset(log(fu_time)),
+          data = hx_matched_data, family = "poisson")
+}
+
+# Apply the function
+month_1 <- did_poisson_fn(hx_matched_data$all_month1, hx_matched_data$fu_time_m1)
+month_2 <- did_poisson_fn(hx_matched_data$all_month2, hx_matched_data$fu_time_m2)
+month_3 <- did_poisson_fn(hx_matched_data$all_month3, hx_matched_data$fu_time_m3)
+month_4 <- did_poisson_fn(hx_matched_data$all_month4, hx_matched_data$fu_time_m4)
+month_5 <- did_poisson_fn(hx_matched_data$all_month5, hx_matched_data$fu_time_m5)
+month_6 <- did_poisson_fn(hx_matched_data$all_month6, hx_matched_data$fu_time_m6)
+month_7 <- did_poisson_fn(hx_matched_data$all_month7, hx_matched_data$fu_time_m7)
+month_8 <- did_poisson_fn(hx_matched_data$all_month8, hx_matched_data$fu_time_m8)
+month_9 <- did_poisson_fn(hx_matched_data$all_month9, hx_matched_data$fu_time_m9)
+month_10 <- did_poisson_fn(hx_matched_data$all_month10, hx_matched_data$fu_time_m10)
+month_11 <- did_poisson_fn(hx_matched_data$all_month11, hx_matched_data$fu_time_m11)
+month_12 <- did_poisson_fn(hx_matched_data$all_month12, hx_matched_data$fu_time_m12)
+
+
+# Obtain the fitted value:
