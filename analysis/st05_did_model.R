e207968a54dd711673234f5ad94b402e21ea3a5a
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-06-22 16:35:04 +0100

    Fix the model by adding log(fu). Compare models

--- analysis/st05_did_model.R
+++ analysis/st05_did_model.R
@@ -3,44 +3,24 @@
 # Load previous data management
 source("analysis/dm02_matched_hx_data.R")
 
-# For organising the outputs
-options(digits=2)
-check_dispersion <- hx_matched_data %>%
-      group_by(exposure) %>%
-      summarise(
-            zero1 = sum(all_month1 == 0),
-            zero2 = sum(all_month2 == 0),
-            zero3 = sum(all_month3 == 0),
-            zero4 = sum(all_month4 == 0),
-            zero5 = sum(all_month5 == 0),
-            zero6 = sum(all_month6 == 0),
-            zero7 = sum(all_month7 == 0),
-            zero8 = sum(all_month8 == 0),
-            zero9 = sum(all_month9 == 0),
-            zero10 = sum(all_month10 == 0),
-            zero11 = sum(all_month11 == 0),
-            zero12 = sum(all_month12 == 0)
-      ) 
 
 # 0. model selection: Use 6 month to test models
-model_poisson <- glm(all_month6 ~ exposure*time + offset(fu_time_m6),
+model_poisson <- glm(all_month6 ~ exposure*time + offset(log(fu_time_m6)),
        data = hx_matched_data, family = "poisson")
-
-check_dispersion$dispersion <- summary(model_poisson)$deviance / df.residual(model_poisson)
-
-check_dispersion %>% write.csv(here("output", "sup_st01_check_dispersion_poisson.csv"), row.names = F)
-
 # Use negative binomial model: 
-glm.nb(all_month6 ~ exposure*time + offset(fu_time_m6),
-       data = hx_matched_data) %>% tidy() %>% 
-      write.csv(here("output", "results_models.csv"), row.names = F)
+model_nb <- glm.nb(all_month6 ~ exposure*time + offset(log(fu_time_m6)),
+       data = hx_matched_data) 
 
-# # compare models:
-# bind_rows(
-#       model_nb %>% glance() %>% mutate(model = "Negative binomial - crude"),
-#       model_poisson %>% glance() %>% mutate(model = "Poisson - crude")
-#       ) %>%
-#       write.csv(here("output", "model_comparison.csv"), row.names = F)
+# compare models:
+bind_rows(
+      (model_nb %>% glance() %>% dplyr::select(AIC, BIC, deviance, df.residual) %>% 
+            mutate(model = "Negative binomial - crude")),
+      (model_poisson %>% glance()  %>% dplyr::select(AIC, BIC, deviance, df.residual) %>% 
+            mutate(model = "Poisson - crude"))
+      ) %>% relocate(model) %>% 
+      mutate(dispersion = deviance/df.residual) %>% 
+      write.csv(here("output", "sup_st01_model_compare.csv"), row.names = F)
+
 # 
 # # 1. print the output
 # bind_rows(
