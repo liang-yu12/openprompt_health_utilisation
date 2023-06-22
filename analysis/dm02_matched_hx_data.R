ae2db7abcd3f1972cc526603e0b503ad4a5fc63b
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-06-22 10:52:47 +0100

    Fix the as Date issue

--- analysis/dm02_matched_hx_data.R
+++ analysis/dm02_matched_hx_data.R
@@ -73,7 +73,7 @@ now_exp <- now_exp %>% mutate(
             end_date = pmin(as.numeric(end_death), 
                             as.numeric(end_deregist), 
                             as.numeric(end_lc_cure), 
-                            as.Date.numeric("2023-01-31", origin = "1970-01-01"), na.rm = T)
+                            as.numeric(as.Date("2023-01-31", origin = "1970-01-01")), na.rm = T)
             )
 ### calculate the follow-up time for each month:
 now_exp <- now_exp %>% 
@@ -186,7 +186,7 @@ now_com <- now_com %>% mutate(
       end_date = pmin(as.numeric(end_death), 
                       as.numeric(end_deregist), 
                       as.numeric(end_lc_cure), 
-                      as.Date.numeric("2023-01-31", origin = "1970-01-01"), na.rm = T)
+                      as.numeric(as.Date("2023-01-31", origin = "1970-01-01")), na.rm = T)
 )
 
 
