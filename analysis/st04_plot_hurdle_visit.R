e429ffa02c79e61c8e92932b8725e472a38592f9
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-06-26 16:52:55 +0100

    add error bars

--- analysis/st04_plot_hurdle_visit.R
+++ analysis/st04_plot_hurdle_visit.R
@@ -9,6 +9,7 @@ crude <- all_visit %>% filter(model == "Crude") %>%
       ggplot(aes(x = month, y = mean, group = exposure)) +
       geom_line(aes(color=exposure)) +
       geom_point(aes(color=exposure)) +
+      geom_errorbar(aes(ymin = (mean-sd), ymax = (mean+sd)), width=0.2) + 
       ggtitle("Crude") + theme(legend.title = element_blank()) +
       scale_x_continuous("Months after long COVID diagnosis", breaks = 1:12) +
       scale_y_continuous("Average healthcare utilisation")
@@ -24,7 +25,8 @@ partially <- all_visit %>% filter(model == "Partially adjusted") %>%
       ggplot(aes(x = month, y = mean, group = exposure)) +
       ggtitle("Partially adjuested") + theme(legend.title = element_blank()) +
       geom_line(aes(color=exposure))+
-      geom_point(aes(color=exposure))
+      geom_point(aes(color=exposure)) + 
+      geom_errorbar(aes(ymin = (mean-sd), ymax = (mean+sd)), width=0.2)
 
 
 ggsave(partially, 
@@ -37,7 +39,8 @@ full <- all_visit %>% filter(model == "Fully adjusted") %>%
       ggplot(aes(x = month, y = mean, group = exposure)) +
       ggtitle("Fully adjusted") + theme(legend.title = element_blank()) +
       geom_line(aes(color=exposure))+
-      geom_point(aes(color=exposure))
+      geom_point(aes(color=exposure))+ 
+      geom_errorbar(aes(ymin = (mean-sd), ymax = (mean+sd)), width=0.2)
 
 ggsave(full, 
        filename = "st_04_full_adj_healthcare_visit.png",
