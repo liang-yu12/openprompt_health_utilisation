9c00637eed8c83c08c99583a7db44bdd26686b3f
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-07-12 16:55:27 +0100

    Revise the hurdle model codes using the new approach

--- analysis/st03_hurdle_model_rate_ratio.R
+++ analysis/st03_hurdle_model_rate_ratio.R
@@ -28,13 +28,11 @@ data.frame(
 
 
 
-
-
 # Crude Hurdle model function:
-month_hurdle_fn <- function(month, n, data, fu_time) {
+month_hurdle_fn <- function( n, data) {
       # Fit a hurdle model to the data.
       model <- hurdle(
-            month ~ exposure + offset(log(fu_time)) | age_cat + sex+ region,
+            monthly_visits ~ exposure + offset(log(follow_up_time)) | age_cat + sex+ region,
             data = data,
             zero.dist = "binomial",
             dist = "negbin"
@@ -53,43 +51,53 @@ month_hurdle_fn <- function(month, n, data, fu_time) {
       results <- cbind(coef_results, results_ci)
 
       results$month <- n
-
+      results <- results %>% rownames_to_column("stat_name") %>% 
+            filter(str_detect(stat_name, "count_exposure"))
       # Return the results.
       return(results)
 }
 
 
+
+matched_data_ts[matched_data_ts$month ==1,]
+
+
+month_hurdle_fn(1,matched_data_ts[matched_data_ts$month ==1,]) 
+month_hurdle_fn(1,matched_data_ts[matched_data_ts$month ==1:2,]) 
+
 # A. Crude results: overall ----
 all_results <- bind_rows(
-      month_hurdle_fn(matched_data$all_month1, 1, data = matched_data, matched_data$follow_up_m1),
-      month_hurdle_fn(matched_data$all_month2, 2, data = matched_data, matched_data$follow_up_m2),
-      month_hurdle_fn(matched_data$all_month3, 3, data = matched_data, matched_data$follow_up_m3),
-      month_hurdle_fn(matched_data$all_month4, 4, data = matched_data, matched_data$follow_up_m4),
-      month_hurdle_fn(matched_data$all_month5, 5, data = matched_data, matched_data$follow_up_m5),
-      month_hurdle_fn(matched_data$all_month6, 6, data = matched_data, matched_data$follow_up_m6),
-      month_hurdle_fn(matched_data$all_month7, 7, data = matched_data, matched_data$follow_up_m7),
-      month_hurdle_fn(matched_data$all_month8, 8, data = matched_data, matched_data$follow_up_m8),
-      month_hurdle_fn(matched_data$all_month9, 9, data = matched_data, matched_data$follow_up_m9),
-      month_hurdle_fn(matched_data$all_month10, 10, data = matched_data, matched_data$follow_up_m10),
-      month_hurdle_fn(matched_data$all_month11, 11, data = matched_data, matched_data$follow_up_m11),
-      month_hurdle_fn(matched_data$all_month12, 12, data = matched_data, matched_data$follow_up_m12),
+      month_hurdle_fn(1, matched_data_ts[matched_data_ts$month == 1,]), 
+      month_hurdle_fn(2, matched_data_ts[matched_data_ts$month == 1:2,]),
+      month_hurdle_fn(3, matched_data_ts[matched_data_ts$month == 1:3,]), 
+      month_hurdle_fn(4, matched_data_ts[matched_data_ts$month == 1:4,]),
+      month_hurdle_fn(5, matched_data_ts[matched_data_ts$month == 1:5,]),
+      month_hurdle_fn(6, matched_data_ts[matched_data_ts$month == 1:6,]),
+      month_hurdle_fn(7, matched_data_ts[matched_data_ts$month == 1:7,]),
+      month_hurdle_fn(8, matched_data_ts[matched_data_ts$month == 1:8,]),
+      month_hurdle_fn(9, matched_data_ts[matched_data_ts$month == 1:9,]),
+      month_hurdle_fn(10, matched_data_ts[matched_data_ts$month == 1:10,]),
+      month_hurdle_fn(11, matched_data_ts[matched_data_ts$month == 1:11,]),
+      month_hurdle_fn(12, matched_data_ts[matched_data_ts$month == 1:12,])
 )
+
 
 # organise the output: keep only the exposure, and label the model.
 
 crude <- all_results %>%
-      filter(rownames(.) %>% startsWith("count_exposure")) %>%
       mutate(model = "Crude") %>%
-      relocate(model) %>% relocate(month, .after = model) %>% tibble::remove_rownames()
+      relocate(model) %>% relocate(month, .after = model) %>%
+      tibble::remove_rownames() %>% 
+      dplyr::select("model", "month", "Coefficients", "Lower CI","Upper CI")
 
 
 # B. Adjusted model
 
 # Partially adjusted Hurdle model function:
-month_adj_hurdle_fn <- function(month, n, data, fu_time) {
+month_adj_hurdle_fn <- function(n, data) {
       # Fit a hurdle model to the data.
       model <- hurdle(
-            month ~ exposure + age_cat + sex + offset(log(fu_time))| age_cat + sex+ region,
+            monthly_visits ~ exposure + age_cat + sex + offset(log(follow_up_time))| age_cat + sex+ region,
             data = data,
             zero.dist = "binomial",
             dist = "negbin"
@@ -108,40 +116,42 @@ month_adj_hurdle_fn <- function(month, n, data, fu_time) {
       results <- cbind(coef_results, results_ci)
 
       results$month <- n
-
+      results <- results %>% rownames_to_column("stat_name") %>% 
+            filter(str_detect(stat_name, "count_exposure"))
       # Return the results.
       return(results)
 }
 
 # B. Adjusted results: overall ---------
 all_par_adj_results <- bind_rows(
-      month_adj_hurdle_fn(matched_data$all_month1, 1, data = matched_data, matched_data$follow_up_m1),
-      month_adj_hurdle_fn(matched_data$all_month2, 2, data = matched_data, matched_data$follow_up_m2),
-      month_adj_hurdle_fn(matched_data$all_month3, 3, data = matched_data, matched_data$follow_up_m3),
-      month_adj_hurdle_fn(matched_data$all_month4, 4, data = matched_data, matched_data$follow_up_m4),
-      month_adj_hurdle_fn(matched_data$all_month5, 5, data = matched_data, matched_data$follow_up_m5),
-      month_adj_hurdle_fn(matched_data$all_month6, 6, data = matched_data, matched_data$follow_up_m6),
-      month_adj_hurdle_fn(matched_data$all_month7, 7, data = matched_data, matched_data$follow_up_m7),
-      month_adj_hurdle_fn(matched_data$all_month8, 8, data = matched_data, matched_data$follow_up_m8),
-      month_adj_hurdle_fn(matched_data$all_month9, 9, data = matched_data, matched_data$follow_up_m9),
-      month_adj_hurdle_fn(matched_data$all_month10, 10, data = matched_data, matched_data$follow_up_m10),
-      month_adj_hurdle_fn(matched_data$all_month11, 11, data = matched_data, matched_data$follow_up_m11),
-      month_adj_hurdle_fn(matched_data$all_month12, 12, data = matched_data, matched_data$follow_up_m12),
+      month_adj_hurdle_fn(1, matched_data_ts[matched_data_ts$month == 1,]), 
+      month_adj_hurdle_fn(2, matched_data_ts[matched_data_ts$month == 1:2,]),
+      month_adj_hurdle_fn(3, matched_data_ts[matched_data_ts$month == 1:3,]), 
+      month_adj_hurdle_fn(4, matched_data_ts[matched_data_ts$month == 1:4,]),
+      month_adj_hurdle_fn(5, matched_data_ts[matched_data_ts$month == 1:5,]),
+      month_adj_hurdle_fn(6, matched_data_ts[matched_data_ts$month == 1:6,]),
+      month_adj_hurdle_fn(7, matched_data_ts[matched_data_ts$month == 1:7,]),
+      month_adj_hurdle_fn(8, matched_data_ts[matched_data_ts$month == 1:8,]),
+      month_adj_hurdle_fn(9, matched_data_ts[matched_data_ts$month == 1:9,]),
+      month_adj_hurdle_fn(10, matched_data_ts[matched_data_ts$month == 1:10,]),
+      month_adj_hurdle_fn(11, matched_data_ts[matched_data_ts$month == 1:11,]),
+      month_adj_hurdle_fn(12, matched_data_ts[matched_data_ts$month == 1:12,])
 )
 
 
 par_adj <- all_par_adj_results %>%
-      filter(rownames(.) %>% startsWith("count_exposure")) %>%
       mutate(model = "Partially adjusted") %>%
-      relocate(model) %>% relocate(month, .after = model) %>% tibble::remove_rownames()
+      relocate(model) %>% relocate(month, .after = model) %>%
+      tibble::remove_rownames() %>% 
+      dplyr::select("model", "month", "Coefficients", "Lower CI","Upper CI")
 
 # C. Fully adjusted model  ---------
 # Fully adjusted model Hurdle model function:
-month_full_hurdle_fn <- function(month, n, data, fu_time) {
+month_full_hurdle_fn <- function(n, data) {
       # Fit a hurdle model to the data.
       model <- hurdle(
-            month ~ exposure + age_cat + sex  + cov_covid_vax_n_cat + bmi_cat + imd_q5 + ethnicity_6 + region +
-                  number_comorbidities_cat + offset(log(fu_time)) | age_cat + sex+ region,
+            monthly_visits ~ exposure + age_cat + sex  + cov_covid_vax_n_cat + bmi_cat + imd_q5 + ethnicity_6 + region +
+                  number_comorbidities_cat + offset(log(follow_up_time)) | age_cat + sex+ region,
             data = data,
             zero.dist = "binomial",
             dist = "negbin"
@@ -160,31 +170,34 @@ month_full_hurdle_fn <- function(month, n, data, fu_time) {
       results <- cbind(coef_results, results_ci)
 
       results$month <- n
-
+      results <- results %>% rownames_to_column("stat_name") %>% 
+            filter(str_detect(stat_name, "count_exposure"))
       # Return the results.
       return(results)
 }
 
 # C-1 Fully adjusted model: all
 all_full_adj_results <- bind_rows(
-      month_full_hurdle_fn(matched_data$all_month1, 1, data = matched_data, matched_data$follow_up_m1),
-      month_full_hurdle_fn(matched_data$all_month2, 2, data = matched_data, matched_data$follow_up_m2),
-      month_full_hurdle_fn(matched_data$all_month3, 3, data = matched_data, matched_data$follow_up_m3),
-      month_full_hurdle_fn(matched_data$all_month4, 4, data = matched_data, matched_data$follow_up_m4),
-      month_full_hurdle_fn(matched_data$all_month5, 5, data = matched_data, matched_data$follow_up_m5),
-      month_full_hurdle_fn(matched_data$all_month6, 6, data = matched_data, matched_data$follow_up_m6),
-      month_full_hurdle_fn(matched_data$all_month7, 7, data = matched_data, matched_data$follow_up_m7),
-      month_full_hurdle_fn(matched_data$all_month8, 8, data = matched_data, matched_data$follow_up_m8),
-      month_full_hurdle_fn(matched_data$all_month9, 9, data = matched_data, matched_data$follow_up_m9),
-      month_full_hurdle_fn(matched_data$all_month10, 10, data = matched_data, matched_data$follow_up_m10),
-      month_full_hurdle_fn(matched_data$all_month11, 11, data = matched_data, matched_data$follow_up_m11),
-      month_full_hurdle_fn(matched_data$all_month12, 12, data = matched_data, matched_data$follow_up_m12),
+      month_full_hurdle_fn(1, matched_data_ts[matched_data_ts$month == 1,]), 
+      month_full_hurdle_fn(2, matched_data_ts[matched_data_ts$month == 1:2,]),
+      month_full_hurdle_fn(3, matched_data_ts[matched_data_ts$month == 1:3,]), 
+      month_full_hurdle_fn(4, matched_data_ts[matched_data_ts$month == 1:4,]),
+      month_full_hurdle_fn(5, matched_data_ts[matched_data_ts$month == 1:5,]),
+      month_full_hurdle_fn(6, matched_data_ts[matched_data_ts$month == 1:6,]),
+      month_full_hurdle_fn(7, matched_data_ts[matched_data_ts$month == 1:7,]),
+      month_full_hurdle_fn(8, matched_data_ts[matched_data_ts$month == 1:8,]),
+      month_full_hurdle_fn(9, matched_data_ts[matched_data_ts$month == 1:9,]),
+      month_full_hurdle_fn(10, matched_data_ts[matched_data_ts$month == 1:10,]),
+      month_full_hurdle_fn(11, matched_data_ts[matched_data_ts$month == 1:11,]),
+      month_full_hurdle_fn(12, matched_data_ts[matched_data_ts$month == 1:12,])
 )
 
 full_adj <- all_full_adj_results %>%
-      filter(rownames(.) %>% startsWith("count_exposure")) %>%
       mutate(model = "Fully adjusted") %>%
-      relocate(model) %>% relocate(month, .after = model) %>% tibble::remove_rownames()
+      relocate(model) %>% relocate(month, .after = model) %>%
+      tibble::remove_rownames() %>% 
+      dplyr::select("model", "month", "Coefficients", "Lower CI","Upper CI")
+
 
 
 # Final output: ------
