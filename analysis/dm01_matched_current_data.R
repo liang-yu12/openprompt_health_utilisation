799b56cb76cbbe15297e7cc4ab3e6ee82eca0551
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-07-24 14:39:15 +0100

    Manage datasets saperately

--- analysis/dm01_matched_current_data.R
+++ analysis/dm01_matched_current_data.R
@@ -22,27 +22,9 @@ lc_exp_matched <- read_csv("output/matched_cases_with_ehr.csv",
                                           bmi_date = col_date(format = "%Y-%m-%d")))
 lc_exp_matched$match_counts <- NULL
 
-#  # Comparators:
-com_matched <- read_csv("output/matched_control_with_ehr.csv", 
-                        col_types = cols(registration_date = col_date(format = "%Y-%m-%d"), 
-                                         long_covid_dx_date = col_date(format = "%Y-%m-%d"), 
-                                         index_date = col_date(format = "%Y-%m-%d"), 
-                                         end_death = col_date(format = "%Y-%m-%d"), 
-                                         end_deregist = col_date(format = "%Y-%m-%d"), 
-                                         end_lc_cure = col_date(format = "%Y-%m-%d"), 
-                                         covid_vacc_1_vacc_tab = col_date(format = "%Y-%m-%d"), 
-                                         covid_vacc_2_vacc_tab = col_date(format = "%Y-%m-%d"), 
-                                         covid_vacc_3_vacc_tab = col_date(format = "%Y-%m-%d"), 
-                                         covid_vacc_4_vacc_tab = col_date(format = "%Y-%m-%d"), 
-                                         covid_vacc_5_vacc_tab = col_date(format = "%Y-%m-%d"), 
-                                         covid_vacc_6_vacc_tab = col_date(format = "%Y-%m-%d"), 
-                                         covid_dx_month = col_date(format = "%Y-%m-%d"), 
-                                         bmi_date = col_date(format = "%Y-%m-%d")))
 
-#  combine two datasets
-matched_data <- bind_rows(lc_exp_matched, com_matched)
 
-rm(lc_exp_matched, com_matched) # house keeping
+# Data management of exposure dataset: --------------
 
 # ============== Data management for each variables 
 # check the data type
@@ -54,10 +36,10 @@ to_be_factors <- c("sex", "region", "exposure", "covid_positive", "ethnicity",
   "cov_asplenia",   "cov_hiv",   "cov_aplastic_anemia",   "cov_permanent_immune_suppress",
   "cov_temporary_immune_suppress")
 
-matched_data[to_be_factors] <- lapply(matched_data[to_be_factors], as.factor)
+lc_exp_matched[to_be_factors] <- lapply(lc_exp_matched[to_be_factors], as.factor)
 
 # drop unused levels
-matched_data[to_be_factors] <- lapply(matched_data[to_be_factors], droplevels)
+lc_exp_matched[to_be_factors] <- lapply(lc_exp_matched[to_be_factors], droplevels)
 
 
 # Calculate vaccine numbers: -------
@@ -69,9 +51,9 @@ c19_vax_dates <- c("covid_vacc_1_vacc_tab",
                    "covid_vacc_5_vacc_tab", 
                    "covid_vacc_6_vacc_tab")
 
-matched_data$cov_covid_vaccine_number <- rowSums(!is.na(matched_data[, c19_vax_dates, with = FALSE]))
+lc_exp_matched$cov_covid_vaccine_number <- rowSums(!is.na(lc_exp_matched[, c19_vax_dates, with = FALSE]))
 # categorize the vaccine number
-matched_data <- matched_data %>% 
+lc_exp_matched <- lc_exp_matched %>% 
       mutate(cov_covid_vax_n_cat = case_when(
             cov_covid_vaccine_number == 0 ~ 0,
             cov_covid_vaccine_number == 1 ~ 1,
@@ -79,14 +61,14 @@ matched_data <- matched_data %>%
             cov_covid_vaccine_number >= 3 ~ 3)
 )
 # Change covid vax numbers into categorical vars 
-matched_data$cov_covid_vax_n_cat <- matched_data$cov_covid_vax_n_cat %>% 
+lc_exp_matched$cov_covid_vax_n_cat <- lc_exp_matched$cov_covid_vax_n_cat %>% 
       factor(labels = c("0 dose","1 dose","2 doses","3 or more doses"))
 
 # Label exposure indicator
-levels(matched_data$exposure) <- c("Comparator", "Long covid exposure")
+levels(lc_exp_matched$exposure) <- c("Comparator", "Long covid exposure")
 
 # Other data management: IMD quintiles, ethnicity, BMI categories for ethnicity  
-matched_data <- matched_data %>% mutate(
+lc_exp_matched <- lc_exp_matched %>% mutate(
       imd_q5 = cut2(imd, g = 5),
       ethnicity_6 = factor(
             ethnicity,
@@ -124,7 +106,7 @@ matched_data <- matched_data %>% mutate(
 )
 
 # label the imd cat
-levels(matched_data$imd_q5) <- c("least_deprived",
+levels(lc_exp_matched$imd_q5) <- c("least_deprived",
                                  "2_deprived",
                                  "3_deprived",
                                  "4_deprived",
@@ -133,16 +115,16 @@ levels(matched_data$imd_q5) <- c("least_deprived",
 
 # ============== Define the end date ============== 
 # Correct the deregist date 
-matched_data$end_deregist <- as.IDate(matched_data$end_deregist)
+lc_exp_matched$end_deregist <- as.IDate(lc_exp_matched$end_deregist)
 
-matched_data$all_study_end_date <- as.Date("2023-01-31")
+lc_exp_matched$all_study_end_date <- as.Date("2023-01-31")
 
 # data management of some unreasonable dates.
-matched_data$index_date %>% summary
+lc_exp_matched$index_date %>% summary
 
 # Only keep people who are 1. alive 2. registered 3 haven't recovered from lc on the index date
 
-matched_data <- matched_data %>% filter(
+lc_exp_matched <- lc_exp_matched %>% filter(
       ((end_death > index_date) | is.na(end_death)) & 
             ((end_deregist > index_date) | is.na(end_deregist) )& 
             ((end_lc_cure > index_date) | is.na(end_lc_cure))
@@ -150,7 +132,7 @@ matched_data <- matched_data %>% filter(
 
 
 # censored the comparator group if they were diagnosed with long COVID.
-matched_data <- matched_data %>% 
+lc_exp_matched <- lc_exp_matched %>% 
       mutate(
       end_date = ifelse(
             exposure == "Comparator",
@@ -164,18 +146,17 @@ matched_data <- matched_data %>%
                  as.numeric(all_study_end_date), na.rm = T)
       )
 )
-matched_data$end_date <- as.Date.numeric(matched_data$end_date, origin = "1970-01-01")
-matched_data$end_date %>% summary
-matched_data$index_date %>% summary
+lc_exp_matched$end_date <- as.Date.numeric(lc_exp_matched$end_date, origin = "1970-01-01")
+lc_exp_matched$end_date %>% summary
+lc_exp_matched$index_date %>% summary
 
 # calculate follow-up time
-filter(matched_data, end_date == index_date) %>% nrow() # some follow-up time = 0
+filter(lc_exp_matched, end_date == index_date) %>% nrow() # some follow-up time = 0
 
-matched_data$follow_up_time <- as.numeric(matched_data$end_date) - as.numeric(matched_data$index_date)
-matched_data$follow_up_time %>% summary
+lc_exp_matched$follow_up_time <- as.numeric(lc_exp_matched$end_date) - as.numeric(lc_exp_matched$index_date)
+lc_exp_matched$follow_up_time %>% summary
 
-matched_data <- matched_data %>% filter(follow_up_time != 0)
-filter(matched_data, end_date == index_date) %>% nrow() # all follow-up time > 0
+filter(lc_exp_matched, end_date == index_date) %>% nrow() # all follow-up time > 0
 
 # ============== Caclulate the number of comorbidities 
 
@@ -185,32 +166,206 @@ comorbidities <- c("cov_cancer",  "cov_mental_health",   "cov_asthma",
 "cov_asplenia",   "cov_hiv",   "cov_aplastic_anemia",   "cov_permanent_immune_suppress",
 "cov_temporary_immune_suppress")
 # change them into logical factors
-matched_data <- matched_data %>% as_tibble()
-matched_data[comorbidities] <- lapply(matched_data[comorbidities], as.logical)
+lc_exp_matched <- lc_exp_matched %>% as_tibble()
+lc_exp_matched[comorbidities] <- lapply(lc_exp_matched[comorbidities], as.logical)
 
-matched_data$number_comorbidities <- rowSums(matched_data[comorbidities], na.rm = T) # add them up
+lc_exp_matched$number_comorbidities <- rowSums(lc_exp_matched[comorbidities], na.rm = T) # add them up
 
-matched_data <- matched_data %>% 
+lc_exp_matched <- lc_exp_matched %>% 
       mutate(number_comorbidities_cat = case_when(
             number_comorbidities ==0 ~ 0,
             number_comorbidities ==1 ~ 1,
             number_comorbidities ==2 ~ 2,
             number_comorbidities >=3 ~ 3)) 
 
-matched_data$number_comorbidities_cat <- matched_data$number_comorbidities_cat %>% 
+lc_exp_matched$number_comorbidities_cat <- lc_exp_matched$number_comorbidities_cat %>% 
       as.factor()
 
 
 # ============== combine the healthcare visits ============== 
-matched_data$all_month_m1 <- rowSums(matched_data[, grepl("m1$", names(matched_data)), with = FALSE], na.rm = T)
-matched_data$all_month_m2 <- rowSums(matched_data[, grepl("m2$", names(matched_data)), with = FALSE], na.rm = T)
-matched_data$all_month_m3 <- rowSums(matched_data[, grepl("m3$", names(matched_data)), with = FALSE], na.rm = T)
-matched_data$all_month_m4 <- rowSums(matched_data[, grepl("m4$", names(matched_data)), with = FALSE], na.rm = T)
-matched_data$all_month_m5 <- rowSums(matched_data[, grepl("m5$", names(matched_data)), with = FALSE], na.rm = T)
-matched_data$all_month_m6 <- rowSums(matched_data[, grepl("m6$", names(matched_data)), with = FALSE], na.rm = T)
-matched_data$all_month_m7 <- rowSums(matched_data[, grepl("m7$", names(matched_data)), with = FALSE], na.rm = T)
-matched_data$all_month_m8 <- rowSums(matched_data[, grepl("m8$", names(matched_data)), with = FALSE], na.rm = T)
-matched_data$all_month_m9 <- rowSums(matched_data[, grepl("m9$", names(matched_data)), with = FALSE], na.rm = T)
-matched_data$all_month_m10 <- rowSums(matched_data[, grepl("m10$", names(matched_data)), with = FALSE], na.rm = T)
-matched_data$all_month_m11 <- rowSums(matched_data[, grepl("m11$", names(matched_data)), with = FALSE], na.rm = T)
-matched_data$all_month_m12 <- rowSums(matched_data[, grepl("m12$", names(matched_data)), with = FALSE], na.rm = T)
+lc_exp_matched$all_month_m1 <- rowSums(lc_exp_matched[, grepl("m1$", names(lc_exp_matched)), with = FALSE], na.rm = T)
+lc_exp_matched$all_month_m2 <- rowSums(lc_exp_matched[, grepl("m2$", names(lc_exp_matched)), with = FALSE], na.rm = T)
+lc_exp_matched$all_month_m3 <- rowSums(lc_exp_matched[, grepl("m3$", names(lc_exp_matched)), with = FALSE], na.rm = T)
+lc_exp_matched$all_month_m4 <- rowSums(lc_exp_matched[, grepl("m4$", names(lc_exp_matched)), with = FALSE], na.rm = T)
+lc_exp_matched$all_month_m5 <- rowSums(lc_exp_matched[, grepl("m5$", names(lc_exp_matched)), with = FALSE], na.rm = T)
+lc_exp_matched$all_month_m6 <- rowSums(lc_exp_matched[, grepl("m6$", names(lc_exp_matched)), with = FALSE], na.rm = T)
+lc_exp_matched$all_month_m7 <- rowSums(lc_exp_matched[, grepl("m7$", names(lc_exp_matched)), with = FALSE], na.rm = T)
+lc_exp_matched$all_month_m8 <- rowSums(lc_exp_matched[, grepl("m8$", names(lc_exp_matched)), with = FALSE], na.rm = T)
+lc_exp_matched$all_month_m9 <- rowSums(lc_exp_matched[, grepl("m9$", names(lc_exp_matched)), with = FALSE], na.rm = T)
+lc_exp_matched$all_month_m10 <- rowSums(lc_exp_matched[, grepl("m10$", names(lc_exp_matched)), with = FALSE], na.rm = T)
+lc_exp_matched$all_month_m11 <- rowSums(lc_exp_matched[, grepl("m11$", names(lc_exp_matched)), with = FALSE], na.rm = T)
+lc_exp_matched$all_month_m12 <- rowSums(lc_exp_matched[, grepl("m12$", names(lc_exp_matched)), with = FALSE], na.rm = T)
+
+
+
+# Data management of the comparator dataset --------------
+
+#  # Comparators:
+com_matched <- read_csv("output/matched_control_with_ehr.csv", 
+                        col_types = cols(registration_date = col_date(format = "%Y-%m-%d"), 
+                                         long_covid_dx_date = col_date(format = "%Y-%m-%d"), 
+                                         index_date = col_date(format = "%Y-%m-%d"), 
+                                         end_death = col_date(format = "%Y-%m-%d"), 
+                                         end_deregist = col_date(format = "%Y-%m-%d"), 
+                                         end_lc_cure = col_date(format = "%Y-%m-%d"), 
+                                         covid_vacc_1_vacc_tab = col_date(format = "%Y-%m-%d"), 
+                                         covid_vacc_2_vacc_tab = col_date(format = "%Y-%m-%d"), 
+                                         covid_vacc_3_vacc_tab = col_date(format = "%Y-%m-%d"), 
+                                         covid_vacc_4_vacc_tab = col_date(format = "%Y-%m-%d"), 
+                                         covid_vacc_5_vacc_tab = col_date(format = "%Y-%m-%d"), 
+                                         covid_vacc_6_vacc_tab = col_date(format = "%Y-%m-%d"), 
+                                         covid_dx_month = col_date(format = "%Y-%m-%d"), 
+                                         bmi_date = col_date(format = "%Y-%m-%d")))
+com_matched[to_be_factors] <- lapply(com_matched[to_be_factors], as.factor)
+
+# drop unused levels
+com_matched[to_be_factors] <- lapply(com_matched[to_be_factors], droplevels)
+
+
+# Calculate vaccine numbers: -------
+
+com_matched$cov_covid_vaccine_number <- rowSums(!is.na(com_matched[, c19_vax_dates, with = FALSE]))
+# categorize the vaccine number
+com_matched <- com_matched %>% 
+      mutate(cov_covid_vax_n_cat = case_when(
+            cov_covid_vaccine_number == 0 ~ 0,
+            cov_covid_vaccine_number == 1 ~ 1,
+            cov_covid_vaccine_number == 2 ~ 2,
+            cov_covid_vaccine_number >= 3 ~ 3)
+      )
+# Change covid vax numbers into categorical vars 
+com_matched$cov_covid_vax_n_cat <- com_matched$cov_covid_vax_n_cat %>% 
+      factor(labels = c("0 dose","1 dose","2 doses","3 or more doses"))
+
+# Label exposure indicator
+levels(com_matched$exposure) <- c("Comparator", "Long covid exposure")
+
+# Other data management: IMD quintiles, ethnicity, BMI categories for ethnicity  
+com_matched <- com_matched %>% mutate(
+      imd_q5 = cut2(imd, g = 5),
+      ethnicity_6 = factor(
+            ethnicity,
+            levels = 1:5, 
+            labels = c(
+                  "White",
+                  "Mixed", 
+                  "South Asian", 
+                  "Black",
+                  "Other")),
+      age_cat = cut(
+            age, 
+            breaks = c(0, seq(30, 70, 10), Inf),
+            labels = c(
+                  "18-29",
+                  "30-39",
+                  "40-49",
+                  "50-59",
+                  "60-69",
+                  "70+")),
+      bmi_cat = case_when(
+            ethnicity_6 %in% c("South Asian", "Other") ~ cut(bmi, breaks = c(0,18.5,23,27.5,Inf), 
+                                                             labels = c(
+                                                                   "Underweight", 
+                                                                   "Normal Weight", 
+                                                                   "Overweight", 
+                                                                   "Obese")),
+            !ethnicity_6 %in% c("South Asian", "Other") ~ cut(bmi, breaks = c(0,18.5,25,30,Inf),
+                                                              labels = c(
+                                                                    "Underweight", 
+                                                                    "Normal Weight", 
+                                                                    "Overweight", 
+                                                                    "Obese"))
+      )
+)
+
+# label the imd cat
+levels(com_matched$imd_q5) <- c("least_deprived",
+                                 "2_deprived",
+                                 "3_deprived",
+                                 "4_deprived",
+                                 "most_deprived")
+
+
+# ============== Define the end date ============== 
+# Correct the deregist date 
+com_matched$end_deregist <- as.IDate(com_matched$end_deregist)
+
+com_matched$all_study_end_date <- as.Date("2023-01-31")
+
+# data management of some unreasonable dates.
+com_matched$index_date %>% summary
+
+# Only keep people who are 1. alive 2. registered 3 haven't recovered from lc on the index date
+
+com_matched <- com_matched %>% filter(
+      ((end_death > index_date) | is.na(end_death)) & 
+            ((end_deregist > index_date) | is.na(end_deregist) )& 
+            ((end_lc_cure > index_date) | is.na(end_lc_cure))
+)
+
+
+# censored the comparator group if they were diagnosed with long COVID.
+com_matched <- com_matched %>% 
+      mutate(
+            end_date = ifelse(
+                  exposure == "Comparator",
+                  pmin(as.numeric(end_death), 
+                       as.numeric(end_deregist), 
+                       as.numeric(long_covid_dx_date), 
+                       as.numeric(all_study_end_date), na.rm = T),
+                  pmin(as.numeric(end_death), 
+                       as.numeric(end_deregist), 
+                       as.numeric(end_lc_cure), 
+                       as.numeric(all_study_end_date), na.rm = T)
+            )
+      )
+com_matched$end_date <- as.Date.numeric(com_matched$end_date, origin = "1970-01-01")
+com_matched$end_date %>% summary
+com_matched$index_date %>% summary
+
+# calculate follow-up time
+filter(com_matched, end_date == index_date) %>% nrow() # some follow-up time = 0
+
+com_matched$follow_up_time <- as.numeric(com_matched$end_date) - as.numeric(com_matched$index_date)
+com_matched$follow_up_time %>% summary
+
+com_matched <- com_matched %>% filter(follow_up_time != 0)
+filter(com_matched, end_date == index_date) %>% nrow() # all follow-up time > 0
+
+# ============== Caclulate the number of comorbidities 
+
+comorbidities <- c("cov_cancer",  "cov_mental_health",   "cov_asthma",
+                   "cov_organ_transplant",   "cov_chronic_cardiac_disease",   "cov_chronic_liver_disease",
+                   "cov_stroke_dementia",   "cov_other_neuro_diseases",   "cov_ra_sle_psoriasis",
+                   "cov_asplenia",   "cov_hiv",   "cov_aplastic_anemia",   "cov_permanent_immune_suppress",
+                   "cov_temporary_immune_suppress")
+# change them into logical factors
+com_matched <- com_matched %>% as_tibble()
+com_matched[comorbidities] <- lapply(com_matched[comorbidities], as.logical)
+
+com_matched$number_comorbidities <- rowSums(com_matched[comorbidities], na.rm = T) # add them up
+
+com_matched <- com_matched %>% 
+      mutate(number_comorbidities_cat = case_when(
+            number_comorbidities ==0 ~ 0,
+            number_comorbidities ==1 ~ 1,
+            number_comorbidities ==2 ~ 2,
+            number_comorbidities >=3 ~ 3)) 
+
+com_matched$number_comorbidities_cat <- com_matched$number_comorbidities_cat %>% 
+      as.factor()
+
+
+# ============== combine the healthcare visits ============== 
+com_matched$all_month_m1 <- rowSums(com_matched[, grepl("m1$", names(com_matched)), with = FALSE], na.rm = T)
+com_matched$all_month_m2 <- rowSums(com_matched[, grepl("m2$", names(com_matched)), with = FALSE], na.rm = T)
+com_matched$all_month_m3 <- rowSums(com_matched[, grepl("m3$", names(com_matched)), with = FALSE], na.rm = T)
+com_matched$all_month_m4 <- rowSums(com_matched[, grepl("m4$", names(com_matched)), with = FALSE], na.rm = T)
+com_matched$all_month_m5 <- rowSums(com_matched[, grepl("m5$", names(com_matched)), with = FALSE], na.rm = T)
+com_matched$all_month_m6 <- rowSums(com_matched[, grepl("m6$", names(com_matched)), with = FALSE], na.rm = T)
+com_matched$all_month_m7 <- rowSums(com_matched[, grepl("m7$", names(com_matched)), with = FALSE], na.rm = T)
+com_matched$all_month_m8 <- rowSums(com_matched[, grepl("m8$", names(com_matched)), with = FALSE], na.rm = T)
+com_matched$all_month_m9 <- rowSums(com_matched[, grepl("m9$", names(com_matched)), with = FALSE], na.rm = T)
+com_matched$all_month_m10 <- rowSums(com_matched[, grepl("m10$", names(com_matched)), with = FALSE], na.rm = T)
+com_matched$all_month_m11 <- rowSums(com_matched[, grepl("m11$", names(com_matched)), with = FALSE], na.rm = T)
+com_matched$all_month_m12 <- rowSums(com_matched[, grepl("m12$", names(com_matched)), with = FALSE], na.rm = T)
