28b8c64b902ff503cd4892c7b7ca39cd4a41985b
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-07-27 18:06:45 +0100

    Try running random effect poisson (random intercept)

--- analysis/st03_model_02_cluster_analysis.R
+++ analysis/st03_model_02_cluster_analysis.R
@@ -2,4 +2,40 @@
 source("analysis/dm03_5_matched_pivot_long.R")
 
 # Fitting random-effects Poisson regression ----
-# - This will allow clustering (by individual)
+# - Allowing clustering (by individual)
+
+# Random intercept model: cluster: by individual:  "patient_id"      
+
+# define a function  to organise the regression output
+output_org_fn <- function(reg, m){
+      output <- reg %>% summary() %>% coef() %>% as.data.frame() 
+      output <- output %>% mutate(term = rownames(output)) %>% 
+            mutate(or=exp(Estimate)) %>% 
+            mutate(lci=exp((Estimate-1.96*`Std. Error`))) %>% 
+            mutate(hci=exp((Estimate+1.96*`Std. Error`))) %>% 
+            mutate(model=m) %>% 
+            dplyr::select(model, term, or, lci, hci,`Pr(>|z|)`)
+      return(output)
+}
+
+
+# Crude random intercept
+crude_glmer <- glmer(
+      formula = monthly_visits ~ 1 + exposure + offset(log(follow_up_time)) + (1|patient_id),
+      data = matched_data_ts,
+      family = poisson(link = "log") 
+)
+
+ri_poisson_crude <- output_org_fn(crude_glmer, "Random intercept Poisson")
+
+# adjusted random intercept
+adj_glmer <- glmer(
+      formula = monthly_visits ~ 1 + exposure + offset(log(follow_up_time)) + (1|patient_id) +
+            sex + region + age_cat + imd_q5 + ethnicity_6 + bmi_cat +
+            number_comorbidities_cat + previous_covid_hosp + cov_covid_vax_n_cat,
+      data = matched_data_ts,
+      family = poisson(link = "log") 
+)
+
+ri_poisson_adj <- output_org_fn(adj_glmer)
+
