fced4f0c883ed4849aa040f67d4a4e7c9d5f9223
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-07-28 15:38:22 +0100

    Added GEE

--- analysis/st03_model_02_cluster_analysis.R
+++ analysis/st03_model_02_cluster_analysis.R
@@ -70,7 +70,44 @@ rs_poisson_adj <- output_org_fn(adj_glmer_slope, "Random slope Poisson adjusted"
 
 # GEE -----
 
+# function for organising outputs
+gee_output_org_fn <- function(reg, m){
+      output <- reg %>% summary() %>% coef() %>% as.data.frame() 
+      output <- output %>% mutate(term = rownames(output)) %>% 
+            mutate(or=exp(Estimate)) %>% 
+            mutate(lci=exp((Estimate-1.96*`Std.err`))) %>% 
+            mutate(hci=exp((Estimate+1.96*`Std.err`))) %>% 
+            mutate(model=m) %>% 
+            dplyr::select(model, term, or, lci, hci,`Pr(>|W|)`) %>% 
+            filter(term == "(Intercept)" | term == "exposureLong covid exposure")
+      
+      return(output)
+}
 
+# Crude
+gee_crude <- geeglm(monthly_visits ~ exposure + offset(log(follow_up_time)),
+                    data = drop_na(matched_data_ts, any_of(c("monthly_visits", "exposure", "follow_up_time"))),
+                    id = patient_id,
+                    family = poisson(link = "log") ,
+                    corstr = "ar1"
+)
+
+results_gee_crude <- gee_crude %>% gee_output_org_fn("GEE Crude")
+
+
+# Adjusted 
+all_var <- c("monthly_visits","exposure", "follow_up_time", "sex", "region",
+             "age_cat", "imd_q5", "ethnicity_6", "bmi_cat", "number_comorbidities_cat",
+             "previous_covid_hosp", "cov_covid_vax_n_cat") # variables for non-missing
+
+gee_adj <- geeglm(monthly_visits ~ exposure + offset(log(follow_up_time)),
+                  data = drop_na(matched_data_ts, any_of(all_var)),
+                  id = patient_id,
+                  family = poisson(link = "log") ,
+                  corstr = "ar1"
+)
+
+results_gee_adj <- gee_adj %>% gee_output_org_fn("GEE Adjusted")
 
 
 # organised and save output
