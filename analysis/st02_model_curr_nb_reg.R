5060cfc8587d57e069d9e94d1f5cd94eb3e08419
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-08-04 14:50:06 +0100

    Write non clustered outputs

--- analysis/st02_model_curr_nb_reg.R
+++ analysis/st02_model_curr_nb_reg.R
@@ -2,11 +2,28 @@
 source("analysis/dm03_5_matched_pivot_long.R")
 
 # Data management for modeling:: --------
-## Collapsing data by summarising the visits and follow-up time -----
+# Collapsing data by summarising the visits and follow-up time, and 
+# generate three datasets for follow-up 3m, 6m, and 12m
 
+# # 3 months
+matched_data_3m <- matched_data_ts %>% 
+      filter(month %in% c(1,2,3)) %>% 
+      group_by(patient_id, exposure) %>% 
+      summarise(
+            visits = sum(monthly_visits),
+            follow_up = sum(follow_up_time)) %>% 
+      ungroup()
 
+# # 6 months
+matched_data_6m <- matched_data_ts %>% 
+      filter(month %in% c(1,2,3,4,5,6)) %>% 
+      group_by(patient_id, exposure) %>% 
+      summarise(
+            visits = sum(monthly_visits),
+            follow_up = sum(follow_up_time)) %>% 
+      ungroup()
 
-
+# follow 12 months 
 matched_data_12m <- matched_data_ts %>% 
       group_by(patient_id, exposure) %>% 
       summarise(
@@ -15,7 +32,7 @@ matched_data_12m <- matched_data_ts %>%
       ungroup()
 
 
-## Add covariates 
+# # Add covariates for adjustment
 for_covariates <- matched_data_ts %>% distinct(patient_id, exposure, .keep_all = T) %>% 
       dplyr::select("patient_id",     
                     "exposure",           
@@ -29,86 +46,101 @@ for_covariates <- matched_data_ts %>% distinct(patient_id, exposure, .keep_all =
                     "cov_covid_vax_n_cat",     
                     "number_comorbidities_cat")
 
-matched_data_year <- left_join(matched_data_year, for_covariates,
+# # add covariates back to the summarised data frame
+matched_data_3m <- left_join(matched_data_3m, for_covariates,
                                by = c("patient_id" = "patient_id", "exposure" = "exposure"))
 
-matched_data_year$exposure <- relevel(matched_data_year$exposure, ref = "Comparator")
-matched_data_year$exposure %>% levels()
+matched_data_6m <- left_join(matched_data_6m, for_covariates,
+                             by = c("patient_id" = "patient_id", "exposure" = "exposure"))
 
-# Model: crude poisson model: ------
-poisson_crude <- glm(visits ~ exposure + offset(log(follow_up)), 
-                     data = matched_data_year, 
-                     family = "poisson") %>% summary()
+matched_data_12m <- left_join(matched_data_12m, for_covariates,
+                             by = c("patient_id" = "patient_id", "exposure" = "exposure"))
 
-# organised output:
-result_poisson_crude <- as.data.frame(poisson_crude$coefficients) 
-result_poisson_crude$lci <- exp(result_poisson_crude$Estimate - 1.96*result_poisson_crude$`Std. Error`)
-result_poisson_crude$uci <- exp(result_poisson_crude$Estimate + 1.96*result_poisson_crude$`Std. Error`)
-result_poisson_crude$Estimate <- exp(result_poisson_crude$Estimate)
-result_poisson_crude$terms <- rownames(result_poisson_crude)
-result_poisson_crude <- result_poisson_crude %>% 
-      filter(terms == "exposureLong covid exposure") %>% 
-      mutate(aic = poisson_crude$aic) %>% 
-      mutate(model = "Poisson crude") %>% relocate(model, terms, Estimate,lci,uci)
+# correct the level of exposure groups
+matched_data_3m$exposure <- relevel(matched_data_3m$exposure, ref = "Comparator")
+matched_data_6m$exposure <- relevel(matched_data_6m$exposure, ref = "Comparator")
+matched_data_12m$exposure <- relevel(matched_data_12m$exposure, ref = "Comparator")
 
+# Stats: one part model
 # Model: crude negative binomial model: -----
-nb_crude <- glm.nb(visits ~ exposure + offset(log(follow_up)), 
-                   data = matched_data_year,
-                   link = log) %>% summary()
+# 3m 
+nb_crude_3m <- glm.nb(visits ~ exposure + offset(log(follow_up)), 
+                   data = matched_data_3m,
+                   link = log)
 
-result_nb_crude <- as.data.frame(nb_crude$coefficients)
-result_nb_crude$lci <- exp(result_nb_crude$Estimate - 1.96*result_nb_crude$`Std. Error`)
-result_nb_crude$uci <- exp(result_nb_crude$Estimate + 1.96*result_nb_crude$`Std. Error`)
-result_nb_crude$Estimate <- exp(result_nb_crude$Estimate)
-result_nb_crude$terms <- rownames(result_nb_crude)
-result_nb_crude <- result_nb_crude %>% 
-      filter(terms == "exposureLong covid exposure") %>% 
-      mutate(aic = nb_crude$aic) %>% 
-      mutate(model = "Negative binomial crude") %>% relocate(model, terms, Estimate,lci,uci)
+nb_crude_6m <- glm.nb(visits ~ exposure + offset(log(follow_up)), 
+                      data = matched_data_6m,
+                      link = log)
 
+nb_crude_12m <- glm.nb(visits ~ exposure + offset(log(follow_up)), 
+                      data = matched_data_12m,
+                      link = log)
 
-# Model: adjusted Poisson: ------
-poisson_adjusted <- glm(visits ~ exposure + offset(log(follow_up)) + 
-                              sex + region + age_cat + imd_q5 + ethnicity_6 + bmi_cat +
-                              number_comorbidities_cat + previous_covid_hosp + cov_covid_vax_n_cat, 
-                        data = matched_data_year, 
-                        family = "poisson")  %>% summary()
-# output organised 
-result_poisson_adjusted <- as.data.frame(poisson_adjusted$coefficients) 
-result_poisson_adjusted$lci <- exp(result_poisson_adjusted$Estimate - 1.96*result_poisson_adjusted$`Std. Error`)
-result_poisson_adjusted$uci <- exp(result_poisson_adjusted$Estimate + 1.96*result_poisson_adjusted$`Std. Error`)
-result_poisson_adjusted$Estimate <- exp(result_poisson_adjusted$Estimate)
-result_poisson_adjusted$terms <- rownames(result_poisson_adjusted)
-result_poisson_adjusted <- result_poisson_adjusted %>% 
-      filter(terms == "exposureLong covid exposure") %>% 
-      mutate(aic = poisson_adjusted$aic) %>% 
-      mutate(model = "Poisson adjusted") %>% relocate(model, terms, Estimate,lci,uci)
+# Function to organise the regression outputs
+org_reg_results_fn <- function(nb_reg, time) {
+      # Calculate the coefficients and confidence intervals
+      results <- bind_cols(
+            (coef(nb_reg) %>% exp() %>% as.data.frame()),
+            (confint(nb_reg) %>% exp() %>% as.data.frame()))
+      
+      # Set the row names as the variable names
+      results$terms <- rownames(results)
+      
+      # Rename the "." column to "estimates"
+      results <- rename(results, estimates = .)
+      
+      # Add a time frame column
+      results <- mutate(results, time_frame =time)
+      
+      # Rearrange the columns
+      results <- relocate(results, time_frame, terms, estimates)
+      
+      return(results)
+}
 
-
+# Organised and combine crude results
+crude_reg_results <- bind_rows(
+      org_reg_results_fn(nb_crude_3m, "3 months"),
+      org_reg_results_fn(nb_crude_6m, "6 months"),
+      org_reg_results_fn(nb_crude_12m, "12 months")
+) %>% mutate(model = "Crude") %>% relocate(model)
 
 # Model: adjusted Negative binomial: ------
-nb_adj <- glm.nb(visits ~ exposure + offset(log(follow_up)) + 
+# 3 months
+nb_adj_3m <- glm.nb(visits ~ exposure + offset(log(follow_up)) + 
                        sex + region + age_cat + imd_q5 + ethnicity_6 + bmi_cat +
                        number_comorbidities_cat + previous_covid_hosp + cov_covid_vax_n_cat, 
-                 data = matched_data_year,
-                 link = log) %>% summary()
+                 data = matched_data_3m,
+                 link = log)
 
-# output organised 
-result_nb_adj <- as.data.frame(nb_adj$coefficients)
-result_nb_adj$lci <- exp(result_nb_adj$Estimate - 1.96*result_nb_adj$`Std. Error`)
-result_nb_adj$uci <- exp(result_nb_adj$Estimate + 1.96*result_nb_adj$`Std. Error`)
-result_nb_adj$Estimate <- exp(result_nb_adj$Estimate)
-result_nb_adj$terms <- rownames(result_nb_adj)
-result_nb_adj <- result_nb_adj %>% 
-      filter(terms == "exposureLong covid exposure") %>% 
-      mutate(aic = nb_adj$aic) %>% 
-      mutate(model = "Negative binomial adjusted") %>% relocate(model, terms, Estimate,lci,uci)
+# 6 months
+nb_adj_6m <- glm.nb(visits ~ exposure + offset(log(follow_up)) + 
+                          sex + region + age_cat + imd_q5 + ethnicity_6 + bmi_cat +
+                          number_comorbidities_cat + previous_covid_hosp + cov_covid_vax_n_cat, 
+                    data = matched_data_6m,
+                    link = log)
+
+# 12 months
+nb_adj_12m <- glm.nb(visits ~ exposure + offset(log(follow_up)) + 
+                          sex + region + age_cat + imd_q5 + ethnicity_6 + bmi_cat +
+                          number_comorbidities_cat + previous_covid_hosp + cov_covid_vax_n_cat, 
+                    data = matched_data_12m,
+                    link = log)
+
+# combine results
+adj_reg_results <- bind_rows(
+      org_reg_results_fn(nb_adj_3m, "3 months"),
+      org_reg_results_fn(nb_adj_6m, "6 months"),
+      org_reg_results_fn(nb_adj_12m, "12 months")
+) %>% mutate(model = "Adjusted") %>% relocate(model)
+
+# write outputs:
+bind_rows(crude_reg_results, adj_reg_results) %>% 
+      write_csv(here("output", "st_02_non_cluster_model.csv"))
 
 
-# save outputs: ------
-bind_rows(result_poisson_crude, result_poisson_adjusted,
-          result_nb_crude, result_nb_adj) %>% 
-      write_csv(here("output", "st_sup_model_selection.csv"))
+
+
 
 
 
