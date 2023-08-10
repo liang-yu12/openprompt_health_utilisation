afcf75ed1e1051119e329848656bed771f17adf4
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-08-10 09:19:57 +0100

    still error

--- analysis/qc01_unmatched_control.R
+++ analysis/qc01_unmatched_control.R
@@ -16,9 +16,9 @@ lc_exp_raw <- read_csv(here("output", "dataset_exp_lc_unmatched.csv"),
                              end_lc_cure = col_date(format =  "%Y-%m-%d")
                            ))
 
-lc_exp_raw <- lc_exp_raw %>% mutate(wrong_date = ifelse((long_covid_dx_date <= end_death |
-                                                            long_covid_dx_date <= end_deregist |
-                                                            long_covid_dx_date <= end_lc_cure), 1, 0))
+lc_exp_raw <- lc_exp_raw %>% mutate(wrong_date = ifelse((as.numeric(long_covid_dx_date) > as.numeric(end_death) |
+                                                            as.numeric(long_covid_dx_date) > as.numeric(end_deregist) |
+                                                            as.numeric(long_covid_dx_date) > as.numeric(end_lc_cure)), 1, 0))
 lc_exp_raw$wrong_date <- as.factor(lc_exp_raw$wrong_date)
 lc_exp_raw$exposure <- "long COVID"
 
@@ -39,10 +39,10 @@ lc_com_raw <- read_csv(here("output", "dataset_comparator_unmatched.csv"),
                          end_lc_cure = col_date(format = "%Y-%m-%d")
                        ))
 
-lc_com_raw <- lc_com_raw %>% mutate(wrong_date = ifelse((long_covid_dx_date <= end_death |
-                                  long_covid_dx_date <= end_deregist |
-                                  long_covid_dx_date <= end_lc_cure), 1, 0))
-
+lc_com_raw <- lc_com_raw %>% mutate(wrong_date = ifelse((as.numeric(long_covid_dx_date) > as.numeric(end_death) |
+                                                            as.numeric(long_covid_dx_date) > as.numeric(end_deregist) |
+                                                            as.numeric(long_covid_dx_date) > as.numeric(end_lc_cure)), 1, 0))
+                                                            
 lc_com_raw$wrong_date <- as.factor(lc_com_raw$wrong_date)
 lc_com_raw$exposure <- "Comparison"
 
