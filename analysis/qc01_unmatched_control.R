1b826ce107a3831f67c0a5c2fad79b4a63c2d5c6
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-08-10 09:27:43 +0100

    investigating time

--- analysis/qc01_unmatched_control.R
+++ analysis/qc01_unmatched_control.R
@@ -1,19 +1,19 @@
 # Load all packages
 source("analysis/settings_packages.R")
 # unmatched exposure
-lc_exp_raw <- read_csv(here("output", "dataset_exp_lc_unmatched.csv"), 
-                           col_types = cols(
-                             patient_id = col_double(),
-                             age = col_double(),
-                             sex = col_character(),
-                             region = col_character(),
-                             registration_date = col_date(format =  "%Y-%m-%d"),
-                             long_covid_dx = col_double(),
-                             long_covid_dx_date = col_date(format =  "%Y-%m-%d"),
-                             index_date = col_date(format =  "%Y-%m-%d"),
-                             end_death = col_date(format =  "%Y-%m-%d"),
-                             end_deregist = col_date(format =  "%Y-%m-%d"),
-                             end_lc_cure = col_date(format =  "%Y-%m-%d")
+lc_exp_raw <- read_csv(here("output", "dataset_exp_lc_unmatched.csv")
+                          #  col_types = cols(
+                          #    patient_id = col_double(),
+                          #    age = col_double(),
+                          #    sex = col_character(),
+                          #    region = col_character(),
+                          #    registration_date = col_date(format =  "%Y-%m-%d"),
+                          #    long_covid_dx = col_double(),
+                          #    long_covid_dx_date = col_date(format =  "%Y-%m-%d"),
+                          #    index_date = col_date(format =  "%Y-%m-%d"),
+                          #    end_death = col_date(format =  "%Y-%m-%d"),
+                          #    end_deregist = col_date(format =  "%Y-%m-%d"),
+                          #    end_lc_cure = col_date(format =  "%Y-%m-%d")
                            ))
 # 
 # lc_exp_raw <- lc_exp_raw %>% mutate(wrong_date = ifelse((as.numeric(long_covid_dx_date) > as.numeric(end_death) |
@@ -25,18 +25,18 @@ lc_exp_raw$exposure <- "long COVID"
 
 
 # unmatched comparator
-lc_com_raw <- read_csv(here("output", "dataset_comparator_unmatched.csv"), 
-                       col_types = cols(
-                         patient_id = col_double(),
-                         age = col_double(),
-                         sex = col_character(),
-                         region = col_character(),
-                         registration_date = col_date(format = "%Y-%m-%d"),
-                         long_covid_dx = col_double(),
-                         long_covid_dx_date = col_date(format = "%Y-%m-%d"),
-                         end_death = col_date(format = "%Y-%m-%d"),
-                         end_deregist = col_date(format = "%Y-%m-%d"),
-                         end_lc_cure = col_date(format = "%Y-%m-%d")
+lc_com_raw <- read_csv(here("output", "dataset_comparator_unmatched.csv")
+                      #  col_types = cols(
+                      #    patient_id = col_double(),
+                      #    age = col_double(),
+                      #    sex = col_character(),
+                      #    region = col_character(),
+                      #    registration_date = col_date(format = "%Y-%m-%d"),
+                      #    long_covid_dx = col_double(),
+                      #    long_covid_dx_date = col_date(format = "%Y-%m-%d"),
+                      #    end_death = col_date(format = "%Y-%m-%d"),
+                      #    end_deregist = col_date(format = "%Y-%m-%d"),
+                      #    end_lc_cure = col_date(format = "%Y-%m-%d")
                        ))
 
 # lc_com_raw <- lc_com_raw %>% mutate(wrong_date = ifelse((as.numeric(long_covid_dx_date) > as.numeric(end_death) |
