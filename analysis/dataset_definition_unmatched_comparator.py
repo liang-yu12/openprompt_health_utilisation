b3c9870ce37d0ae5acf62fa8f28936f026a279d5
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-08-16 15:11:30 +0100

    Make the comparator group consistent

--- analysis/dataset_definition_unmatched_comparator.py
+++ analysis/dataset_definition_unmatched_comparator.py
@@ -26,9 +26,6 @@ dataset.define_population(
     & registration.exists_for_patient() 
     & target_practices.exists_for_patient()
     & patients.sex.contains("male")
-    & ((lc_cure_date > lc_dx_date) | (lc_cure_date is None) | (lc_dx_date is None))
-    & ((end_reg_date > lc_dx_date) | (end_reg_date is None) | (lc_dx_date is None))
-    & ((death_date > lc_dx_date) | (death_date is None) | (lc_dx_date is None))
 )
 dataset.age = age
 dataset.sex = patients.sex
