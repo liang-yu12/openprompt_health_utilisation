0b95b32630154e5ffb96ccf40858647dae1b8bc1
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-08-17 22:18:23 +0100

    Fix the wrong date columns.

--- analysis/qc01_unmatched_control.R
+++ analysis/qc01_unmatched_control.R
@@ -10,22 +10,13 @@ lc_exp_raw <- read_csv(here("output", "dataset_exp_lc_unmatched.csv"),
                              registration_date = col_date(format = "%Y-%m-%d"),
                              long_covid_dx = col_double(),
                              long_covid_dx_date = col_date(format = "%Y-%m-%d"),
-                             index_date = col_skip(),
+                             index_date = col_date(format = "%Y-%m-%d"),
                              end_death = col_date(format = "%Y-%m-%d"),
                              end_deregist = col_date(format = "%Y-%m-%d"),
                              end_lc_cure = col_date(format = "%Y-%m-%d")
                              )
 )
-
-lc_exp_raw <- lc_exp_raw %>% mutate(wrong_date = ifelse((as.numeric(long_covid_dx_date) > as.numeric(end_death) |
-                                                            as.numeric(long_covid_dx_date) > as.numeric(end_deregist) |
-                                                            as.numeric(long_covid_dx_date) > as.numeric(end_lc_cure)), 1, 0)) %>% 
-      mutate(lc_yes=ifelse(!is.na(long_covid_dx_date), 1,0)) %>% mutate(exposure = "long COVID")
-lc_exp_raw$wrong_date <- as.factor(lc_exp_raw$wrong_date)
-lc_exp_raw$lc_yes <- as.factor(lc_exp_raw$lc_yes)
-
-
-
+lc_exp_raw$exposure <- 1
 
 # unmatched comparator
 lc_com_raw <- read_csv(here("output", "dataset_comparator_unmatched.csv"),
@@ -42,15 +33,9 @@ lc_com_raw <- read_csv(here("output", "dataset_comparator_unmatched.csv"),
                              end_lc_cure = col_date(format = "%Y-%m-%d")
                               )
 )
-
-lc_com_raw <- lc_com_raw %>% mutate(wrong_date = ifelse((as.numeric(long_covid_dx_date) > as.numeric(end_death) |
-                                                                as.numeric(long_covid_dx_date) > as.numeric(end_deregist) |
-                                                                as.numeric(long_covid_dx_date) > as.numeric(end_lc_cure)), 1, 0)) %>% 
-      mutate(lc_yes=ifelse(!is.na(long_covid_dx_date), 1,0)) %>% mutate(exposure = "Comparison")
-
-lc_com_raw$wrong_date <- as.factor(lc_com_raw$wrong_date)
-lc_com_raw$lc_yes <- as.factor(lc_com_raw$lc_yes)
-
+lc_com_raw$index_date <- as.Date("2020-11-01")
+lc_com_raw <- lc_com_raw %>% relocate(index_date, .before = end_death)
+lc_com_raw$exposure <- 0
 
 # combind and explore
 unmatched_all <- bind_rows(lc_exp_raw, lc_com_raw)
@@ -63,7 +48,7 @@ unmatched_all <- unmatched_all %>%
 
 # generate basic summarised table:
 dependent = "exposure"
-explanatory = c("sex", "age", "lc_yes", "wrong_death", "wrong_regist", "wrong_lc_cure")
+explanatory = c("sex", "age", "wrong_death", "wrong_regist", "wrong_lc_cure")
 
 unmatched_results <- unmatched_all %>% summary_factorlist(dependent, explanatory) 
 unmatched_results %>% write_csv(here("output", "qc01_check_unmatched.csv"))
