e2d0efb889df1981e3377fc31ab1c95815d49144
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-08-18 09:02:58 +0100

    remove NA to censore

--- analysis/st02_01_09_y_opa_model_curr_nb_reg.R
+++ analysis/st02_01_09_y_opa_model_curr_nb_reg.R
@@ -7,7 +7,7 @@ source("analysis/dm03_9_pivot_opa_long.R")
 matched_data_opa_ts$month %>% table
 # # 3 months
 matched_data_opa_3m <- matched_data_opa_ts %>% 
-      filter(month %in% c(1,2,3)) %>% 
+      filter(month %in% c(1,2,3) & !is.na(follow_up_time)) %>% 
       group_by(patient_id, exposure) %>% 
       summarise(
             visits = sum(monthly_opa_visits),
@@ -16,7 +16,7 @@ matched_data_opa_3m <- matched_data_opa_ts %>%
 
 # # 6 months
 matched_data_opa_6m <- matched_data_opa_ts %>% 
-      filter(month %in% c(1,2,3,4,5,6)) %>% 
+      filter(month %in% c(1,2,3,4,5,6) & !is.na(follow_up_time)) %>% 
       group_by(patient_id, exposure) %>% 
       summarise(
             visits = sum(monthly_opa_visits),
@@ -25,6 +25,7 @@ matched_data_opa_6m <- matched_data_opa_ts %>%
 
 # follow 12 months 
 matched_data_opa_12m <- matched_data_opa_ts %>% 
+      filter(!is.na(follow_up_time)) %>% 
       group_by(patient_id, exposure) %>% 
       summarise(
             visits = sum(monthly_opa_visits),
