0918c3195f9efa6be074a2a2871fb0a42d80b5ac
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-08-22 15:27:23 +0100

    Try avoiding zero follow-up time

--- analysis/dm03_matched_define_monthly_follow_up_time.R
+++ analysis/dm03_matched_define_monthly_follow_up_time.R
@@ -6,21 +6,36 @@ source("analysis/dm01_matched_current_data.R")
 # the healthcare utilisation by month. However, we need to consider 
 # end_date may fall within the monthly follow-up time. 
 #
-# Concept: use month 4 as example:
 
+# Goal: 
+# fu_total, fu in month 1, 
+# 0		, NA	      ==> fu_total==0~NA_real_
+# 15		, 15		==> ((fu_total%/%30) <1 & fu_total!=0) ~ fu_total
+# 30		, 30		==> fu_total%/%30>=1 ~ 30
+# 35		, 30		==> fu_total%/%30>=1 ~ 30
+# 45		, 30		==> fu_total%/%30>=1 ~ 30
+# 60		, 30		==> fu_total%/%30>=1 ~ 30
+# 65		, 30		==> fu_total%/%30>=1 ~ 30
 
-# mutate(follow_up_m4=case_when(
-#       fu_total%/%30>=4 ~ 30,
-# If the total follow_up time can be exactly devided by 30, then the follow_up 
-# is 30; 
-#       fu_total%/%30==3 ~ fu_total%%30,
+# fu_total, fu in month 2,
+# 0		,NA   ==> fu_total==0~NA_real_
+# 15		,NA   ==> (fu_total%/%30) <1 ~ NA_real_
+# 30		,NA   ==> fu_total%/%30 == 1 & fu_total%%30 ==0 ~ NA_real_
+# 35		,5    ==> fu_total%/%30 == 1 & fu_total%%30 !=0 ~ fu_total%%30
+# 45		,15   ==> fu_total%/%30 == 1 & fu_total%%30 !=0 ~ fu_total%%30
+# 60		,30   ==> fu_total%/%30 > 1 ~ 30
+# 65		,30   ==> fu_total%/%30 > 1 ~ 30
+
+# Concept: use month 2 as example:
+# If the total follow_up time can be exactly devided by 30 and the ratio >2, then the follow_up is 30; 
 # If the follow up ended this month, then the remain of the dates will be the 
-# follow-up days;
-#       fu_total%/%30< 3 ~ NA_real_))
+#     fu_total%/%30==3 & fu_total%%30 !=0~ fu_total%%30,
+# If the remain is 0 when deviding by 30, then censored it with NA
+#     fu_total%/%30 == 1 & fu_total%%30 ==0 ~ NA_real_
 # If the follow-up ended in the previous month, then censored it with NA
+#     fu_total%/%30 <1 ~ NA_real_
 
 # total follow-up time: fu_total
-
 # define follow-up time in the exposure dataset: ------
 
 # define follow-up by months
@@ -30,57 +45,57 @@ lc_exp_matched <- lc_exp_matched %>%
             fu_total%/%30 <1 & fu_total!=0  ~ fu_total,
             fu_total == 0 ~ NA_real_)) %>% 
       mutate(follow_up_m2=case_when(
-            fu_total%/%30>=2 ~ 30,
+            fu_total%/%30> 1 ~ 30,
             fu_total%/%30==1 & fu_total%%30!=0 ~ fu_total%%30,
             fu_total%/%30==1 & fu_total%%30==0 ~ NA_real_,
-            fu_total%/%30< 1 ~ NA_real_)) %>% 
+            fu_total%/%30 < 1 ~ NA_real_)) %>% 
       mutate(follow_up_m3=case_when(
-            fu_total%/%30>=3 ~ 30,
+            fu_total%/%30> 2 ~ 30,
             fu_total%/%30==2 & fu_total%%30!=0  ~ fu_total%%30,
             fu_total%/%30==2 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30< 2 ~ NA_real_)) %>%
       mutate(follow_up_m4=case_when(
-            fu_total%/%30>=4 ~ 30,
+            fu_total%/%30> 3 ~ 30,
             fu_total%/%30==3 & fu_total%%30!=0  ~ fu_total%%30,
             fu_total%/%30==3 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30< 3 ~ NA_real_)) %>%
       mutate(follow_up_m5=case_when(
-            fu_total%/%30>=5 ~ 30,
+            fu_total%/%30> 4 ~ 30,
             fu_total%/%30==4 & fu_total%%30!=0  ~ fu_total%%30,
             fu_total%/%30==4 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30< 4 ~ NA_real_)) %>%
       mutate(follow_up_m6=case_when(
-            fu_total%/%30>=6 ~ 30,
+            fu_total%/%30> 5 ~ 30,
             fu_total%/%30==5 & fu_total%%30!=0 ~ fu_total%%30,
             fu_total%/%30==5 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30< 5 ~ NA_real_)) %>%
       mutate(follow_up_m7=case_when(
-            fu_total%/%30>=7 ~ 30,
+            fu_total%/%30> 6 ~ 30,
             fu_total%/%30==6 & fu_total%%30!=0 ~ fu_total%%30,
             fu_total%/%30==6 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30< 6 ~ NA_real_)) %>%
       mutate(follow_up_m8=case_when(
-            fu_total%/%30>=8 ~ 30,
+            fu_total%/%30> 7 ~ 30,
             fu_total%/%30==7 & fu_total%%30!=0 ~ fu_total%%30,
             fu_total%/%30==7 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30< 7 ~ NA_real_)) %>%
       mutate(follow_up_m9=case_when(
-            fu_total%/%30>=9 ~ 30,
+            fu_total%/%30> 8 ~ 30,
             fu_total%/%30==8 & fu_total%%30!=0 ~ fu_total%%30,
             fu_total%/%30==8 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30< 8 ~ NA_real_)) %>%
       mutate(follow_up_m10=case_when(
-            fu_total%/%30>=10~ 30,
+            fu_total%/%30> 9~ 30,
             fu_total%/%30==9 & fu_total%%30!=0 ~ fu_total%%30,
             fu_total%/%30==9 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30<9~NA_real_)) %>%
       mutate(follow_up_m11=case_when(
-            fu_total%/%30>=11~ 30,
+            fu_total%/%30> 10~ 30,
             fu_total%/%30==10 & fu_total%%30!=0 ~ fu_total%%30,
             fu_total%/%30==10 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30<10~NA_real_)) %>%
       mutate(follow_up_m12=case_when(
-            fu_total%/%30>=12~ 30,
+            fu_total%/%30> 11~ 30,
             fu_total%/%30==11 & fu_total%%30!=0 ~ fu_total%%30,
             fu_total%/%30==11 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30<11~NA_real_))
@@ -97,63 +112,64 @@ lapply(lc_exp_matched[follow_up], summary)
 com_matched<- com_matched%>% 
       mutate(follow_up_m1=case_when(
             fu_total%/%30>=1 ~ 30,
-            fu_total%/%30 <1 & fu_total!=0 ~ fu_total,
+            fu_total%/%30 <1 & fu_total!=0  ~ fu_total,
             fu_total == 0 ~ NA_real_)) %>% 
       mutate(follow_up_m2=case_when(
-            fu_total%/%30>=2 ~ 30,
+            fu_total%/%30> 1 ~ 30,
             fu_total%/%30==1 & fu_total%%30!=0 ~ fu_total%%30,
             fu_total%/%30==1 & fu_total%%30==0 ~ NA_real_,
-            fu_total%/%30< 1 ~ NA_real_)) %>% 
+            fu_total%/%30 < 1 ~ NA_real_)) %>% 
       mutate(follow_up_m3=case_when(
-            fu_total%/%30>=3 ~ 30,
+            fu_total%/%30> 2 ~ 30,
             fu_total%/%30==2 & fu_total%%30!=0  ~ fu_total%%30,
             fu_total%/%30==2 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30< 2 ~ NA_real_)) %>%
       mutate(follow_up_m4=case_when(
-            fu_total%/%30>=4 ~ 30,
+            fu_total%/%30> 3 ~ 30,
             fu_total%/%30==3 & fu_total%%30!=0  ~ fu_total%%30,
             fu_total%/%30==3 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30< 3 ~ NA_real_)) %>%
       mutate(follow_up_m5=case_when(
-            fu_total%/%30>=5 ~ 30,
+            fu_total%/%30> 4 ~ 30,
             fu_total%/%30==4 & fu_total%%30!=0  ~ fu_total%%30,
             fu_total%/%30==4 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30< 4 ~ NA_real_)) %>%
       mutate(follow_up_m6=case_when(
-            fu_total%/%30>=6 ~ 30,
+            fu_total%/%30> 5 ~ 30,
             fu_total%/%30==5 & fu_total%%30!=0 ~ fu_total%%30,
             fu_total%/%30==5 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30< 5 ~ NA_real_)) %>%
       mutate(follow_up_m7=case_when(
-            fu_total%/%30>=7 ~ 30,
+            fu_total%/%30> 6 ~ 30,
             fu_total%/%30==6 & fu_total%%30!=0 ~ fu_total%%30,
             fu_total%/%30==6 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30< 6 ~ NA_real_)) %>%
       mutate(follow_up_m8=case_when(
-            fu_total%/%30>=8 ~ 30,
+            fu_total%/%30> 7 ~ 30,
             fu_total%/%30==7 & fu_total%%30!=0 ~ fu_total%%30,
             fu_total%/%30==7 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30< 7 ~ NA_real_)) %>%
       mutate(follow_up_m9=case_when(
-            fu_total%/%30>=9 ~ 30,
+            fu_total%/%30> 8 ~ 30,
             fu_total%/%30==8 & fu_total%%30!=0 ~ fu_total%%30,
             fu_total%/%30==8 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30< 8 ~ NA_real_)) %>%
       mutate(follow_up_m10=case_when(
-            fu_total%/%30>=10~ 30,
+            fu_total%/%30> 9~ 30,
             fu_total%/%30==9 & fu_total%%30!=0 ~ fu_total%%30,
             fu_total%/%30==9 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30<9~NA_real_)) %>%
       mutate(follow_up_m11=case_when(
-            fu_total%/%30>=11~ 30,
+            fu_total%/%30> 10~ 30,
             fu_total%/%30==10 & fu_total%%30!=0 ~ fu_total%%30,
             fu_total%/%30==10 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30<10~NA_real_)) %>%
       mutate(follow_up_m12=case_when(
-            fu_total%/%30>=12~ 30,
+            fu_total%/%30> 11~ 30,
             fu_total%/%30==11 & fu_total%%30!=0 ~ fu_total%%30,
             fu_total%/%30==11 & fu_total%%30==0 ~ NA_real_,
             fu_total%/%30<11~NA_real_))
+
 
 
 lapply(com_matched[follow_up], summary)
