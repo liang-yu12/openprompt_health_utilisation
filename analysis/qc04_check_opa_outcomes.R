6ce05fda0168e587b0c46a62492efebb5222b455
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-08-25 11:26:01 +0100

    Compare the distribution of regions and unreasonalbe counts

--- analysis/qc04_check_opa_outcomes.R
+++ analysis/qc04_check_opa_outcomes.R
@@ -1,14 +1,25 @@
 source("analysis/dm03_9_pivot_opa_long.R")
 
-opa_couts_summary <- matched_data_opa_ts %>% group_by(exposure) %>% 
+# # For later data management: keep covariates
+for_covariates <- matched_data_opa_ts %>% distinct(patient_id, exposure, .keep_all = T) %>% 
+      dplyr::select("patient_id",     
+                    "exposure",                
+                    "region",      
+                    "previous_covid_hosp")
+
+# 1.check previous_covid_hosp and region -----
+
+# # 1.1 Overall data ----
+opa_couts_summary <- matched_data_opa_ts %>% group_by(region, exposure) %>% 
       summarise(opa_median = median(monthly_opa_visits),
-                opa_mean = mean(monthly_opa_visits),
+                opa_mean = mean(monthly_opa_visits, na.rm = T),
                 opa_max = max(monthly_opa_visits),
+                high_use= sum(monthly_opa_visits>=4, na.rm = T),
                 fu_time = mean(follow_up_time, na.rm =T)
-                ) %>% as.data.frame() %>% mutate(data = "original")
+      ) %>% as.data.frame() %>% mutate(data = "original")
 
 
-# # 3 months
+# # 1.2 3 months-----
 matched_data_opa_3m <- matched_data_opa_ts %>% 
       filter(month %in% c(1,2,3) & !is.na(follow_up_time)) %>% 
       group_by(patient_id, exposure) %>% 
@@ -17,16 +28,21 @@ matched_data_opa_3m <- matched_data_opa_ts %>%
             follow_up = sum(follow_up_time, na.rm = T)) %>% 
       ungroup()
 
-# check distribution: 
-opa_summary_3m <- matched_data_opa_3m %>% group_by(exposure) %>% 
+matched_data_opa_3m <- left_join(matched_data_opa_3m, for_covariates,
+                                by = c("patient_id" = "patient_id", "exposure" = "exposure"))
+
+
+# # check distribution: 
+opa_summary_3m <- matched_data_opa_3m %>% group_by(region,exposure) %>% 
       summarise(opa_median = median(visits),
-                opa_mean = mean(visits),
+                opa_mean = mean(visits, na.rm =T),
                 opa_max = max(visits),
-                fu_time = mean(follow_up)
-      ) %>% as.data.frame() %>% mutate(data = "3 month")
+                high_use= sum(visits>=4, na.rm = T),
+                fu_time = mean(follow_up, na.rm =T)) %>% 
+      as.data.frame() %>% mutate(data = "3 month")
 
 
-# # 6 months
+# # 1.3 6 months-------
 matched_data_opa_6m <- matched_data_opa_ts %>% 
       filter(month %in% c(1,2,3,4,5,6) & !is.na(follow_up_time)) %>% 
       group_by(patient_id, exposure) %>% 
@@ -35,16 +51,21 @@ matched_data_opa_6m <- matched_data_opa_ts %>%
             follow_up = sum(follow_up_time, na.rm = T)) %>% 
       ungroup()
 
+
+matched_data_opa_6m <- left_join(matched_data_opa_6m, for_covariates,
+                                 by = c("patient_id" = "patient_id", "exposure" = "exposure"))
+
 # check distribution
-opa_summary_6m <- matched_data_opa_6m %>% group_by(exposure) %>% 
+opa_summary_6m <- matched_data_opa_6m %>% group_by(region,exposure) %>% 
       summarise(opa_median = median(visits),
-                opa_mean = mean(visits),
+                opa_mean = mean(visits, na.rm =T),
                 opa_max = max(visits),
-                fu_time = mean(follow_up)
-      ) %>% as.data.frame() %>% mutate(data = "6 month")
+                high_use= sum(visits>=4, na.rm = T),
+                fu_time = mean(follow_up, na.rm =T)) %>% 
+      as.data.frame() %>% mutate(data = "6 month")
 
 
-# follow 12 months 
+# 1.4 follow 12 months  ------
 matched_data_opa_12m <- matched_data_opa_ts %>% 
       filter(!is.na(follow_up_time)) %>% 
       group_by(patient_id, exposure) %>% 
@@ -53,13 +74,16 @@ matched_data_opa_12m <- matched_data_opa_ts %>%
             follow_up = sum(follow_up_time, na.rm = T)) %>% 
       ungroup()
 
-opa_summary_12m <- matched_data_opa_12m %>% group_by(exposure) %>% 
-      summarise(opa_median = median(visits),
-                opa_mean = mean(visits),
-                opa_max = max(visits),
-                fu_time = mean(follow_up)
-      ) %>% as.data.frame() %>% mutate(data = "12 month")
+matched_data_opa_12m <- left_join(matched_data_opa_12m, for_covariates,
+                                 by = c("patient_id" = "patient_id", "exposure" = "exposure"))
 
+opa_summary_12m <- matched_data_opa_12m %>% group_by(region,exposure) %>% 
+      summarise(opa_median = median(visits),
+                opa_mean = mean(visits, na.rm =T),
+                opa_max = max(visits),
+                high_use= sum(visits>=4, na.rm = T),
+                fu_time = mean(follow_up, na.rm =T)) %>% 
+      as.data.frame() %>% mutate(data = "12 month")
 
 bind_rows(
       opa_couts_summary,
