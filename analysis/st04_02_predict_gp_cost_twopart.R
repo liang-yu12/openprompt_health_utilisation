d41cc922923fe739f29d60effbcbc2a802ec35fb
Liang-Yu Lin <58079677+liang-yu12@users.noreply.github.com>
2023-09-14 07:54:34 +0100

    correct the predict data

--- analysis/st04_02_predict_gp_cost_twopart.R
+++ analysis/st04_02_predict_gp_cost_twopart.R
@@ -222,7 +222,7 @@ predict_avg_gp_cost_fn <- function(dataset, fu_time, first_reg, sec_reg){
       input$nonzero_chance <- predict(first_reg, newdata = input, type = "response")
       
       # Predict the cost by using original data
-      tpm <- predict(sec_reg, newdata = dataset, type ="link", se.fit =T)
+      tpm <- predict(sec_reg, newdata = input, type ="link", se.fit =T)
       
       # Calculate 95% CI
       input <- input %>% mutate(
