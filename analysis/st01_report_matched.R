e89a16139d7fd895ad858df682f5e3a602405aea
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-09-26 11:57:02 +0100

    Changed median to mean(SD)

--- analysis/st01_report_matched.R
+++ analysis/st01_report_matched.R
@@ -56,11 +56,10 @@ basic_demographic <- matched_data %>% summary_factorlist(dependent, explanatory,
 outcomes <- c(visit_cols, gp_visits, hos_visits, a_e_visits, opd_visits, opa_lc, fu_cols, costs)
 
 outcome_summary <- matched_data %>% summary_factorlist(dependent, outcomes,
-                                                       p = TRUE, p_cont_para = "t.test",
+                                                       p = TRUE, p_cont_para = "aov",
                                                        add_row_totals = TRUE,
                                                        row_totals_colname = "Total",
-                                                       cont = "median",
-                                                       cont_range = T)
+                                                       cont = "mean",  cont_cut = 0)
 
 bind_rows(basic_demographic, outcome_summary) %>% 
       write.csv(here("output", "st01_matched_numbers_table.csv"), row.names = F)
