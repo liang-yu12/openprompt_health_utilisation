701744c54fce67f98c222760f0e48b0420429a66
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-09-26 16:35:52 +0100

    Add prediction functions

--- analysis/st03_03_model07_hos_hurdel.R
+++ analysis/st03_03_model07_hos_hurdel.R
@@ -98,12 +98,12 @@ positive_nb_tidy_fu <- function(vg_reg){
 # Organise the first part outputs:
 crude_binomial_outputs <-bind_rows(
       (binomial_tidy_fn(crude_binomial_12m) %>% mutate(time="12 months"))
-) %>% mutate(Adjustment = "Crude GP")
+) %>% mutate(Adjustment = "Crude Hospitalisation")
 
 # Organise the second part outputs:
 crude_hurdle_outputs <- bind_rows(
       (positive_nb_tidy_fu(crude_nb_12m) %>% mutate(time="12 months"))
-) %>% mutate(Adjustment = "Crude GP")
+) %>% mutate(Adjustment = "Crude Hospitalisation")
 
 
 # Adjusted hurdle model:
@@ -149,4 +149,42 @@ st03_03_hos_hurdle <- bind_rows(crude_hurdle_outputs, adj_hurdle_outputs)
 st03_03_hos_hurdle %>% write_csv(here("output", "st03_03_hos_hurdle.csv"))
 
 
+
+# Predict the average healthcare visits:  ----
+# function to predict the average adjusted visits:
+average_visits_fn <- function(dataset, reg_1st, reg_2nd){
+      # part 1:
+      p1 <- predict(reg_1st, type= "response") # predict the first part non-zero prob
+      dataset$nonzero_prob <- p1 # add the probability to the original data
+      # part 2: 
+      p2 <- predictvglm(reg_2nd, newdata = dataset, type = "link", se.fit = T)
+      dataset <- dataset %>% mutate(
+            predict_visit = exp(p2$fitted.values),
+            predict_lci = exp(p2$fitted.values - 1.96*p2$se.fit),
+            predict_hci = exp(p2$fitted.values + 1.96*p2$se.fit)
+      )
+      # multiply p1 and p2
+      dataset <- dataset %>% mutate(
+            c_visit = nonzero_prob*predict_visit,
+            c_lci = nonzero_prob*predict_lci,
+            c_hci = nonzero_prob*predict_hci)
+      
+      results <- dataset %>% group_by(exposure) %>% 
+            summarise(visits = mean(c_visit),
+                      lci = mean(c_lci),
+                      hci = mean(c_hci)
+            )
+      return(results)
+}
+
+# run the predict function and summarised the average vistis:
+summarised_results <- bind_rows(
+      (average_visits_fn(dataset = crude_hos_complete_12m, 
+                         reg_1st = crude_binomial_12m, 
+                         reg_2nd = crude_nb_12m) %>% mutate(model = "Crude")),
+      (average_visits_fn(dataset = adj_hos_complete_12m, 
+                         reg_1st = adj_binomial_12m, 
+                         reg_2nd = adj_nb_12m) %>% mutate(model = "Adjusted")))
+
+summarised_results %>% write_csv(here("output", "st03_03_hos_predicted_counts.csv"))
 
