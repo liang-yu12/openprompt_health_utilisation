9831fa09b1bb63fbf5fe44ce7add5cee61ee6487
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-10-04 17:32:40 +0100

    add the gp cost reg summary

--- analysis/st04_02_predict_gp_cost_twopart.R
+++ analysis/st04_02_predict_gp_cost_twopart.R
@@ -216,6 +216,21 @@ bind_rows(crude_gamma_glm_gp, adj_gamma_glm_gp) %>%
       write_csv(here("output", "st04_02_gp_cost_twopm_output.csv"))
 
 
+# Save the detailed outputs to a text file:
+sink(here("output", "st04_02_gp_reg_summary.txt"))
+print("# Crude binomial model output part 1 ---------")
+print(summary(crude_binary))
+print("# Crude hurdle model output part 2 ---------")
+print(summary(crude_gamma))
+print("# Adjusted binomial model output part 1 ---------")
+print(summary(adj_binary))
+print("# Adjusted hurdle model output part 2 ---------")
+print(summary(adj_gamma))
+sink()
+
+
+
+
 # Crude prediction: -----
 # Use the outputs from the previous models and run the prediction.
 # the follow-up time are set according to the model length
