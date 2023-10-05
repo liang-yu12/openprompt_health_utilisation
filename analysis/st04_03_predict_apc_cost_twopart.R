603af6c12ca52703001315ed11187004476cdb2c
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-10-05 15:30:22 +0100

    fix typo

--- analysis/st04_03_predict_apc_cost_twopart.R
+++ analysis/st04_03_predict_apc_cost_twopart.R
@@ -221,13 +221,13 @@ bind_rows(crude_gamma_glm_apc, adj_gamma_glm_apc) %>%
 # Save the detailed outputs to a text file:
 sink(here("output", "st04_03_apc_cost_reg_summary.txt"))
 print("# Crude binomial model output part 1 ---------")
-print(summary(crude_binary))
+print(summary(crude_binomial_apc))
 print("# Crude hurdle model output part 2 ---------")
-print(summary(crude_gamma))
+print(summary(crude_gamma_glm_apc))
 print("# Adjusted binomial model output part 1 ---------")
-print(summary(adj_binary))
+print(summary(adj_binomial_apc))
 print("# Adjusted hurdle model output part 2 ---------")
-print(summary(adj_gamma))
+print(summary(adj_gamma_glm_apc))
 sink()
 
 
