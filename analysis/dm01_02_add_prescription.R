5052c020d47326df202df7a297312b24910e34c0
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-10-31 15:33:30 +0000

    Organise the outcomes

--- analysis/dm01_02_add_prescription.R
+++ analysis/dm01_02_add_prescription.R
@@ -31,13 +31,12 @@ com_drug_counts <- com_drug_counts %>% mutate(exposure = ifelse(exposure ==1, "L
 # Data management of the exposure group: ------
 
 # Group same drug counts by months
-num_drugs <- 12
-for (i in 1:num_drugs) {
+for (i in 1:12) {
       assign(paste0("drug_", i), exp_drug_counts[, grepl(paste0("_drug_", i, "$"), names(exp_drug_counts)), with = FALSE] %>% names() %>% as.vector())
 }
 
 # Calculate the row sums by months
-for (i in 1:num_drugs) {
+for (i in 1:12) {
       drug_vector <- get(paste0("drug_", i))
       exp_drug_counts[[paste0("prescription_", i)]] <- rowSums(exp_drug_counts[, drug_vector], na.rm = T)
 }
@@ -52,12 +51,14 @@ exp_drug_counts <- exp_drug_counts %>% dplyr::select(patient_id, exposure, all_o
 
 # Data management of the comparator group: 
 # Group same drugs use in the same month
-for (i in 1:num_drugs) {
+
+
+for (i in 1:12) {
       assign(paste0("drug_", i), com_drug_counts[, grepl(paste0("_drug_", i, "$"), names(com_drug_counts)), with = FALSE] %>% names() %>% as.vector())
 }
 
 # Calculate the row sums by months
-for (i in 1:num_drugs) {
+for (i in 1:12) {
       drug_vector <- get(paste0("drug_", i))
       com_drug_counts[[paste0("prescription_", i)]] <- rowSums(com_drug_counts[, drug_vector], na.rm = T)
 }
@@ -74,3 +75,64 @@ com_matched <-  left_join(com_matched, com_drug_counts,
 
 # housekeeping
 rm(list = ls(pattern = "drug"))
+
+
+# Define the total healthcare utilisations: 
+# concept: Primary care resource use = GP consultations + GP prescription
+# total healthcare utilization: GP visits + GP prescriptions + Hospital admission + A&E visits + OPA visits
+
+# set up primary care use vectors, prescription and gp visits
+for (i in 1:12) {
+      assign(paste0("primary_care_", i), c(paste0("prescription_", i), paste0("gp_visit_m", i)))
+}
+
+# # Exposure group: ----
+
+# Primary care use by month: adding gp visits and prescription counts at the same month
+for (i in 1:12) {
+      primary_care_vector <- get(paste0("primary_care_", i))
+      lc_exp_matched[[paste0("primary_care_use_", i)]] <- rowSums(lc_exp_matched[, primary_care_vector], na.rm = T)
+}
+
+# Define secondary healthcare visit vectors: need to exclude long COVID clinic visits (without opa LC visit)
+for (i in 1:12) {
+      visit_vector <- lc_exp_matched[, grepl(paste0("visit_m", i, "$"), names(lc_exp_matched)), with = FALSE] %>% names() %>% as.vector()
+      visit_vector <- visit_vector[!visit_vector==paste0("opa_lc_visit_m", i)]
+      visit_vector <- visit_vector[!visit_vector==paste0("gp_visit_m", i)]
+      assign(paste0("secondary_care_", i), visit_vector)
+}
+
+# set up total visits vectors: all from primary + secondary
+for (i in 1:12) {
+      primary_care_vector <- get(paste0("primary_care_", i))
+      secondary_care_vector <- get(paste0("secondary_care_", i))
+      assign(paste0("total_care_", i), c(primary_care_vector, secondary_care_vector))
+}
+
+# Add primary and secondary care together: 
+for (i in 1:12) {
+      total_use <- get(paste0("total_care_", i))
+      lc_exp_matched[[paste0("all_month_m", i)]] <- rowSums(lc_exp_matched[, total_use], na.rm = T)
+}
+
+
+#  # Control group: -----
+# Define primary care use, by adding gp visits and prescription counts at the same month
+for (i in 1:12) {
+      primary_care_vector <- get(paste0("primary_care_", i))
+      com_matched[[paste0("primary_care_use_", i)]] <- rowSums(com_matched[, primary_care_vector], na.rm = T)
+}
+
+# Add primary and secondary care together: 
+for (i in 1:12) {
+      total_use <- get(paste0("primary_care_", i))
+      com_matched[[paste0("all_month_m", i)]] <- rowSums(com_matched[, total_use], na.rm = T)
+}
+
+# Outcomes summary: (i =1:12)
+# 1. primary_care_use_i: gp visits + prescription counts 
+# 2. gp_visit_mi: gp visits
+# 3. prescription_i: prescription counts
+# 4. hos_visit_mi: hospital admission counts
+# 5. opa_visit_mi: outpatient clinic counts
+# 6. ae_visit_mi: A&E visits counts
