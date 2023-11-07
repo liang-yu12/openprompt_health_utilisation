0dded4b944d610d9f78533c8d4252982cc9abc3f
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-11-07 17:40:13 +0000

    fix source and remove unused subsets

--- analysis/st04_01_total_cost_twopart.R
+++ analysis/st04_01_total_cost_twopart.R
@@ -1,106 +1,18 @@
-source("analysis/dm04_03_pivot_long.R")
+source("analysis/dm03_03_now_pivot_total_long.R")
 
 # Goal: analysing long COVID exposure and the cost outcomes
 # Model: two-part model 
 
-# Data management: colllapsing data by different follow-up time. -------
-# 3 months 
-matched_cost_3m <- matched_cost_ts %>% 
-      filter(month %in% c(1,2,3)& !is.na(follow_up_time)) %>% 
-      group_by(patient_id, exposure) %>% 
-      summarise(
-            total_cost = sum(monthly_total_cost, na.rm =T),
-            follow_up = sum(follow_up_time, na.rm = T)) %>% 
-      ungroup()
-
-# 6 months
-matched_cost_6m <- matched_cost_ts %>% 
-      filter(month %in% c(1,2,3,4,5,6)& !is.na(follow_up_time)) %>% 
-      group_by(patient_id, exposure) %>% 
-      summarise(
-            total_cost = sum(monthly_total_cost, na.rm =T),
-            follow_up = sum(follow_up_time, na.rm = T)) %>% 
-      ungroup()
-
-# 12 months
-matched_cost_12m <- matched_cost_ts %>% 
-      filter(!is.na(follow_up_time)) %>% 
-      group_by(patient_id, exposure) %>% 
-      summarise(
-            total_cost = sum(monthly_total_cost, na.rm =T),
-            follow_up = sum(follow_up_time, na.rm = T)) %>% 
-      ungroup()
-
-
-# # Add covariates for adjustment
-for_covariates <- matched_cost_ts %>% distinct(patient_id, exposure, .keep_all = T) %>% 
-      dplyr::select("patient_id",     
-                    "exposure",           
-                    "age", "age_cat",               
-                    "sex",                     
-                    "bmi_cat",
-                    "ethnicity_6",             
-                    "imd_q5",                  
-                    "region",      
-                    "cov_asthma",
-                    "cov_mental_health",   
-                    "previous_covid_hosp",     
-                    "cov_covid_vax_n_cat",     
-                    "number_comorbidities_cat")
-
-levels_check <- c("exposure", "sex", "bmi_cat", "ethnicity_6", "imd_q5",                  
-                  "region", "previous_covid_hosp", "cov_covid_vax_n_cat", "number_comorbidities_cat")
-
-
-for_covariates$sex <- relevel(for_covariates$sex, ref = "male")
-for_covariates$bmi_cat <- relevel(for_covariates$bmi_cat, ref = "Normal Weight")
-for_covariates$ethnicity_6 <- relevel(for_covariates$ethnicity_6, ref = "White")
-for_covariates$imd_q5 <- relevel(for_covariates$imd_q5, ref = "least_deprived")
-for_covariates$region <- relevel(for_covariates$region, ref = "London" )
-for_covariates$cov_mental_health <- relevel(for_covariates$cov_mental_health, ref = "FALSE")
-for_covariates$previous_covid_hosp <- relevel(for_covariates$previous_covid_hosp, ref = "FALSE")
-for_covariates$previous_covid_hosp <- relevel(for_covariates$previous_covid_hosp, ref = "FALSE")
-for_covariates$cov_covid_vax_n_cat <- relevel(for_covariates$cov_covid_vax_n_cat, ref = "0 dose")
-for_covariates$number_comorbidities_cat <- relevel(for_covariates$number_comorbidities_cat, ref = "0")
-
-lapply(for_covariates[levels_check], levels) # need to correct some levels
-
-# # add covariates back to the summarised data frame
-matched_cost_3m <- left_join(matched_cost_3m, for_covariates,
-                             by = c("patient_id" = "patient_id", "exposure" = "exposure"))
-
-matched_cost_6m <- left_join(matched_cost_6m, for_covariates,
-                             by = c("patient_id" = "patient_id", "exposure" = "exposure"))
-
-matched_cost_12m <- left_join(matched_cost_12m, for_covariates,
-                              by = c("patient_id" = "patient_id", "exposure" = "exposure"))
-
-# Make sure the exposure level is correct
-matched_cost_3m$exposure <- relevel(matched_cost_3m$exposure, ref = "Comparator")
-matched_cost_6m$exposure <- relevel(matched_cost_6m$exposure, ref = "Comparator")
-matched_cost_12m$exposure <- relevel(matched_cost_12m$exposure, ref = "Comparator")
 
 # Stats: Two-part model: -----
 # Data management: exclude rows with NA in the model: 
 crude_vars <- c("total_cost", "exposure", "follow_up")
 
-crude_cost_complete_3m <- matched_cost_3m %>% drop_na(any_of(crude_vars)) %>% 
-      mutate(cost_binary = ifelse(total_cost>0, 1, 0))
-crude_cost_complete_6m <- matched_cost_3m %>% drop_na(any_of(crude_vars)) %>% 
-      mutate(cost_binary = ifelse(total_cost>0, 1, 0))
 crude_cost_complete_12m <- matched_cost_3m %>% drop_na(any_of(crude_vars)) %>% 
       mutate(cost_binary = ifelse(total_cost>0, 1, 0)) 
 
 # Crude Two-part model: -----
 # First part: binomial model 
-
-curde_binomial_3m <- glm(cost_binary ~ exposure + offset(log(follow_up)),
-                         data = crude_cost_complete_3m,
-                         family = binomial(link="logit"))
-
-curde_binomial_6m <- glm(cost_binary ~ exposure + offset(log(follow_up)),
-                         data = crude_cost_complete_6m,
-                         family = binomial(link="logit"))
 
 curde_binomial_12m <- glm(cost_binary ~ exposure + offset(log(follow_up)),
                          data = crude_cost_complete_12m,
@@ -123,11 +35,9 @@ binary_output_fn <- function(bi_reg){
       
 }
 
-crude_binary <- bind_rows(
-      binary_output_fn(curde_binomial_3m) %>% mutate(time = "3m"),
-      binary_output_fn(curde_binomial_6m) %>% mutate(time = "6m"),
-      binary_output_fn(curde_binomial_12m) %>% mutate(time = "12m")
-) %>% mutate(model = "Crude")
+crude_binary <- binary_output_fn(curde_binomial_12m) %>% 
+      mutate(time = "12m") %>% 
+      mutate(model = "Crude")
 
 
 
