51f30dbac30dfe8edf8feb81f17fa509a3b98ad9
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-11-07 12:33:50 +0000

    add the data management codes for stata

--- analysis/dm02_03_now_pivot_hos_long.R
+++ analysis/dm02_03_now_pivot_hos_long.R
@@ -74,3 +74,52 @@ com_hos_long$follow_up_time %>% summary
 matched_data_hos_ts <- bind_rows(exp_hos_long, com_hos_long)
 matched_data_hos_ts$exposure <- factor(matched_data_hos_ts$exposure, levels = c("Comparator", "Long covid exposure"))
 matched_data_hos_ts$exposure %>% levels()
+
+
+# Data management for modeling:: --------
+# Collapsing data by summarising the visits and follow-up time, and 
+# generate three datasets for follow-up 12m
+
+# follow 12 months 
+matched_data_hos_12m <- matched_data_hos_ts %>% 
+      filter(!is.na(follow_up_time)) %>% 
+      group_by(patient_id, exposure) %>% 
+      summarise(
+            visits = sum(monthly_hos_visits),
+            follow_up = sum(follow_up_time)) %>% 
+      ungroup()
+
+
+# # Add covariates for adjustment
+for_covariates <- matched_data_hos_ts %>% distinct(patient_id, exposure, .keep_all = T) %>% 
+      dplyr::select("patient_id",     
+                    "exposure",           
+                    "age", "age_cat",               
+                    "sex",                     
+                    "bmi_cat",
+                    "ethnicity_6",             
+                    "imd_q5",                  
+                    "region",      
+                    "cov_asthma",
+                    "cov_mental_health",   
+                    "previous_covid_hosp",     
+                    "cov_covid_vax_n_cat",     
+                    "number_comorbidities_cat")
+for_covariates$sex <- relevel(for_covariates$sex, ref = "male")
+for_covariates$bmi_cat <- relevel(for_covariates$bmi_cat, ref = "Normal Weight")
+for_covariates$ethnicity_6 <- relevel(for_covariates$ethnicity_6, ref = "White")
+for_covariates$imd_q5 <- relevel(for_covariates$imd_q5, ref = "least_deprived")
+for_covariates$region <- relevel(for_covariates$region, ref = "London" )
+for_covariates$cov_mental_health <- relevel(for_covariates$cov_mental_health, ref = "FALSE")
+for_covariates$previous_covid_hosp <- relevel(for_covariates$previous_covid_hosp, ref = "FALSE")
+for_covariates$previous_covid_hosp <- relevel(for_covariates$previous_covid_hosp, ref = "FALSE")
+for_covariates$cov_covid_vax_n_cat <- relevel(for_covariates$cov_covid_vax_n_cat, ref = "0 dose")
+for_covariates$number_comorbidities_cat <- relevel(for_covariates$number_comorbidities_cat, ref = "0")
+
+# # add covariates back to the summarised data frame
+
+matched_data_hos_12m <- left_join(matched_data_hos_12m, for_covariates,
+                                  by = c("patient_id" = "patient_id", "exposure" = "exposure"))
+
+# correct the level of exposure groups
+matched_data_hos_12m$exposure <- relevel(matched_data_hos_12m$exposure, ref = "Comparator")
