d2a1374ab34e92e6fbcdb1642436ab167b0cd4f1
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-11-07 12:48:18 +0000

    remove data management codes and rename outputs

--- analysis/st02_03_now_hospital_hurdel.R
+++ analysis/st02_03_now_hospital_hurdel.R
@@ -1,55 +1,6 @@
 # Load previous data management
 source("analysis/dm02_03_now_pivot_hos_long.R")
 
-# Data management for modeling:: --------
-# Collapsing data by summarising the visits and follow-up time, and 
-# generate three datasets for follow-up 12m
-
-
-# follow 12 months 
-matched_data_hos_12m <- matched_data_hos_ts %>% 
-  filter(!is.na(follow_up_time)) %>% 
-  group_by(patient_id, exposure) %>% 
-  summarise(
-    visits = sum(monthly_hos_visits),
-    follow_up = sum(follow_up_time)) %>% 
-  ungroup()
-
-
-# # Add covariates for adjustment
-for_covariates <- matched_data_hos_ts %>% distinct(patient_id, exposure, .keep_all = T) %>% 
-      dplyr::select("patient_id",     
-                    "exposure",           
-                    "age", "age_cat",               
-                    "sex",                     
-                    "bmi_cat",
-                    "ethnicity_6",             
-                    "imd_q5",                  
-                    "region",      
-                    "cov_asthma",
-                    "cov_mental_health",   
-                    "previous_covid_hosp",     
-                    "cov_covid_vax_n_cat",     
-                    "number_comorbidities_cat")
-for_covariates$sex <- relevel(for_covariates$sex, ref = "male")
-for_covariates$bmi_cat <- relevel(for_covariates$bmi_cat, ref = "Normal Weight")
-for_covariates$ethnicity_6 <- relevel(for_covariates$ethnicity_6, ref = "White")
-for_covariates$imd_q5 <- relevel(for_covariates$imd_q5, ref = "least_deprived")
-for_covariates$region <- relevel(for_covariates$region, ref = "London" )
-for_covariates$cov_mental_health <- relevel(for_covariates$cov_mental_health, ref = "FALSE")
-for_covariates$previous_covid_hosp <- relevel(for_covariates$previous_covid_hosp, ref = "FALSE")
-for_covariates$previous_covid_hosp <- relevel(for_covariates$previous_covid_hosp, ref = "FALSE")
-for_covariates$cov_covid_vax_n_cat <- relevel(for_covariates$cov_covid_vax_n_cat, ref = "0 dose")
-for_covariates$number_comorbidities_cat <- relevel(for_covariates$number_comorbidities_cat, ref = "0")
-
-# # add covariates back to the summarised data frame
-
-matched_data_hos_12m <- left_join(matched_data_hos_12m, for_covariates,
-                                  by = c("patient_id" = "patient_id", "exposure" = "exposure"))
-
-# correct the level of exposure groups
-matched_data_hos_12m$exposure <- relevel(matched_data_hos_12m$exposure, ref = "Comparator")
-
 
 # Stats: two part (Hurdle) model -----
 # first need to exclude rows with NA and create 1/0 outcomes:
@@ -147,13 +98,13 @@ adj_hurdle_outputs <- bind_rows(
 # Combine outputs
 # Save both outputs: # Combine total outputs and save:
 st03_03_hos_binomial <- bind_rows(crude_binomial_outputs, adj_binomial_outputs)
-st03_03_hos_binomial %>% write_csv(here("output", "st03_03_hos_binomial.csv"))
+st03_03_hos_binomial %>% write_csv(here("output", "st02_03_hos_binomial.csv"))
 
 st03_03_hos_hurdle <- bind_rows(crude_hurdle_outputs, adj_hurdle_outputs)
-st03_03_hos_hurdle %>% write_csv(here("output", "st03_03_hos_hurdle.csv"))
+st03_03_hos_hurdle %>% write_csv(here("output", "st02_03_hos_hurdle.csv"))
 
 # Save the detailed outputs to a text file:
-sink(here("output", "hos_reg_summary.txt"))
+sink(here("output", "st02_03_hos_reg_summary.txt"))
 print("# Crude binomial model output part 1 ---------")
 print(summary(crude_binomial_12m))
 print("# Crude hurdle model output part 2 ---------")
@@ -238,7 +189,7 @@ bind_rows(
              mutate(model = "Crude")),
       (bi_model_count_fn(adj_hos_complete_12m) %>% mutate(time = "12m")) %>% 
             mutate(model = "Adjusted")) %>% 
-      write_csv("output/st03_03_hos_binomial_model_counts.csv")
+      write_csv("output/st02_03_hos_binomial_model_counts.csv")
 
 
 bind_rows(
@@ -246,5 +197,5 @@ bind_rows(
             mutate(model = "Crude"),
       (hurdle_model_count_fn(adj_hos_complete_12m) %>% mutate(time = "12m")) %>% 
             mutate(model = "Adjusted")) %>% 
-      write_csv("output/st03_03_hos_hurdle_model_counts.csv")
+      write_csv("output/st02_03_hos_hurdle_model_counts.csv")
 
