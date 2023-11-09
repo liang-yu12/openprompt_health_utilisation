86844d4c89fa681a249a267f14ef64644291840d
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-11-09 14:32:35 +0000

    Write the unit costs togather

--- analysis/dm03_05_qc1_now_apc_unit_costs.R
+++ analysis/dm03_05_qc1_now_apc_unit_costs.R
@@ -8,6 +8,9 @@
 source("analysis/dm01_02_now_monthly_follow_up.R")
 matched_combine <- bind_rows(com_matched, lc_exp_matched)
 
+
+# Hospitalisation unit cost: ------
+
 # define admin cols
 all_admission <- c()
 for (i in 1:12){
@@ -33,3 +36,43 @@ unit_apc_costs <- matched_combine %>%
       summarise(unit_cost = sum(total_admission_costs, na.rm = T)/sum(total_admission_counts, na.rm = T))
 
 
+# OPA unit cost:
+# cost vectors
+opa_cost <- c()
+for (i in 1:12){
+      opa_cost <- c(opa_cost, paste0("opd_cost_m", i))
+}
+matched_combine$total_opa_cost <- rowSums(matched_combine[,opa_cost], na.rm = T)
+
+# visit vectors
+opa_visit <- c()
+for (i in 1:12) {
+      opa_visit <- c(opa_visit, paste0("opa_visit_m", i))
+}
+matched_combine$total_opa_visit <- rowSums(matched_combine[,opa_visit], na.rm = T)
+
+
+# Keep people with both records, and then calculate the average cost for inputation
+unit_opa_cost <- matched_combine %>% 
+      filter(total_opa_visit>0 & total_opa_cost>0) %>% 
+      summarise(unit_cost = sum(total_opa_cost, na.rm = T)/sum(total_opa_visit, na.rm = T))
+
+
+# A&E visit unit cost: ----
+# A&E visit costs
+ane_cost <- c()
+for (i in 1:12){
+      ane_cost <- c(ane_cost, paste0("er_cost_m", i))
+}
+# ane visit cols
+ane_visit <- c()
+for (i in 1:12){
+      ane_visit <- c(ane_visit, paste0("ae_visit_m", i))      
+}
+
+matched_combine$total_ane_costs <- rowSums(matched_combine[, ane_cost], na.rm = T)
+matched_combine$total_ane_visits <- rowSums(matched_combine[, ane_visit], na.rm = T)
+
+unit_ane_cost <- matched_combine %>% 
+      filter(total_ane_visits>0 & total_ane_costs>0) %>% 
+      summarise(unit_cost = sum(total_ane_costs, na.rm = T)/sum(total_ane_visits, na.rm = T))
