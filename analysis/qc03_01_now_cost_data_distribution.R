760755892c461603574b30f13b32b296cf019e6f
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-11-10 14:32:31 +0000

    ditch function. Filter doesn't work

--- analysis/qc03_01_now_cost_data_distribution.R
+++ analysis/qc03_01_now_cost_data_distribution.R
@@ -2,66 +2,58 @@ source("analysis/dm03_03_v2_now_pivot_total_long_inputed_cost.R")
 
 # Goal: check the secondary cost distribution
 
-description_fn <- function(var, cost_table){
-      result_1 <- combine %>% 
-            filter(var > 0 & !is.na(var)) %>% 
-            summarise(
-                  minimal = min(var, na.rm = T),
-                  median = median(var, na.rm = T),
-                  mean = mean(var, na.rm = T),
-                  max = max(var, na.rm = T)
-            ) %>% mutate(Table = cost_table) %>% 
-            relocate(Table)
-      
-      reault_2 <- combine  %>% summarise(
-            zero_count = sum(var==0, na.rm = T),
-            missing_count = sum(is.na(var), na.rm = T),
-            total_n = n()
-      )
-      
-      output <- bind_cols(result_1, reault_2)
-      
-      return(output)
-}
-
-description_group_fn <- function(var, cost_table){
-      result_1 <- combine %>% 
-            filter(var > 0 & !is.na(var)) %>% 
-            group_by(exposure) %>%
-            summarise(
-                  minimal = min(var, na.rm = T),
-                  median = median(var, na.rm = T),
-                  mean = mean(var, na.rm = T),
-                  max = max(var, na.rm = T)) %>% 
-            mutate(Table = cost_table) %>% 
-            relocate(Table)
-      
-      reault_2 <- combine %>% group_by(exposure) %>% 
-            summarise(
-                  zero_count = sum(var==0, na.rm = T),
-                  missing_count = sum(is.na(var), na.rm = T),
-                  total_n = n()
-                  )
-      output <- bind_cols(result_1, reault_2)
-      
-      return(output)
-}
-
-
-
 # Check total_admission_costs, total_ane_costs, total_opa_cost
 combine <- bind_rows(lc_exp_matched, com_matched)
 
+hos <- combine %>% filter(total_admission_costs > 0 & !is.na(total_admission_costs)) %>% 
+      summarise(
+            minimal = min(total_admission_costs, na.rm = T),
+            median = median(total_admission_costs),
+            mean = mean(total_admission_costs, na.rm = T),
+            max = max(total_admission_costs, na.rm = T)) %>% 
+      mutate(Table = "APC") %>% relocate(Table)
+
+hos_count <-  combine  %>% summarise(
+      zero_count = sum(total_admission_costs==0, na.rm = T),
+      missing_count = sum(is.na(total_admission_costs), na.rm = T),
+      total_n = n()
+)
+
+
+ec <- combine %>% filter(total_ane_costs > 0 & !is.na(total_ane_costs)) %>% 
+      summarise(
+            minimal = min(total_ane_costs, na.rm = T),
+            median = median(total_ane_costs),
+            mean = mean(total_ane_costs, na.rm = T),
+            max = max(total_ane_costs, na.rm = T)) %>% 
+      mutate(Table = "EC") %>% 
+      relocate(Table)
+
+
+ec_count <-  combine  %>% summarise(
+      zero_count = sum(total_ane_costs==0, na.rm = T),
+      missing_count = sum(is.na(total_ane_costs), na.rm = T),
+      total_n = n()
+)
+
+opa <- combine %>% filter(total_opa_cost > 0 & !is.na(total_opa_cost)) %>% 
+      summarise(
+            minimal = min(total_opa_cost, na.rm = T),
+            median = median(total_opa_cost),
+            mean = mean(total_opa_cost, na.rm = T),
+            max = max(total_opa_cost, na.rm = T)) %>% 
+      mutate(Table = "OPA") %>% 
+      relocate(Table)
+
+opa_count <-  combine  %>% summarise(
+      zero_count = sum(total_opa_cost==0, na.rm = T),
+      missing_count = sum(is.na(total_opa_cost), na.rm = T),
+      total_n = n()
+)
+
+# combine results
 bind_rows(
-      description_fn(combine$total_admission_costs, "APC"),
-      description_fn(combine$total_ane_costs, "EC (A&E)"),
-      description_fn(combine$total_opa_cost, "OPA"),
+      bind_cols(hos, hos_count),
+      bind_cols(ec, ec_count),
+      bind_cols(opa, opa_count)
 ) %>% write_csv(here("output", "qc03_01_study_all_cost_distribution.csv"))
-
-
-bind_rows(
-      description_group_fn(combine$total_admission_costs, "APC"),
-      description_group_fn(combine$total_ane_costs, "EC (A&E)"),
-      description_group_fn(combine$total_opa_cost, "OPA"),
-) %>% write_csv(here("output", "qc03_01_study_by_exp_cost_distribution.csv"))
-
