f167b30cd6a144d12670e1c6d7ddfa918c4e80cc
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-11-10 12:05:39 +0000

    Only check the distribution among people with non-zero data

--- analysis/qc03_01_now_cost_data_distribution.R
+++ analysis/qc03_01_now_cost_data_distribution.R
@@ -2,8 +2,8 @@ source("analysis/dm03_03_v2_now_pivot_total_long_inputed_cost.R")
 
 # Goal: check the secondary cost distribution
 
-description_fn <- function(var, cost_table){
-      results <- combine %>% 
+description_fn <- function(dataset, var, cost_table){
+      results <- dataset %>% 
             summarise(
                   minimal = min(var, na.rm = T),
                   median = median(var, na.rm = T),
@@ -14,8 +14,8 @@ description_fn <- function(var, cost_table){
       return(results)
 }
 
-description_group_fn <- function(var, cost_table){
-      results <- combine %>% group_by(exposure) %>% 
+description_group_fn <- function(dataset, var, cost_table){
+      results <- dataset %>% group_by(exposure) %>% 
             summarise(
                   minimal = min(var, na.rm = T),
                   median = median(var, na.rm = T),
@@ -29,19 +29,23 @@ description_group_fn <- function(var, cost_table){
 
 
 # Check total_admission_costs, total_ane_costs, total_opa_cost
-
 combine <- bind_rows(lc_exp_matched, com_matched)
 
+# First need to exclude people without any cost data:
+hos <- combine %>% filter(total_admission_costs > 0)
+ane <- combine %>% filter(total_ane_costs > 0)
+opa <- combine %>% filter(total_opa_cost > 0)
+
 bind_rows(
-      description_fn(combine$total_admission_costs, "APC"),
-      description_fn(combine$total_ane_costs, "EC (A&E)"),
-      description_fn(combine$total_opa_cost, "OPA"),
+      description_fn(hos, hos$total_admission_costs, "APC"),
+      description_fn(ane, ane$total_ane_costs, "EC (A&E)"),
+      description_fn(opa, opa$total_opa_cost, "OPA"),
 ) %>% write_csv(here("output", "qc03_01_study_all_cost_distribution.csv"))
 
 
 bind_rows(
-      description_group_fn(combine$total_admission_costs, "APC"),
-      description_group_fn(combine$total_ane_costs, "EC (A&E)"),
-      description_group_fn(combine$total_opa_cost, "OPA"),
+      description_group_fn(hos, hos$total_admission_costs, "APC"),
+      description_group_fn(ane, ane$total_ane_costs, "EC (A&E)"),
+      description_group_fn(opa, opa$total_opa_cost, "OPA"),
 ) %>% write_csv(here("output", "qc03_01_study_by_exp_cost_distribution.csv"))
 
