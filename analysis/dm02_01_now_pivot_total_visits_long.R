33c8a5f05fda5cda840b0e8d877bf5b9c99a9dd3
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-11-15 10:38:28 +0000

    remove the wrong drug visits

--- analysis/dm02_01_now_pivot_total_visits_long.R
+++ analysis/dm02_01_now_pivot_total_visits_long.R
@@ -108,7 +108,6 @@ for_covariates <- matched_data_ts %>% distinct(patient_id, exposure, .keep_all =
                     "previous_covid_hosp",     
                     "cov_covid_vax_n_cat",     
                     "number_comorbidities_cat",
-                    "total_drug_visit",
                     "reg_and_visit_gp_1y",
                     "covid_positive")
 
@@ -128,10 +127,6 @@ for_covariates$number_comorbidities_cat <- relevel(for_covariates$number_comorbi
 
 matched_data_12m <- left_join(matched_data_12m, for_covariates,
                               by = c("patient_id" = "patient_id", "exposure" = "exposure"))
-
-
-# add the total_drug_visit to the outcomes: 
-matched_data_12m <- matched_data_12m %>% mutate(visits = visits + total_drug_visit)
 
 # correct the level of exposure groups
 matched_data_12m$exposure <- relevel(matched_data_12m$exposure, ref = "Comparator")
