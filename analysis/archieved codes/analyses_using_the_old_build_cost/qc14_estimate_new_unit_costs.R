e3f2dd599da92a1c6fd56c4d55dff4d8ba297741
Liang-Yu Lin <liang-yu12@users.noreply.github.com>
2023-12-15 16:33:19 +0000

    tidy up repor

--- analysis/archieved codes/analyses_using_the_old_build_cost/qc14_estimate_new_unit_costs.R
+++
@@ -1,9 +0,0 @@
-# new cost datasets:
-# 4. new APC data:
-source("analysis/qc11_apc__new_unit_costs.R")
-
-# 5. new OPA data:
-source("analysis/qc12_opa_new_unit_costs.R")
-
-# 6. new A&E data: 
-source("analysis/qc13_ane_new_unit_costs.R")
